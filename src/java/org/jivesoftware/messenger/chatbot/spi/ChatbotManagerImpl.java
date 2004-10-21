14bbc4bfc5b5951756571359a13ef362478633fc
Matt Tucker <matt@jivesoftware.com>
2004-10-21 07:42:58 +0000

    Fixed compilation errors.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@46 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/chatbot/spi/ChatbotManagerImpl.java
+++ src/java/org/jivesoftware/messenger/chatbot/spi/ChatbotManagerImpl.java
@@ -13,22 +13,22 @@ package org.jivesoftware.messenger.chatbot.spi;
 import org.jivesoftware.messenger.container.BasicModule;
 import org.jivesoftware.util.LocaleUtils;
 import org.jivesoftware.util.Log;
-import org.jivesoftware.util.LongHashMap;
 import org.jivesoftware.util.LongList;
 import org.jivesoftware.messenger.XMPPAddress;
 import org.jivesoftware.messenger.auth.UnauthorizedException;
 import org.jivesoftware.messenger.chatbot.*;
 import org.jivesoftware.messenger.user.UserAlreadyExistsException;
 import org.jivesoftware.messenger.user.UserNotFoundException;
-import java.util.Hashtable;
+
+import java.util.*;
 
 public class ChatbotManagerImpl extends BasicModule implements ChatbotManager {
 
     private ChatbotAccountProvider accountProvider = ChatbotProviderFactory.getChatbotAccountProvider();
     private ChatbotIDProvider idProvider = ChatbotProviderFactory.getChatbotIDProvider();
     private ChatbotInfoProvider infoProvider = ChatbotProviderFactory.getChatbotInfoProvider();
-    private LongHashMap id2botname = new LongHashMap();
-    private LongHashMap id2botinfo = new LongHashMap();
+    private Map id2botname = new HashMap<Integer,Object>();
+    private Map id2botinfo = new HashMap<Integer,Object>();
     private Hashtable botname2id = new Hashtable();
 
     /**
@@ -86,9 +86,9 @@ public class ChatbotManagerImpl extends BasicModule implements ChatbotManager {
 
     public void deleteChatbot(long chatbotID) throws UnauthorizedException, UserNotFoundException {
         accountProvider.deleteChatbot(chatbotID);
-        id2botinfo.removeKey(chatbotID);
+        id2botinfo.remove(chatbotID);
         botname2id.remove(id2botname.get(chatbotID));
-        id2botname.removeKey(chatbotID);
+        id2botname.remove(chatbotID);
     }
 
     public int getChatbotCount() {

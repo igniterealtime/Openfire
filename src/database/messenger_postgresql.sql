6199fa43e8701038ddc22fbdd32eeaa147644574
Gaston Dombiak <gaston@jivesoftware.com>
2004-10-21 14:08:32 +0000

    Adds MUC tables.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@49 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/database/messenger_postgresql.sql
+++ src/database/messenger_postgresql.sql
@@ -161,6 +161,52 @@ CREATE TABLE jiveID (
   CONSTRAINT jiveID_pk PRIMARY KEY (idType)
 );
 
+-- MUC Tables
+
+CREATE TABLE mucRoom (
+  roomID              INTEGER       NOT NULL,
+  name                VARCHAR(50)   NULL,
+  description         VARCHAR(255),
+  canChangeSubject    INTEGER       NOT NULL,
+  maxUsers            INTEGER       NOT NULL,
+  publicRoom          INTEGER       NOT NULL,
+  moderated           INTEGER       NOT NULL,
+  invitationRequired  INTEGER       NOT NULL,
+  canInvite           INTEGER       NOT NULL,
+  passwordProtected   INTEGER       NOT NULL,
+  password            VARCHAR(50)   NULL,
+  canDiscoverJID      INTEGER       NOT NULL,
+  logEnabled          INTEGER       NOT NULL,
+  subject             VARCHAR(100)  NULL,
+  rolesToBroadcast    INTEGER       NOT NULL,
+  inMemory            INTEGER       NOT NULL,
+  CONSTRAINT mucRoom__pk PRIMARY KEY (name)
+);
+
+CREATE INDEX mucRoom_roomID_idx ON mucRoom(roomID);
+
+CREATE TABLE mucAffiliation (
+  roomID              INTEGER        NOT NULL,
+  jid                 VARCHAR(3000)  NOT NULL,
+  affiliation         INTEGER        NOT NULL,
+  CONSTRAINT mucAffiliation__pk PRIMARY KEY (roomID,jid)
+);
+
+CREATE TABLE mucMember (
+  roomID              INTEGER        NOT NULL,
+  jid                 TEXT           NOT NULL,
+  nickname            VARCHAR(255)   NULL,
+  CONSTRAINT mucMember__pk PRIMARY KEY (roomID,jid)
+);
+
+CREATE TABLE mucConversationLog (
+  roomID              INTEGER        NOT NULL,
+  sender              TEXT           NOT NULL,
+  nickname            VARCHAR(255)   NULL,
+  time                CHAR(15)       NOT NULL,
+  subject             VARCHAR(255)   NULL,
+  body                TEXT           NULL
+);
 
 -- Finally, insert default table values.
 

4c7838e3e3b483ac3b492d90f4c62f0d27ca190e
Gaston Dombiak <gaston@jivesoftware.com>
2004-11-03 12:01:06 +0000

    Removed System.out used for debugging.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@196 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/container/spi/BootstrapContainer.java
+++ src/java/org/jivesoftware/messenger/container/spi/BootstrapContainer.java
@@ -272,7 +272,6 @@ public abstract class BootstrapContainer implements Container, ServiceLookupProv
         for (Module module : modules) {
             boolean started = false;
             try {
-                System.out.println(module);
                 module.start();
             }
             catch (Exception e) {

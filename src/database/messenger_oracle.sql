f4ba007d64b66783f5e83a01521c4c5a65e0c8b1
Gaston Dombiak <gaston@jivesoftware.com>
2004-11-08 12:11:24 +0000

    Renamed database column naturalLanguageRoom to naturalRoomName.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@226 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/database/messenger_oracle.sql
+++ src/database/messenger_oracle.sql
@@ -172,7 +172,7 @@ CREATE TABLE mucRoom(
   creationDate        CHAR(15)      NOT NULL,
   modificationDate    CHAR(15)      NOT NULL,
   name                VARCHAR2(50)  NOT NULL,
-  naturalLanguageRoom VARCHAR2(255) NOT NULL,
+  naturalRoomName     VARCHAR2(255) NOT NULL,
   description         VARCHAR2(255),
   canChangeSubject    INTEGER       NOT NULL,
   maxUsers            INTEGER       NOT NULL,

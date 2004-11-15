ac7b77051f68f930624740e1a28df96cfd3418ba
Bill Lynch <billjive@gmail.com>
2004-11-15 18:36:02 +0000

    Minor
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@415 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/bin/startup.bat
+++ src/bin/startup.bat
@@ -6,4 +6,18 @@ REM # $Revision$
 REM # $Date$
 REM #
 
-start "Jive Messenger" java -jar ..\lib\startup.jar
+if "%JAVA_HOME%" == "" goto javaerror
+if not exist "%JAVA_HOME%\bin\java.exe" goto javaerror
+goto run
+
+:javaerror
+echo.
+echo Error: JAVA_HOME environment variable not set, Jive Messenger not started.
+echo.
+goto end
+
+:run
+start "Jive Messenger" %JAVA_HOME%\bin\java -jar ..\lib\startup.jar
+goto end
+
+:end

194a5146277c3488d0857e60acb5aac02b36dc55
Matt Tucker <matt@jivesoftware.com>
2005-02-01 08:42:41 +0000

    Initial check-in.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@935 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/util/SimpleSSLSocketFactory.java
@@ -0,0 +1,140 @@
+/**
+ * $RCSfile$
+ * $Revision$
+ * $Date$
+ *
+ * Copyright 2005 Jive Software.
+ *
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jivesoftware.util;
+
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.net.SocketFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.KeyManagementException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.CertificateNotYetValidException;
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.IOException;
+
+/**
+ * SSLSocketFactory that accepts any certificate chain and also accepts expired
+ * certificates.
+ *
+ * @author Matt Tucker
+ */
+public class SimpleSSLSocketFactory extends SSLSocketFactory {
+
+    private SSLSocketFactory factory;
+
+    public SimpleSSLSocketFactory() {
+
+        try {
+            SSLContext sslcontent = SSLContext.getInstance("TLS");
+            sslcontent.init(null, // KeyManager not required
+                            new TrustManager[] { new DummyTrustManager() },
+                            new java.security.SecureRandom());
+            factory = sslcontent.getSocketFactory();
+        }
+        catch (NoSuchAlgorithmException e) {
+            Log.error(e);
+        }
+        catch (KeyManagementException e) {
+            Log.error(e);
+        }
+    }
+
+    public static SocketFactory getDefault() {
+        return new SimpleSSLSocketFactory();
+    }
+
+    public Socket createSocket(Socket socket, String s, int i, boolean flag)
+            throws IOException
+    {
+        return factory.createSocket(socket, s, i, flag);
+    }
+
+    public Socket createSocket(InetAddress inaddr, int i, InetAddress inaddr2, int j)
+            throws IOException
+    {
+        return factory.createSocket(inaddr, i, inaddr2, j);
+    }
+
+    public Socket createSocket(InetAddress inaddr, int i)
+            throws IOException
+    {
+        return factory.createSocket(inaddr, i);
+    }
+
+    public Socket createSocket(String s, int i, InetAddress inaddr, int j)
+            throws IOException
+    {
+        return factory.createSocket(s, i, inaddr, j);
+    }
+
+    public Socket createSocket(String s, int i)
+            throws IOException
+    {
+        return factory.createSocket(s, i);
+    }
+
+    public String[] getDefaultCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+
+    public String[] getSupportedCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+
+    private static class DummyTrustManager implements X509TrustManager {
+
+        public boolean isClientTrusted(X509Certificate[] cert) {
+            return true;
+        }
+
+        public boolean isServerTrusted(X509Certificate[] cert) {
+            try {
+                cert[0].checkValidity();
+                return true;
+            }
+            catch (CertificateExpiredException e) {
+                return false;
+            }
+            catch (CertificateNotYetValidException e) {
+                return false;
+            }
+        }
+
+        public void checkClientTrusted(java.security.cert.X509Certificate[] x509Certificates,
+                String s) throws CertificateException
+        {
+        }
+
+        public void checkServerTrusted(java.security.cert.X509Certificate[] x509Certificates,
+                String s) throws CertificateException
+        {
+        }
+
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+}

3d3cfab713f699f311f402df4bfe02aa7288f524
Matt Tucker <matt@jivesoftware.com>
2005-03-08 01:42:42 +0000

    Updated version to 2.1.3
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@1104 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/XMPPServer.java
+++ src/java/org/jivesoftware/messenger/XMPPServer.java
@@ -168,7 +168,7 @@ public class XMPPServer {
             name = "127.0.0.1";
         }
 
-        version = new Version(2, 1, 2, Version.ReleaseStatus.Release, -1);
+        version = new Version(2, 1, 3, Version.ReleaseStatus.Release, -1);
         if ("true".equals(JiveGlobals.getXMLProperty("setup"))) {
             setupMode = false;
         }

0cad5b7f9a4accb12f92404b6b5df6e42582f95c
Matt Tucker <matt@jivesoftware.com>
2005-03-12 00:05:34 +0000

    Don't allow blank password.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@1122 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/ldap/LdapAuthProvider.java
+++ src/java/org/jivesoftware/messenger/ldap/LdapAuthProvider.java
@@ -61,7 +61,7 @@ public class LdapAuthProvider implements AuthProvider {
     }
 
     public void authenticate(String username, String password) throws UnauthorizedException {
-        if (username == null || password == null) {
+        if (username == null || password == null || "".equals(password)) {
             throw new UnauthorizedException();
         }
 

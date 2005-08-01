ce65553f083d14d5dda6df2ad54f140a64786d2d
Gaston Dombiak <gaston@jivesoftware.com>
2005-08-01 20:19:06 +0000

    Preparing for final 2.2 release.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@1735 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/XMPPServer.java
+++ src/java/org/jivesoftware/messenger/XMPPServer.java
@@ -186,7 +186,7 @@ public class XMPPServer {
             name = "127.0.0.1";
         }
 
-        version = new Version(2, 2, 0, Version.ReleaseStatus.Release_Candidate, 1);
+        version = new Version(2, 2, 0, Version.ReleaseStatus.Release, -1);
         if ("true".equals(JiveGlobals.getXMLProperty("setup"))) {
             setupMode = false;
         }

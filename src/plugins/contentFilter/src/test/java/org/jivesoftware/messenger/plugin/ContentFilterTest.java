2c4a7793975063b664020f9fb838898942f5613e
Matt Tucker <matt@jivesoftware.com>
2005-08-19 15:35:32 +0000

    Updated test cases from Conor Hayes.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@2696 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/contentFilter/src/test/java/org/jivesoftware/messenger/plugin/ContentFilterTest.java
+++ src/plugins/contentFilter/src/test/java/org/jivesoftware/messenger/plugin/ContentFilterTest.java
@@ -11,146 +11,150 @@ import org.xmpp.packet.Message;
  * 
  * @author chayes
  */
-public class ContentFilterTest extends TestCase
-{
-  private ContentFilter filter;
+public class ContentFilterTest extends TestCase {
+    private ContentFilter filter;
 
-  public static void main(String[] args)
-  {
-    junit.textui.TestRunner.run(ContentFilterTest.class);
-  }
-
-  /*
-   * @see TestCase#setUp()
-   */
-  protected void setUp() throws Exception
-  {
-    filter = new ContentFilter();
-  }
-
-  /*
-   * @see TestCase#tearDown()
-   */
-  protected void tearDown() throws Exception
-  {
-    super.tearDown();
-  }
-
-  public void testSetInvalidPatterns()
-  {
-    try
-    {
-      filter.setPatterns("$*[");
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(ContentFilterTest.class);
     }
-    catch (PatternSyntaxException e)
-    {
-      System.out.println(e.getMessage());
-      System.out.println(e.getPattern());
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        filter = new ContentFilter();
     }
-  }
-  public void testSetContentMask()
-  {
-    assertFalse(filter.isMaskingContent());
-    filter.setMask("dummy");
-    assertTrue(filter.isMaskingContent());
-    filter.clearMask();
-    assertFalse(filter.isMaskingContent());
-  }
 
-  public void testFilterWithEmptyMessage()
-  {
-    Message message = new Message();
-    boolean matched = filter.filter(message);
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-    // no matches should be found
-    assertFalse(matched);
+    public void testSetInvalidPatterns() {
+        try {
+            filter.setPatterns("$*[");
+            fail("expected PatternSyntaxException");
+        } catch (PatternSyntaxException e) {
+        }
+    }
 
-    // message should not have changed
-    assertEquals(new Message().toXML(), message.toXML());
-  }
+    public void testSetContentMask() {
+        assertFalse(filter.isMaskingContent());
+        filter.setMask("dummy");
+        assertTrue(filter.isMaskingContent());
+        filter.clearMask();
+        assertFalse(filter.isMaskingContent());
+    }
 
-  public void testFilterMessageSubject()
-  {
-    // filter on the word fox
-    filter.setPatterns("fox");
+    public void testFilterWithEmptyMessage() {
+        Message message = new Message();
+        boolean matched = filter.filter(message);
 
-    // test message
-    Message message = new Message();
-    message.setSubject("the quick brown fox jumped over the lazy dog");
-    boolean matched = filter.filter(message);
+        // no matches should be found
+        assertFalse(matched);
 
-    // matches should be found
-    assertTrue(matched);
+        // message should not have changed
+        assertEquals(new Message().toXML(), message.toXML());
+    }
 
-    // content has not changed as there is no content mask
-    assertEquals("the quick brown fox jumped over the lazy dog", message
-        .getSubject());
-    assertNull(message.getBody());
-  }
+    public void testFilterMessageSubject() {
+        // filter on the word fox
+        filter.setPatterns("fox");
 
-  public void testFilterMessageSubjectWithMask()
-  {
+        // test message
+        Message message = new Message();
+        message.setSubject("the quick brown fox jumped over the lazy dog");
+        boolean matched = filter.filter(message);
 
-    // filter on the word fox
-    filter.setPatterns("fox");
-    
-    //set a content mask 
-    filter.setMask("**");
+        // matches should be found
+        assertTrue(matched);
 
-    // test message
-    Message message = new Message();
-    message.setSubject("the quick brown fox jumped over the lazy dog");
-    boolean matched = filter.filter(message);
+        // content has not changed as there is no content mask
+        assertEquals("the quick brown fox jumped over the lazy dog", message
+                .getSubject());
+        assertNull(message.getBody());
+    }
 
-    // matches should be found
-    assertTrue(matched);
+    public void testFilterMessageSubjectWithMask() {
 
-    // content has changed
-    assertEquals("the quick brown ** jumped over the lazy dog", message
-        .getSubject());
-    assertNull(message.getBody());
+        // filter on the word fox
+        filter.setPatterns("fox");
 
-  }
+        // set a content mask
+        filter.setMask("**");
 
-  public void testFilterMessageBody()
-  {
+        // test message
+        Message message = new Message();
+        message.setSubject("the quick brown fox jumped over the lazy dog");
+        boolean matched = filter.filter(message);
 
-    // filter on the word fox
-    filter.setPatterns("fox");
+        // matches should be found
+        assertTrue(matched);
 
-    // test message
-    Message message = new Message();
-    message.setBody("the quick brown fox jumped over the lazy dog");
-    boolean matched = filter.filter(message);
+        // content has changed
+        assertEquals("the quick brown ** jumped over the lazy dog", message
+                .getSubject());
+        assertNull(message.getBody());
 
-    // matches should be found
-    assertTrue(matched);
+    }
 
-    // content has not changed as there is no content mask
-    assertEquals("the quick brown fox jumped over the lazy dog", message
-        .getBody());
-    assertNull(message.getSubject());
-  }
+    public void testFilterMessageBody() {
 
-  public void testFilterMessageBodyWithMask()
-  {
+        // filter on the word fox
+        filter.setPatterns("fox");
 
-    // filter on the word "fox" and "dog"
-    filter.setPatterns("fox,dog");
-    filter.setMask("**");
+        // test message
+        Message message = new Message();
+        message.setBody("the quick brown fox jumped over the lazy dog");
+        boolean matched = filter.filter(message);
 
-    // test message
-    Message message = new Message();
-    message.setBody("the quick brown fox jumped over the lazy dog");
-    boolean matched = filter.filter(message);
+        // matches should be found
+        assertTrue(matched);
 
-    // matches should not be found
-    assertTrue(matched);
+        // content has not changed as there is no content mask
+        assertEquals("the quick brown fox jumped over the lazy dog", message
+                .getBody());
+        assertNull(message.getSubject());
+    }
 
-    // content has changed
-    assertEquals("the quick brown ** jumped over the lazy **", message
-        .getBody());
-    assertNull(message.getSubject());
+    public void testFilterMessageBodyWithMask() {
 
-  }
+        // filter on the word "fox" and "dog"
+        filter.setPatterns("fox,dog");
+        filter.setMask("**");
+
+        // test message
+        Message message = new Message();
+        message.setBody("the quick brown fox jumped over the lazy dog");
+        boolean matched = filter.filter(message);
+
+        // matches should not be found
+        assertTrue(matched);
+
+        // content has changed
+        assertEquals("the quick brown ** jumped over the lazy **", message
+                .getBody());
+        assertNull(message.getSubject());
+
+    }
+
+    public void testFilterWholeWords() {
+        filter.setPatterns("at"); //match every instance of "at" in string
+        filter.setMask("**");
+
+        Message message = new Message();
+        message.setBody("At noon the fat cats ate lunch at Rizzos");
+        boolean matched = filter.filter(message);        
+        assertTrue(matched);
+        assertEquals("At noon the f** c**s **e lunch ** Rizzos", message.getBody());
+        
+        filter.setPatterns("(?i)\\bat\\b"); //match only whole word instances of "at" ignoring case 
+        message.setBody("At noon the fat cats ate lunch at Rizzos");
+        matched = filter.filter(message);        
+        assertTrue(matched);
+        System.out.println(message.getBody());
+        assertEquals("** noon the fat cats ate lunch ** Rizzos", message.getBody());
+    }
 }

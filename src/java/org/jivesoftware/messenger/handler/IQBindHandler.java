a03079b076a12b7f16a4fb88cea13cc7dad2b70a
Gaston Dombiak <gaston@jivesoftware.com>
2005-09-05 19:35:04 +0000

    Fixed NPE when doing anonymous login.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@2778 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/handler/IQBindHandler.java
+++ src/java/org/jivesoftware/messenger/handler/IQBindHandler.java
@@ -66,7 +66,7 @@ public class IQBindHandler extends IQHandler {
         Element child = reply.setChildElement("bind", "urn:ietf:params:xml:ns:xmpp-bind");
         // Check if the client specified a desired resource
         String resource = packet.getChildElement().elementTextTrim("resource");
-        if (resource == null && resource.length() == 0) {
+        if (resource == null || resource.length() == 0) {
             // None was defined so use the random generated resource
             resource = session.getAddress().getResource();
         }

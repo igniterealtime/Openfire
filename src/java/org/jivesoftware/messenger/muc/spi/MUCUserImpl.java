a82796db68e152653763c6b08e0630285c64a083
Gaston Dombiak <gaston@jivesoftware.com>
2005-11-01 18:59:07 +0000

    Ignore IQ packets of type RESULT sent to the MUC service. JM-447
    git-svn-id: http://svn.igniterealtime.org/svn/repos/messenger/trunk@3017 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/messenger/muc/spi/MUCUserImpl.java
+++ src/java/org/jivesoftware/messenger/muc/spi/MUCUserImpl.java
@@ -284,8 +284,8 @@ public class MUCUserImpl implements MUCUser {
     }
 
     public void process(IQ packet) {
-        // Ignore IQs of type ERROR sent to a room 
-        if (IQ.Type.error == packet.getType()) {
+        // Ignore IQs of type ERROR or RESULT sent to a room
+        if (IQ.Type.error == packet.getType() || IQ.Type.result == packet.getType()) {
             return;
         }
         lastPacketTime = System.currentTimeMillis();

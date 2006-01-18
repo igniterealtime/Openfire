e0b0205d121fa1a69d21ab42bfb370d2c809986f
Gaston Dombiak <gaston@jivesoftware.com>
2006-01-18 22:05:47 +0000

    Fixed small memory leaking in occupantsByFullJID. JM-530
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@3323 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/wildfire/muc/MUCRoom.java
+++ src/java/org/jivesoftware/wildfire/muc/MUCRoom.java
@@ -145,7 +145,7 @@ public interface MUCRoom {
      * @return The user's role in the room
      * @throws UserNotFoundException If there is no user with the given nickname
      */
-    MUCRole getOccupantByFullJID(String jid) throws UserNotFoundException;
+    MUCRole getOccupantByFullJID(JID jid) throws UserNotFoundException;
 
     /**
      * Obtain the roles of all users in the chatroom.

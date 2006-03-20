692d9aafdf4e230510e0d348a684ec00f613d93d
Matt Tucker <matt@jivesoftware.com>
2006-03-20 15:58:01 +0000

    Added two new scripts. Both need to be tested.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@3597 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/database/upgrade/6/wildfire_oracle.sql
@@ -0,0 +1,15 @@
+REM // $Revision: 795 $
+REM // $Date: 2005-01-06 07:44:42 -0300 (Thu, 06 Jan 2005) $
+
+REM // Update the jiveVersion table to new definition.
+DROP TABLE jiveVersion;
+CREATE TABLE jiveVersion (
+  name     VARCHAR2(50)  NOT NULL,
+  version  INTEGER  NOT NULL,
+  CONSTRAINT jiveVersion_pk PRIMARY KEY (name)
+);
+INSERT INTO jiveVersion (name, version) VALUES ("wildfire", 6);
+
+REM // Make password column accept null, add encrypted password column.
+ALTER TABLE jiveUser MODIFY password VARCHAR2(32) NULL;
+ALTER TABLE jiveUser ADD COLUMN encryptedPassword VARCHAR(255);

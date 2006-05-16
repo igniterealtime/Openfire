a7b144ca6b71d141aa3c81be21b2d95951596cf1
Gaston Dombiak <gaston@jivesoftware.com>
2006-05-16 19:20:00 +0000

    Updated to latest HSQLDB version. JM-688
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@3907 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/bin/extra/embedded-db-viewer.bat
+++ src/bin/extra/embedded-db-viewer.bat
@@ -9,8 +9,8 @@ REM # on using the tool. The classpath includes JDBC drivers shipped with Wildfi
 REM # to work with the Transfer tool. You will need to add any other JDBC driver
 REM # that you'd like to use with the transfer tool to the classpath manually.
 
-SET CLASSPATH=../../lib/hsqldb.jar;../../lib/hsqldbutil.jar;../../lib/mysql.jar;../../lib/postgres.jar;../../lib/jtds.jar
+SET CLASSPATH=../../lib/hsqldb.jar;../../lib/mysql.jar;../../lib/postgres.jar;../../lib/jtds.jar
 
-echo Starting ebedded database viewer...
+echo Starting embedded database viewer...
 
 java -cp %CLASSPATH% org.hsqldb.util.DatabaseManagerSwing --rcfile embedded-db.rc --urlid embedded-db

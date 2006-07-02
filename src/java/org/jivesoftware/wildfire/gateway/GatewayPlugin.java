cce431374cbe1c520944ebde267d01c309a4e837
Matt Tucker <matt@jivesoftware.com>
2006-07-02 21:12:31 +0000

    Initial check-in.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk/src/plugins/gateway@4212 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/wildfire/gateway/GatewayPlugin.java
@@ -0,0 +1,33 @@
+/**
+ * $Revision$
+ * $Date$
+ *
+ * Copyright (C) 2006 Jive Software. All rights reserved.
+ *
+ * This software is published under the terms of the GNU Public License (GPL),
+ * a copy of which is included in this distribution.
+ */
+
+package org.jivesoftware.wildfire.gateway;
+
+import org.jivesoftware.wildfire.container.Plugin;
+import org.jivesoftware.wildfire.container.PluginManager;
+
+import java.io.File;
+
+/**
+ * IM Gateway plugin.
+ *
+ * @author
+ */
+public class GatewayPlugin implements Plugin {
+
+    public void initializePlugin(PluginManager manager, File pluginDirectory) {
+
+    }
+
+    public void destroyPlugin() {
+
+    }
+
+}

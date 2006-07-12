88bfdf89e843072573eec1b0d1fdc9183e509438
Matt Tucker <matt@jivesoftware.com>
2006-07-12 17:42:03 +0000

    Initial check-in.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk/src/plugins/gateway@4408 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/wildfire/gateway/GatewayType.java
@@ -0,0 +1,60 @@
+/**
+ * $Revision$
+ * $Date$
+ *
+ * Copyright (C) 2006 Jive Software. All rights reserved.
+ *
+ * This software is published under the terms of the GNU Public License (GPL),
+ * a copy of which is included in this distribution.
+ */
+
+package org.jivesoftware.wildfire.gateway;
+
+/**
+ * An enumeration for the valid gateway types, which encompasses proprietary IM networks
+ * as well as other IM protocols.
+ *
+ * @author Matt Tucker
+ */
+public enum GatewayType {
+
+    /**
+     * The AOL instant messaging service.
+     */
+    aim,
+
+    /**
+     * The ICQ instant messaging service.
+     */
+    icq,
+
+    /**
+     * The Yahoo instant messaging service.
+     */
+    yahoo,
+
+    /**
+     * The MSN instant messaging service.
+     */
+    msn,
+
+    /**
+     * A gateway to a SIP/SIMPLE servers.
+     */
+    sip,
+
+    /**
+     * A gateway to other XMPP servers.
+     */
+    xmpp,
+
+    /**
+     * A gateway to IRC servers.
+     */
+    irc,
+
+    /**
+     * A gateway to a service not covered by the other options..
+     */
+    other
+}

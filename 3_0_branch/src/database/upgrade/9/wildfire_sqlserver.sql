962d984f18a010071b4b6d3adb416332be79f439
Gaston Dombiak <gaston@jivesoftware.com>
2006-07-12 16:14:35 +0000

    Ported fix from TRUNK for JM-768.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/branches@4407 b35dd754-fafc-0310-a699-88a17e54d16e

--- 3_0_branch/src/database/upgrade/9/wildfire_sqlserver.sql
+++ 3_0_branch/src/database/upgrade/9/wildfire_sqlserver.sql
@@ -18,7 +18,7 @@ ALTER TABLE jiveOffline DROP CONSTRAINT jiveOffline_pk;
 ALTER TABLE jiveOffline ALTER COLUMN username NVARCHAR(64) NOT NULL;
 ALTER TABLE jiveOffline ADD CONSTRAINT jiveOffline_pk PRIMARY KEY (username, messageID);
 
-DROP INDEX jiveRoster.jiveRoster_username_idx
+DROP INDEX jiveRoster.jiveRoster_username_idx;
 ALTER TABLE jiveRoster ALTER COLUMN username NVARCHAR(64) NOT NULL;
 CREATE INDEX jiveRoster_username_idx ON jiveRoster (username ASC);
 
@@ -26,7 +26,7 @@ ALTER TABLE jiveVCard DROP CONSTRAINT JiveVCard_pk;
 ALTER TABLE jiveVCard ALTER COLUMN username NVARCHAR(64) NOT NULL;
 ALTER TABLE jiveVCard ADD CONSTRAINT JiveVCard_pk PRIMARY KEY (username);
 
-DROP INDEX jivePrivacyList.jivePList_default_idx
+DROP INDEX jivePrivacyList.jivePList_default_idx;
 ALTER TABLE jivePrivacyList DROP CONSTRAINT jivePrivacyList_pk;
 ALTER TABLE jivePrivacyList ALTER COLUMN username NVARCHAR(64) NOT NULL;
 ALTER TABLE jivePrivacyList ADD CONSTRAINT jivePrivacyList_pk PRIMARY KEY (username, name);

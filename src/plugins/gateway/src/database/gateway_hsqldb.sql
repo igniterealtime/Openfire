0e1cf8a278d75313293244b66c61c92a05317e45
Matt Tucker <matt@jivesoftware.com>
2006-07-15 21:19:17 +0000

    Fixed database schema.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@4500 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/gateway/src/database/gateway_hsqldb.sql
+++ src/plugins/gateway/src/database/gateway_hsqldb.sql
@@ -1,12 +1,12 @@
 CREATE TABLE gatewayRegistration (
-   regisrationID     BIGINT         NOT NULL,
+   registrationID     BIGINT         NOT NULL,
    jid               VARCHAR(1024)  NOT NULL,
    gatewayType       VARCHAR(15)    NOT NULL,
    username          VARCHAR(255)   NOT NULL,
    password          VARCHAR(255),
    registrationDate  BIGINT         NOT NULL,
    lastLogin         BIGINT,
-   CONSTRAINT gatewayReg_pk PRIMARY KEY (registrationID);
+   CONSTRAINT gatewayReg_pk PRIMARY KEY (registrationID)
 );
 CREATE INDEX gatewayReg_jid_idx ON gatewayRegistration (jid);
 CREATE INDEX gatewayReg_type_idx ON gatewayRegistration (gatewayType);

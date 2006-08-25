12d10395815de53fcffc6b6f054c5f933594cc26
Daniel Henninger <daniel@vorpalcloud.org>
2006-08-25 21:03:07 +0000

    [GATE-9] Fixed lack of visible MSN contacts at login.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@5064 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/protocols/msn/MSNListener.java
+++ src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/protocols/msn/MSNListener.java
@@ -18,6 +18,9 @@ import net.sf.jml.MsnSwitchboard;
 import net.sf.jml.MsnContact;
 import net.sf.jml.MsnMessenger;
 import net.sf.jml.message.MsnInstantMessage;
+import net.sf.jml.message.MsnControlMessage;
+import net.sf.jml.message.MsnDatacastMessage;
+import net.sf.jml.message.MsnUnknownMessage;
 
 import java.util.Date;
 
@@ -72,24 +75,48 @@ public class MSNListener extends MsnAdapter {
     }
 
     /**
+     * Handles incoming control messages from MSN.
+     */
+    public void controlMessageReceived(MsnSwitchboard switchboard, MsnControlMessage message, MsnContact friend) {
+        Log.debug("MSN: Received control msg to " + switchboard + " from " + friend + ": " + message);
+    }
+
+    /**
+     * Handles incoming datacast messages from MSN.
+     */
+    public void datacastMessageReceived(MsnSwitchboard switchboard, MsnDatacastMessage message, MsnContact friend) {
+        Log.debug("MSN: Received database msn to " + switchboard + " from " + friend + ": " + message);
+    }
+
+    /**
+     * Handles incoming unknown messages from MSN.
+     */
+    public void unknownMessageReceived(MsnSwitchboard switchboard, MsnUnknownMessage message, MsnContact friend) {
+        Log.debug("MSN: Received database msn to " + switchboard + " from " + friend + ": " + message);
+    }
+
+    /**
      * The user's login has completed and was accepted.
      */
     public void loginCompleted(MsnMessenger messenger) {
         Log.debug("MSN login completed");
-        Presence p = new Presence();
-        p.setTo(msnSession.getJID());
-        p.setFrom(msnSession.getTransport().getJID());
-        msnSession.getTransport().sendPacket(p);
 
         msnSession.getRegistration().setLastLogin(new Date());
-
         msnSession.setLoginStatus(true);
+    }
+
+    /**
+     * Contact list initialization has completed.
+     */
+    public void contactListInitCompleted(MsnMessenger messenger) {
+        Log.debug("Contact list init completed.");
     }
 
     /**
      * Contact list has been synced.
      */
     public void contactListSyncCompleted(MsnMessenger messenger) {
+        Log.debug("Contact list sync completed.");
         for (MsnContact msnContact : messenger.getContactList().getContacts()) {
             Log.debug("Got contact "+msnContact);
             msnSession.storeFriend(msnContact);
@@ -101,12 +128,32 @@ public class MSNListener extends MsnAdapter {
      * A friend for this user has changed status.
      */
     public void contactStatusChanged(MsnMessenger messenger, MsnContact friend) {
+        Log.debug("Got MSN status "+friend);
         Presence p = new Presence();
         p.setTo(msnSession.getJID());
         p.setFrom(msnSession.getTransport().convertIDToJID(friend.getEmail().toString()));
         ((MSNTransport)msnSession.getTransport()).setUpPresencePacket(p, friend.getStatus());
         msnSession.getTransport().sendPacket(p);
         msnSession.storeFriend(friend);
+    }
+
+    /**
+     * Owner status has changed.
+     */
+    public void ownerStatusChanged(MsnMessenger messenger) {
+        Log.debug("Owner status has changed: " + messenger);
+        Presence p = new Presence();
+        p.setTo(msnSession.getJID());
+        p.setFrom(msnSession.getTransport().getJID());
+        ((MSNTransport)msnSession.getTransport()).setUpPresencePacket(p, messenger.getOwner().getStatus());
+        msnSession.getTransport().sendPacket(p);
+    }
+
+    /**
+     * Catches MSN exceptions.
+     */
+    public void exceptionCaught(MsnMessenger messenger, Throwable throwable) {
+        Log.debug("Caught MSN exception: "+messenger+":"+throwable.toString());
     }
 
 }

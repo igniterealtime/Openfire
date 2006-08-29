5ad49feea0da188ba912d46c04a96de72d613bab
Gaston Dombiak <gaston@jivesoftware.com>
2006-08-29 19:21:25 +0000

    Minor fix.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@5087 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/wildfire/commands/admin/GetListActiveUsers.java
+++ src/java/org/jivesoftware/wildfire/commands/admin/GetListActiveUsers.java
@@ -58,7 +58,7 @@ public class GetListActiveUsers extends AdHocCommand {
     public void execute(SessionData data, Element command) {
         String max_items = data.getData().get("max_items").get(0);
         int maxItems = -1;
-        if (max_items != null && "none".equals(maxItems)) {
+        if (max_items != null && "none".equals(max_items)) {
             try {
                 maxItems = Integer.parseInt(max_items);
             }

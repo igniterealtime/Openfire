c9d04464f3f924ac2f37b9f8f5e833cc19d38001
Daniel Henninger <daniel@vorpalcloud.org>
2006-08-31 19:57:10 +0000

    [GATE-16] Added timer to routinely check for orphaned transport sessions.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@5136 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/TransportSessionManager.java
+++ src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/TransportSessionManager.java
@@ -13,6 +13,7 @@ package org.jivesoftware.wildfire.gateway;
 import java.util.*;
 
 import org.jivesoftware.util.NotFoundException;
+import org.jivesoftware.wildfire.SessionManager;
 import org.xmpp.packet.JID;
 
 /**
@@ -29,6 +30,43 @@ public class TransportSessionManager {
      * Container for all active sessions.
      */
     private Map<JID,TransportSession> activeSessions = new HashMap<JID,TransportSession>();
+
+    /**
+     * Timer to check for orphaned sessions.
+     */
+    private Timer timer = new Timer();
+
+    /**
+     * Interval at which sessions are reaped.
+     */
+    private int reaperInterval = 300000; // 5 minutes
+
+    /**
+     * The actual repear task.
+     */
+    @SuppressWarnings({"FieldCanBeLocal"}) private SessionReaper sessionReaper;
+
+    /**
+     * The transport we are associated with.
+     */
+    BaseTransport transport;
+
+    /**
+     * Creates the transport session manager instance and initializes.
+     */
+    TransportSessionManager(BaseTransport transport) {
+        this.transport = transport;
+        sessionReaper = new SessionReaper();
+        timer.schedule(sessionReaper, reaperInterval, reaperInterval);
+    }
+
+    /**
+     * Shuts down the session manager.
+     */
+    public void shutdown() {
+        sessionReaper.cancel();
+        timer.cancel();
+    }
 
     /**
      * Retrieve the session instance for a given JID.
@@ -79,6 +117,30 @@ public class TransportSessionManager {
      */
     public Collection<TransportSession> getSessions() {
         return activeSessions.values();
+    }
+
+    /**
+     * Bury any transport sessions that no longer have an associated xmpp session.
+     */
+    private class SessionReaper extends TimerTask {
+        /**
+         * Kill any session that has been orphaned.
+         */
+        public void run() {
+            cleanupOrphanedSessions();
+        }
+    }
+
+    /**
+     * Compares active xmpp sessions with active transport sessions and buries the orphaned.
+     */
+    private void cleanupOrphanedSessions() {
+        SessionManager sessionManager = SessionManager.getInstance();
+        for (TransportSession session : getSessions()) {
+            if (sessionManager.getSessionCount(session.getJID().getNode()) == 0) {
+                transport.registrationLoggedOut(session);
+            }
+        }
     }
 
 }

ac23927c2050746151b4159b5407e0b51fb96501
Daniel Henninger <daniel@vorpalcloud.org>
2006-09-11 18:48:27 +0000

    Adding some stubs.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk/src/plugins/gateway@5338 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/wildfire/gateway/PseudoRosterManager.java
@@ -0,0 +1,18 @@
+/**
+ * $Revision$
+ * $Date$
+ *
+ * Copyright (C) 2006 Jive Software. All rights reserved.
+ *
+ * This software is published under the terms of the GNU Public License (GPL),
+ * a copy of which is included in this distribution.
+ */
+
+package org.jivesoftware.wildfire.gateway;
+
+/**
+ * @author Daniel Henninger
+ */
+public class PseudoRosterManager {
+
+}

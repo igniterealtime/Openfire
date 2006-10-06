9e284770e597786973634a2e163b788614a85896
Gaston Dombiak <gaston@jivesoftware.com>
2006-10-06 07:37:08 +0000

    Fixed dummy copy and paste error. :(
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@5627 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/admin/LdapUserProfile.java
+++ src/java/org/jivesoftware/admin/LdapUserProfile.java
@@ -502,15 +502,15 @@ public class LdapUserProfile {
             }
             element = vCard.element("FN");
             if (element != null) {
-                fullName = vCard.getTextTrim();
+                fullName = element.getTextTrim();
             }
             element = vCard.element("NICKNAME");
             if (element != null) {
-                nickname = vCard.getTextTrim();
+                nickname = element.getTextTrim();
             }
             element = vCard.element("BDAY");
             if (element != null) {
-                birthday = vCard.getTextTrim();
+                birthday = element.getTextTrim();
             }
             // TODO add rest of fields
         }

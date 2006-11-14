6cb2a67979e0506436988afae3e22dc4b45b591e
Daniel Henninger <daniel@vorpalcloud.org>
2006-11-14 03:24:13 +0000

    Updated joscar and swapped some things around for AIM/ICQ away support.  Still not fixed though.  (weird)
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk/src/plugins/gateway@6100 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/wildfire/gateway/protocols/oscar/OSCARSession.java
+++ src/java/org/jivesoftware/wildfire/gateway/protocols/oscar/OSCARSession.java
@@ -472,7 +472,7 @@ public class OSCARSession extends TransportSession {
             if (verboseStatus != null) {
                 awayMsg = verboseStatus;
             }
-            request(new SetInfoCmd(new InfoData(awayMsg, null, MY_CAPS, null)));
+            request(new SetInfoCmd(new InfoData(null, awayMsg, MY_CAPS, null)));
             Presence p = new Presence();
             p.setShow(Presence.Show.away);
             p.setTo(getJID());
@@ -480,7 +480,7 @@ public class OSCARSession extends TransportSession {
             getTransport().sendPacket(p);
         }
         else {
-            request(new SetInfoCmd(new InfoData(InfoData.NOT_AWAY, null, MY_CAPS, null)));
+            request(new SetInfoCmd(new InfoData(null, InfoData.NOT_AWAY, MY_CAPS, null)));
             Presence p = new Presence();
             p.setTo(getJID());
             p.setFrom(getTransport().getJID());

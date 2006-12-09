36bf2bdc5916827be47a4eacb808c567c0606291
Daniel Henninger <daniel@vorpalcloud.org>
2006-12-09 06:26:09 +0000

    Working on options update functionality.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@6349 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/web/TransportInstanceManager.java
+++ src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/web/TransportInstanceManager.java
@@ -13,6 +13,12 @@ package org.jivesoftware.wildfire.gateway.web;
 import org.jivesoftware.wildfire.container.PluginManager;
 import org.jivesoftware.wildfire.XMPPServer;
 import org.jivesoftware.wildfire.gateway.GatewayPlugin;
+import org.jivesoftware.wildfire.gateway.TransportType;
+import org.jivesoftware.util.Log;
+import org.jivesoftware.util.JiveGlobals;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.Attribute;
 
 /**
  * Transport Instance Manager
@@ -40,6 +46,85 @@ public class TransportInstanceManager {
         else {
             plugin.disableService(transportName);
             return false;
+        }
+    }
+
+    /**
+     * Saves settings from options screen.
+     *
+     * @param transportName Name of the transport to have it's options saved (type of transport)
+     * @param options Options passed from options form.
+     */
+    public void saveSettings(String transportName, TransportOptions options) {
+        PluginManager pluginManager = XMPPServer.getInstance().getPluginManager();
+        GatewayPlugin plugin = (GatewayPlugin)pluginManager.getPlugin("gateway");
+        Document optConfig = plugin.getOptionsConfig(TransportType.valueOf(transportName));
+
+        Element leftPanel = optConfig.getRootElement().element("leftpanel");
+        if (leftPanel != null && leftPanel.nodeCount() > 0) {
+            for (Object nodeObj : leftPanel.elements("item")) {
+                Element node = (Element)nodeObj;
+                saveOptionSetting(node, options);
+            }
+        }
+
+        Element rightPanel = optConfig.getRootElement().element("rightpanel");
+        if (rightPanel != null && rightPanel.nodeCount() > 0) {
+            for (Object nodeObj : rightPanel.elements("item")) {
+                Element node = (Element)nodeObj;
+                saveOptionSetting(node, options);
+            }
+        }
+    }
+
+    /**
+     * Helper function designed to handle saving option types.
+     *
+     * @param node Node describing the configuration item.
+     * @param options Options passed from form.
+     */
+    private void saveOptionSetting(Element node, TransportOptions options) {
+        Attribute type = node.attribute("type");
+        if (type.getText().equals("text")) {
+            // Required fields
+            Attribute desc = node.attribute("desc");
+            Attribute var = node.attribute("var");
+            Attribute sysprop = node.attribute("sysprop");
+
+            if (desc == null || var == null || sysprop == null) {
+                Log.error("Missing variable from options config.");
+                return;
+            }
+
+            // Process any variables that we are setting.
+            if (var.getText().equals("host")) {
+                JiveGlobals.setProperty(sysprop.getText(), options.host);
+            }
+            else if (var.getText().equals("port")) {
+                JiveGlobals.setProperty(sysprop.getText(), options.port);
+            }
+            else if (var.getText().equals("encoding")) {
+                JiveGlobals.setProperty(sysprop.getText(), options.encoding);
+            }
+        }
+        else if (type.getText().equals("toggle")) {
+            // Required fields
+            Attribute desc = node.attribute("desc");
+            Attribute var = node.attribute("var");
+            Attribute sysprop = node.attribute("sysprop");
+
+            if (desc == null || var == null || sysprop == null) {
+                Log.error("Missing variable from options config.");
+                return;
+            }
+
+            // Process any variables that we are setting.
+            // None yet.
+
+            for (Object itemObj : node.elements("item")) {
+                Element item = (Element)itemObj;
+                saveOptionSetting(item, options);
+            }
         }
     }
     

f7922a532de95005b593065a351bb79126b01afa
Gaston Dombiak <gaston@jivesoftware.com>
2006-12-11 18:39:41 +0000

    Fixed some NPEs.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@6359 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/wildfire/ldap/LdapUserProvider.java
+++ src/java/org/jivesoftware/wildfire/ldap/LdapUserProvider.java
@@ -94,12 +94,12 @@ public class LdapUserProvider implements UserProvider {
             }
             Date creationDate = new Date();
             Attribute creationDateField = attrs.get("createTimestamp");
-            if (creationDateField != null) {
-                creationDate = parseLDAPDate((String)creationDateField.get());
+            if (creationDateField != null && "".equals(((String) creationDateField.get()).trim())) {
+                creationDate = parseLDAPDate((String) creationDateField.get());
             }
             Date modificationDate = new Date();
             Attribute modificationDateField = attrs.get("modifyTimestamp");
-            if (modificationDateField != null) {
+            if (modificationDateField != null && "".equals(((String) modificationDateField.get()).trim())) {
                 modificationDate = parseLDAPDate((String)modificationDateField.get());
             }
             // Escape the username so that it can be used as a JID.

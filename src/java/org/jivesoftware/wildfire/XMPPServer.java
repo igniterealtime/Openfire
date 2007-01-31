16ee51eb591282cc1d16ecb94aed0e53e8b63b41
Matt Tucker <matt@jivesoftware.com>
2007-01-31 22:39:14 +0000

    Javadoc.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@6897 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/wildfire/XMPPServer.java
+++ src/java/org/jivesoftware/wildfire/XMPPServer.java
@@ -349,8 +349,9 @@ public class XMPPServer {
                             // Otherwise, the page that requested the setup finish won't
                             // render properly!
                             Thread.sleep(1000);
-                            ((AdminConsolePlugin) pluginManager.getPlugin("admin")).shutdown();
-                            ((AdminConsolePlugin) pluginManager.getPlugin("admin")).startup();
+                            ((AdminConsolePlugin) pluginManager.getPlugin("admin")).restart();
+//                            ((AdminConsolePlugin) pluginManager.getPlugin("admin")).shutdown();
+//                            ((AdminConsolePlugin) pluginManager.getPlugin("admin")).startup();
                         }
 
                         verifyDataSource();

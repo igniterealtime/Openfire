4bd6caa372999a85a17781b2e3dd1446de0092ba
Daniel Henninger <daniel@vorpalcloud.org>
2007-03-02 03:55:46 +0000

    [GATE-157] Added ability to have yahoo usernames with .'s in them.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@7348 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/protocols/yahoo/YahooTransport.java
+++ src/plugins/gateway/src/java/org/jivesoftware/wildfire/gateway/protocols/yahoo/YahooTransport.java
@@ -69,7 +69,7 @@ public class YahooTransport extends BaseTransport {
      * @see org.jivesoftware.wildfire.gateway.BaseTransport#isUsernameValid(String)
      */
     public Boolean isUsernameValid(String username) {
-        return username.matches("\\w+");
+        return username.matches("[\\w\\.]+");
     }
 
     /**

abf8d3fdb81c97d2bbe6e8bdbd9ea7f10df629b4
Gaston Dombiak <gaston@jivesoftware.com>
2007-03-08 19:23:46 +0000

    IQ sent to full JID must only be sent to that resource and not a default resource. JM-994
    git-svn-id: http://svn.igniterealtime.org/svn/repos/wildfire/trunk@7410 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/wildfire/IQRouter.java
+++ src/java/org/jivesoftware/wildfire/IQRouter.java
@@ -309,7 +309,7 @@ public class IQRouter extends BasicModule {
                 // So if the target address belongs to this server then use the sessionManager
                 // instead of the routingTable since unavailable clients won't have a route to them
                 if (XMPPServer.getInstance().isLocal(recipientJID)) {
-                    ClientSession session = sessionManager.getBestRoute(recipientJID);
+                    ClientSession session = sessionManager.getSession(recipientJID);
                     if (session != null) {
                         if (session.canProcess(packet)) {
                             session.process(packet);

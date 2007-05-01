5922ec45eb2a8e008a9f96619463c958f76295ec
Gaston Dombiak <gaston@jivesoftware.com>
2007-05-01 21:45:23 +0000

    Initial version. JM-1046
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@8141 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/openfire/privacy/PrivacyListEventListener.java
@@ -0,0 +1,43 @@
+/**
+ * $RCSfile$
+ * $Revision: $
+ * $Date: $
+ *
+ * Copyright (C) 2004-2007 Jive Software. All rights reserved.
+ *
+ * This software is published under the terms of the GNU Public License (GPL),
+ * a copy of which is included in this distribution.
+ */
+
+package org.jivesoftware.openfire.privacy;
+
+/**
+ /**
+  * Interface to listen for privacy list events. Use the
+  * {@link PrivacyListManager#addListener(PrivacyListEventListener)}
+  * method to register for events.
+  *
+ */
+public interface PrivacyListEventListener {
+
+    /**
+     * A privacy list was created.
+     *
+     * @param list the privacy list.
+     */
+    public void privacyListCreated(PrivacyList list);
+
+    /**
+     * A privacy list is being deleted.
+     *
+     * @param listName name of the the privacy list that has been deleted.
+     */
+    public void privacyListDeleting(String listName);
+
+    /**
+     * Properties of the privacy list were changed.
+     *
+     * @param list the privacy list.
+     */
+    public void privacyListModified(PrivacyList list);
+}

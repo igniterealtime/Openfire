c4f0c023515fca7c001ee79fcc20bf17b01f0571
Gaston Dombiak <gaston@jivesoftware.com>
2007-06-08 23:04:28 +0000

    Do not print warn message with returned IQ error packets.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@8524 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/IQRouter.java
+++ src/java/org/jivesoftware/openfire/IQRouter.java
@@ -255,7 +255,7 @@ public class IQRouter extends BasicModule {
                     namespace = childElement.getNamespaceURI();
                 }
                 if (namespace == null) {
-                    if (packet.getType() != IQ.Type.result) {
+                    if (packet.getType() != IQ.Type.result && packet.getType() != IQ.Type.error) {
                         // Do nothing. We can't handle queries outside of a valid namespace
                         Log.warn("Unknown packet " + packet);
                     }

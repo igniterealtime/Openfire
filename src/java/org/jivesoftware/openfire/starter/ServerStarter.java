bab6f4cf3374a1800e6db3566a545a5c5d314308
Jay Kline <jay@slushpupie.com>
2007-06-20 14:04:31 +0000

    Fixes JM-1077 (location of admin lib dir)
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@8582 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/starter/ServerStarter.java
+++ src/java/org/jivesoftware/openfire/starter/ServerStarter.java
@@ -44,6 +44,7 @@ public class ServerStarter {
      * Default to this location if one has not been specified
      */
     private static final String DEFAULT_LIB_DIR = "../lib";
+    private static final String DEFAULT_ADMIN_LIB_DIR = "../plugins/admin/webapp/WEB-INF/lib";
 
     public static void main(String [] args) {
         new ServerStarter().start();
@@ -80,9 +81,20 @@ public class ServerStarter {
             // Unpack any pack files.
             unpackArchives(libDir, true);
 
-            File adminLibDir = new File("../plugins/admin/webapp/WEB-INF/lib");
+            String adminLibDirString = System.getProperty("openfireHome");
+            if (adminLibDirString == null) {
+                adminLibDirString = DEFAULT_ADMIN_LIB_DIR;
+            }
+            else {
+                adminLibDirString = adminLibDirString+"/plugins/admin/webapp/WEB-INF/lib";
+            }
+            File adminLibDir = new File(adminLibDirString);
             if (adminLibDir.exists()) {
                 unpackArchives(adminLibDir, false);
+            }
+            else {
+                Log.warn("Admin Lib Directory " + adminLibDirString +
+                    " does not exist. Web admin console may not work.");
             }
 
             ClassLoader loader = new JiveClassLoader(parent, libDir);

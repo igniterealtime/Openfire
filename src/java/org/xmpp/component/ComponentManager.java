3d8eb1b409f3bb1c3e1ced235e97781bfcf8c5d0
Alex Wenckus <awenckus@gmail.com>
2007-06-28 20:48:31 +0000

    Removed convinence
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@8662 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/xmpp/component/ComponentManager.java
+++ src/java/org/xmpp/component/ComponentManager.java
@@ -8,7 +8,6 @@
 package org.xmpp.component;
 
 import org.jivesoftware.openfire.IQResultListener;
-import org.jivesoftware.openfire.component.ComponentLifecycle;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.Packet;
 
@@ -28,19 +27,14 @@ public interface ComponentManager {
      *
      * @param subdomain the subdomain of the component's address.
      * @param component the component.
-     * @throws ComponentException
      */
     public void addComponent(String subdomain, Component component) throws ComponentException;
-
-    ComponentLifecycle addComponent(String subdomain, Component component, String jiveProperty)
-            throws ComponentException;
 
     /**
      * Removes a component. The {@link Component#shutdown} method will be called on the
      * component.
      *
      * @param subdomain the subdomain of the component's address.
-     * @throws ComponentException
      */
     public void removeComponent(String subdomain) throws ComponentException;
 
@@ -53,7 +47,6 @@ public interface ComponentManager {
      *
      * @param component the component sending the packet.
      * @param packet the packet to send.
-     * @throws ComponentException
      */
     public void sendPacket(Component component, Packet packet) throws ComponentException;
 
@@ -73,7 +66,6 @@ public interface ComponentManager {
      * @param timeout the number of milliseconds to wait before returning an IQ error.
      * @return the answer sent by the server. The answer could be an IQ of type result or
      *         error.
-     * @throws ComponentException
      */
     public IQ query(Component component, IQ packet, int timeout) throws ComponentException;
 
@@ -84,7 +76,6 @@ public interface ComponentManager {
      * @param component the component sending the packet.
      * @param packet the IQ packet to send.
      * @param listener the listener that will be invoked when an answer is received.
-     * @throws ComponentException
      */
     public void query(Component component, IQ packet, IQResultListener listener) throws ComponentException;
 

bbf7256700a1b1151fab3941183615de8a19c73a
Gaston Dombiak <gaston@jivesoftware.com>
2007-10-29 08:11:32 +0000

    Updated for 3.4.0.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@9372 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/XMPPServer.java
+++ src/java/org/jivesoftware/openfire/XMPPServer.java
@@ -313,7 +313,7 @@ public class XMPPServer {
 
         name = JiveGlobals.getProperty("xmpp.domain", "127.0.0.1").toLowerCase();
 
-        version = new Version(3, 4, 0, Version.ReleaseStatus.Beta, 2);
+        version = new Version(3, 4, 0, Version.ReleaseStatus.Release, -1);
         if ("true".equals(JiveGlobals.getXMLProperty("setup"))) {
             setupMode = false;
         }

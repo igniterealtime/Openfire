7b9294994a7cd28682b793e6278b405572242f37
Daniel Henninger <daniel@vorpalcloud.org>
2007-11-12 15:04:22 +0000

    Added a little more robustness to installPlugin routine.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@9477 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/container/PluginManager.java
+++ src/java/org/jivesoftware/openfire/container/PluginManager.java
@@ -130,9 +130,18 @@ public class PluginManager {
      * @return true if the plugin was successfully installed or updated.
      */
     public boolean installPlugin(InputStream in, String pluginFilename) {
+        if (in == null || pluginFilename == null || pluginFilename.length() < 1) {
+            Log.error("Error installing plugin: Input stream was null or pluginFilename was null or had no length.");
+            return false;
+        }
         try {
             byte[] b = new byte[1024];
             int len;
+            // If pluginFilename is a path instead of a simple file name, we only want the file name
+            int index = pluginFilename.lastIndexOf(File.separator);
+            if (index != -1) {
+                pluginFilename = pluginFilename.substring(index+1);
+            }
             // Absolute path to the plugin file
             String absolutePath = pluginDirectory + File.separator + pluginFilename;
             // Save input stream contents to a temp file

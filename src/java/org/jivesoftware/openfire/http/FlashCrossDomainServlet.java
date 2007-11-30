b0beaebe0070c40c83c746d4e93997cd1640b0e9
Daniel Henninger <daniel@vorpalcloud.org>
2007-11-30 01:29:37 +0000

    [JM-1196] crossdomain.xml is now served via port 9090 as well.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@9533 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/http/FlashCrossDomainServlet.java
+++ src/java/org/jivesoftware/openfire/http/FlashCrossDomainServlet.java
@@ -10,6 +10,8 @@
  */
 package org.jivesoftware.openfire.http;
 
+import org.jivesoftware.openfire.XMPPServer;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,15 +26,22 @@ import java.io.IOException;
  */
 public class FlashCrossDomainServlet extends HttpServlet {
 
+    public static String CROSS_DOMAIN_TEXT = "<?xml version=\"1.0\"?>" +
+            "<!DOCTYPE cross-domain-policy SYSTEM \"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd\">" +
+            "<cross-domain-policy>" +
+            "<allow-access-from domain=\"*\" to-ports=\"";
+
+    public static String CROSS_DOMAIN_END_TEXT = "\" /></cross-domain-policy>";
+
     @Override
     protected void doGet(HttpServletRequest httpServletRequest,
                          HttpServletResponse response) throws
             ServletException, IOException {
         StringBuilder builder = new StringBuilder();
-        builder.append("<?xml version=\"1.0\"?>\n" +
-                "<!DOCTYPE cross-domain-policy " +
-                "SYSTEM \"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd\">" +
-                "<cross-domain-policy><allow-access-from domain=\"*\" /></cross-domain-policy>");
+        builder.append(CROSS_DOMAIN_TEXT +
+                XMPPServer.getInstance().getConnectionManager().getClientListenerPort() +
+                CROSS_DOMAIN_END_TEXT);
+        builder.append("\n");
         response.setContentType("text/xml");
         response.getOutputStream().write(builder.toString().getBytes());
     }

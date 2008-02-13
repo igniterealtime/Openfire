c0864e93872ad56e9771640ef3ffda9b6ad206a3
Gaston Dombiak <gaston@jivesoftware.com>
2008-02-13 18:44:20 +0000

    Fixed double byte char fix to accept \t heart beats.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@9897 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/nio/XMLLightweightParser.java
+++ src/java/org/jivesoftware/openfire/nio/XMLLightweightParser.java
@@ -156,8 +156,11 @@ class XMLLightweightParser {
 
         // Verify if the last received byte is an incomplete double byte character
         char lastChar = buf[readByte-1];
-        if (Character.isISOControl(lastChar) || lastChar >= 0xfff0) {
-            Log.debug("Waiting to get complete char: " + String.valueOf(buf));
+        //if (Character.isISOControl(lastChar) || lastChar >= 0xfff0) {
+        if (lastChar >= 0xfff0) {
+            if (Log.isDebugEnabled()) {
+                Log.debug("Waiting to get complete char: " + String.valueOf(buf));
+            }
             // Rewind the position one place so the last byte stays in the buffer
             // The missing byte should arrive in the next iteration. Once we have both
             // of bytes we will have the correct character

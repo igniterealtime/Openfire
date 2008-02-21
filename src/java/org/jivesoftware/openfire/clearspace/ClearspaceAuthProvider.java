94018157424ed8019b624e36ed669a99cc2148ea
Armando Jagucki <ajagucki@gmail.com>
2008-02-21 00:37:33 +0000

    Added support for a custom SASL mechanism called 'CLEARSPACE' for use with Clearspace's group chat authentication. This is handled by a new SaslServer called ClearspaceSaslServer.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@9957 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/clearspace/ClearspaceAuthProvider.java
+++ src/java/org/jivesoftware/openfire/clearspace/ClearspaceAuthProvider.java
@@ -13,6 +13,7 @@ import org.jivesoftware.openfire.auth.AuthProvider;
 import org.jivesoftware.openfire.auth.UnauthorizedException;
 import static org.jivesoftware.openfire.clearspace.ClearspaceManager.HttpType.GET;
 import org.jivesoftware.openfire.user.UserNotFoundException;
+import org.jivesoftware.openfire.net.SASLAuthentication;
 
 /**
  * @author Daniel Henninger
@@ -25,6 +26,9 @@ public class ClearspaceAuthProvider implements AuthProvider {
     public ClearspaceAuthProvider() {
         // gets the manager
         manager = ClearspaceManager.getInstance();
+
+        // Add SASL mechanism for use with Clearspace's group chat integration
+        SASLAuthentication.addSupportedMechanism("CLEARSPACE");
     }
 
     public boolean isPlainSupported() {

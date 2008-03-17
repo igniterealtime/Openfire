c0099b206db87dc9754a7bfbf56ca3510143d3fb
Daniel Henninger <daniel@vorpalcloud.org>
2008-03-17 20:51:37 +0000

    qdox appears to get mad about a lack of semicolon at the end of an enum.  k...
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@10074 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/muc/HistoryStrategy.java
+++ src/java/org/jivesoftware/openfire/muc/HistoryStrategy.java
@@ -239,7 +239,7 @@ public class HistoryStrategy {
      */
     public enum Type {
         defaulType, none, all, number
-    }
+    };
 
     /**
      * Obtain the strategy type from string name. See the Type enumeration name

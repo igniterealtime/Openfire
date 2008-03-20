b09d1fe63573a1cff0e2ad234a6002976aefe2a5
Daniel Henninger <daniel@vorpalcloud.org>
2008-03-20 18:21:05 +0000

    Ported changes from trunk.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/branches/openfire_3_5_0@10096 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/database/upgrade/14/openfire_mysql.sql
+++ src/database/upgrade/14/openfire_mysql.sql
@@ -1,4 +1,7 @@
+# jiveRoster: Change jid column to varchar
+ALTER TABLE jiveRoster ALTER COLUMN jid jid varchar(1024) not null;
+
 # jiveRoster: Add new index
-ALTER TABLE jiveRoster ADD INDEX jiveRoster_jid_idx (jid(1024));
+ALTER TABLE jiveRoster ADD INDEX jiveRoster_jid_idx (jid);
 
 UPDATE jiveVersion set version=14 where name = 'openfire';

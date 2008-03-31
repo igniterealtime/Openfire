abf11aa5dbce4a8eabff05442b0445ac8852fdf6
Daniel Henninger <daniel@vorpalcloud.org>
2008-03-31 15:41:37 +0000

    Bugfix for db2 upgrade script. Added comment for postgres.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@10153 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/database/upgrade/18/openfire_postgresql.sql
+++ src/database/upgrade/18/openfire_postgresql.sql
@@ -1,4 +1,4 @@
--- add isHidden column to mucService
+-- add isHidden column to mucService, with isHidden set to false by default
 ALTER TABLE mucService ADD COLUMN isHidden INTEGER NOT NULL DEFAULT 0;
 
 UPDATE jiveVersion set version=18 where name = 'openfire';

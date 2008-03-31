3db95b89ae91069be62dac59bfa0941972037041
Daniel Henninger <daniel@vorpalcloud.org>
2008-03-31 04:28:10 +0000

    Bugfix for postgres upgrade script.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@10149 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/database/upgrade/18/openfire_postgresql.sql
+++ src/database/upgrade/18/openfire_postgresql.sql
@@ -1,8 +1,4 @@
 -- add isHidden column to mucService
-ALTER TABLE mucService ADD COLUMN isHidden INTEGER NOT NULL;
-
--- set all current services to isHidden = false
-UPDATE mucService set isHidden = 0;
-
+ALTER TABLE mucService ADD COLUMN isHidden INTEGER NOT NULL DEFAULT 0;
 
 UPDATE jiveVersion set version=18 where name = 'openfire';

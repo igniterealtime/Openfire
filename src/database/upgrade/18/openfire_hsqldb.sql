742bbb519db66cfaaecc971b7887349d44f92dc1
Daniel Henninger <daniel@vorpalcloud.org>
2008-03-31 04:53:41 +0000

    Bugfix for hsql upgrade script.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@10151 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/database/upgrade/18/openfire_hsqldb.sql
+++ src/database/upgrade/18/openfire_hsqldb.sql
@@ -1,8 +1,7 @@
 // add isHidden column to mucService
-ALTER TABLE mucService ADD COLUMN isHidden INTEGER NOT NULL;
+ALTER TABLE mucService ADD COLUMN isHidden INTEGER;
 
 // set all current services to isHidden = false
 UPDATE mucService set isHidden = 0;
-
 
 UPDATE jiveVersion set version=18 where name = 'openfire';

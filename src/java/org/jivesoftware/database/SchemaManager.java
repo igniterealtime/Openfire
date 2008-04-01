1e4165bf8d8a467f9b60e20bef5d8f897fb1de91
Daniel Henninger <daniel@vorpalcloud.org>
2008-04-01 00:35:06 +0000

    Updated schema version.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@10159 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/database/SchemaManager.java
+++ src/java/org/jivesoftware/database/SchemaManager.java
@@ -44,7 +44,7 @@ public class SchemaManager {
     /**
      * Current Openfire database schema version.
      */
-    private static final int DATABASE_VERSION = 18;
+    private static final int DATABASE_VERSION = 19;
 
     /**
      * Creates a new Schema manager.

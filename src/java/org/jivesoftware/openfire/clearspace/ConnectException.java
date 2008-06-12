05e6ec4ecca00b9166404f7235ec0c4dbba2387a
Gaston Dombiak <gaston@jivesoftware.com>
2008-06-12 23:03:29 +0000

    Force that an exception is provided. We should always wrap an exception.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@10515 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/clearspace/ConnectException.java
+++ src/java/org/jivesoftware/openfire/clearspace/ConnectException.java
@@ -4,13 +4,6 @@ package org.jivesoftware.openfire.clearspace;
  * Thrown when an exception occurs connecting to CS.
  */
 public class ConnectException extends Exception {
-    public ConnectException() {
-        super();
-    }
-
-    public ConnectException(String s) {
-        super(s);
-    }
 
     public ConnectException(String s, Throwable throwable) {
         super(s, throwable);

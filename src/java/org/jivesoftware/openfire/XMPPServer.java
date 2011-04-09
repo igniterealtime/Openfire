d3a723d8009a718f1fcf0fb1d878cc725275e627
guus <guus@b35dd754-fafc-0310-a699-88a17e54d16e>
2011-04-09 11:37:18 +0000

    Making sure that builds up until the next release get a '3.7.1 ALPHA' version identifier.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@12232 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/XMPPServer.java
+++ src/java/org/jivesoftware/openfire/XMPPServer.java
@@ -355,7 +355,7 @@ public class XMPPServer {
             Log.warn("Unable to determine local hostname.", ex);
         }
 
-        version = new Version(3, 7, 0, Version.ReleaseStatus.Release, -1);
+        version = new Version(3, 7, 1, Version.ReleaseStatus.Alpha, -1);
         if ("true".equals(JiveGlobals.getXMLProperty("setup"))) {
             setupMode = false;
         }

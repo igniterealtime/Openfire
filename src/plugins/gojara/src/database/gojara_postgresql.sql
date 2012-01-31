bbccd3d49876736fb482c98d73cc7f9ce491b6c9
Holger Bergunde <holger.bergunde@b35dd754-fafc-0310-a699-88a17e54d16e>
2012-01-31 15:51:05 +0000

    OF-490 updating postgresql database create table for gojara. should work now!
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@12959 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/gojara/src/database/gojara_postgresql.sql
+++ src/plugins/gojara/src/database/gojara_postgresql.sql
@@ -1,13 +1,12 @@
-
-
 CREATE TABLE ofGojaraStatistics (
   logID 			bigserial NOT NULL,
   messageDate		bigint NOT NULL,
-  messageType 		tinytext NOT NULL,
+  messageType 		varchar(255) NOT NULL,
   fromJID 			varchar(255)  NOT NULL,
-  toJID 			varchar(255 NOT NULL,
-  component			varchar(255 NOT NULL,
+  toJID 			varchar(255) NOT NULL,
+  component			varchar(255) NOT NULL,
   PRIMARY KEY (logID)
 );
+
 
 INSERT INTO ofVersion (name, version) VALUES ('gojara', 0);

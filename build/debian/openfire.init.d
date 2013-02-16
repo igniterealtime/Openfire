3327730ba18a159de963e853c44ed4cbedfb8ef6
guus <guus@b35dd754-fafc-0310-a699-88a17e54d16e>
2013-02-16 14:21:35 +0000

    OF-614: The Debian startup script should check a number of well known locations for a JRE if JAVA_HOME is not set.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@13487 b35dd754-fafc-0310-a699-88a17e54d16e

--- build/debian/openfire.init.d
+++ build/debian/openfire.init.d
@@ -23,8 +23,14 @@
 # Attempt to locate JAVA_HOME, code borrowed from jabref package
 if [ -z $JAVA_HOME ]
 then
-	t=/usr/lib/jvm/java-1.5.0-sun && test -d $t && JAVA_HOME=$t
+	t=/usr/lib/jvm/default-java && test -d $t && JAVA_HOME=$t
+	t=/usr/lib/jvm/java-7-sun && test -d $t && JAVA_HOME=$t
 	t=/usr/lib/jvm/java-6-sun && test -d $t && JAVA_HOME=$t
+	t=/usr/lib/jvm/java-1.5.0-sun && test -d $t && JAVA_HOME=$t
+    t=/usr/lib/jvm/java-7-openjdk-amd64 && test -d $t && JAVA_HOME=$t
+    t=/usr/lib/jvm/java-7-openjdk && test -d $t && JAVA_HOME=$t
+    t=/usr/lib/jvm/java-6-openjdk-amd64 && test -d $t && JAVA_HOME=$t
+    t=/usr/lib/jvm/java-6-openjdk && test -d $t && JAVA_HOME=$t
 fi
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:${JAVA_HOME}/bin

7668d3875dbc30a37762413ea8400255758d4358
Leon Roy <leon@boxpeg.net>
2013-04-30 14:27:45 +0000

    OF-667 - Monitoring plugin bad SQL for upgrade
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@13626 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/monitoring/src/database/upgrade/1/monitoring_hsqldb.sql
+++ src/plugins/monitoring/src/database/upgrade/1/monitoring_hsqldb.sql
@@ -5,4 +5,4 @@ ALTER TABLE ofMessageArchive ADD COLUMN fromJIDResource VARCHAR(255) NULL;
 ALTER TABLE ofMessageArchive ADD COLUMN toJIDResource VARCHAR(255) NULL;
 
 -- Update database version
-UPDATE jiveVersion SET version = 1 WHERE name = 'monitoring';
+UPDATE ofVersion SET version = 1 WHERE name = 'monitoring';
--- src/plugins/monitoring/src/database/upgrade/1/monitoring_oracle.sql
+++ src/plugins/monitoring/src/database/upgrade/1/monitoring_oracle.sql
@@ -5,6 +5,6 @@ ALTER TABLE ofMessageArchive ADD fromJIDResource VARCHAR(255) NULL;
 ALTER TABLE ofMessageArchive ADD toJIDResource VARCHAR(255) NULL;
 
 -- Update database version
-UPDATE jiveVersion SET version = 1 WHERE name = 'monitoring';
+UPDATE ofVersion SET version = 1 WHERE name = 'monitoring';
 
 commit;

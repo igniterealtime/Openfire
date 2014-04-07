4f3e66f7ca68a0aa6da0e6a7cdacbcf2007f106b
Christian Schudt <christian.schudt@gmx.de>
2014-04-07 21:52:28 +0200

    testing git + some JavaDoc

--- src/java/org/jivesoftware/openfire/carbons/Received.java
+++ src/java/org/jivesoftware/openfire/carbons/Received.java
@@ -4,9 +4,12 @@ import org.jivesoftware.openfire.forward.Forwarded;
 import org.xmpp.packet.PacketExtension;
 
 /**
+ * The implementation of the {@code <received xmlns="urn:xmpp:carbons:2"/>} extension.
+ * It indicates, that a message has been received by another resource.
+ *
  * @author Christian Schudt
  */
-public class Received extends PacketExtension {
+public final class Received extends PacketExtension {
     public Received(Forwarded forwarded) {
         super("received", "urn:xmpp:carbons:2");
         element.add(forwarded.getElement());
--- src/java/org/jivesoftware/openfire/carbons/Sent.java
+++ src/java/org/jivesoftware/openfire/carbons/Sent.java
@@ -4,9 +4,12 @@ import org.jivesoftware.openfire.forward.Forwarded;
 import org.xmpp.packet.PacketExtension;
 
 /**
+ * The implementation of the {@code <sent xmlns="urn:xmpp:carbons:2"/>} extension.
+ * It indicates, that a message has been sent by the same user from another resource.
+ *
  * @author Christian Schudt
  */
-public class Sent extends PacketExtension {
+public final class Sent extends PacketExtension {
     public Sent(Forwarded forwarded) {
         super("sent", "urn:xmpp:carbons:2");
         element.add(forwarded.getElement());

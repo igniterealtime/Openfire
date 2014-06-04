41b001b988446dde519c50f30f2b510622dd64a9
Florian Schmaus <flo@geekplace.eu>
2014-06-04 14:18:24 +0200

    Shorten if/else statement in OfflineMessageStrategy
    and add some comments.

--- src/java/org/jivesoftware/openfire/OfflineMessageStrategy.java
+++ src/java/org/jivesoftware/openfire/OfflineMessageStrategy.java
@@ -117,12 +117,12 @@ public class OfflineMessageStrategy extends BasicModule {
                 // For a message stanza of type "normal", "groupchat", or "headline", the server MUST either (a) silently ignore the stanza
                 // or (b) return an error stanza to the sender, which SHOULD be <service-unavailable/>.
                 if (message.getType() == Message.Type.normal || message.getType() == Message.Type.groupchat || message.getType() == Message.Type.headline) {
+                    // Depending on the OfflineMessageStragey, we may silently ignore or bounce
                     if (type == Type.bounce) {
                         bounce(message);
-                        return;
-                    } else {
-                        return;
                     }
+                    // Either bounce or silently ignore, never store such messages
+                    return;
                 }
                 // For a message stanza of type "error", the server MUST silently ignore the stanza.
                 else if (message.getType() == Message.Type.error) {

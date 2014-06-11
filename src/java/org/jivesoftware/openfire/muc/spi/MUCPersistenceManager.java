3337e79d5f5e48f57281736e10953d865d73be87
Christian Schudt <christian.schudt@gmx.de>
2014-06-11 20:14:12 +0200

    OF-823 Numeric overflow in MUCPersistenceManager when loading history older than 24 days.

--- src/java/org/jivesoftware/openfire/muc/spi/MUCPersistenceManager.java
+++ src/java/org/jivesoftware/openfire/muc/spi/MUCPersistenceManager.java
@@ -20,6 +20,7 @@
 
 package org.jivesoftware.openfire.muc.spi;
 
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -229,7 +230,7 @@ public class MUCPersistenceManager {
                 pstmt = con.prepareStatement(LOAD_HISTORY);
                 // Reload the history, using "muc.history.reload.limit" (days); defaults to 2
                 int reloadLimitDays = JiveGlobals.getIntProperty(MUC_HISTORY_RELOAD_LIMIT, 2);
-                long from = System.currentTimeMillis() - (86400000 * reloadLimitDays);
+                long from = System.currentTimeMillis() - (BigInteger.valueOf(86400000).multiply(BigInteger.valueOf(reloadLimitDays))).longValue();
                 pstmt.setString(1, StringUtils.dateToMillis(new Date(from)));
                 pstmt.setLong(2, room.getID());
                 rs = pstmt.executeQuery();
@@ -546,7 +547,7 @@ public class MUCPersistenceManager {
                 // if the property is defined, but not numeric, default to 2 (days)
                 int reloadLimitDays = JiveGlobals.getIntProperty(MUC_HISTORY_RELOAD_LIMIT, 2);
                 Log.warn("MUC history reload limit set to " + reloadLimitDays + " days");
-                from = System.currentTimeMillis() - (86400000 * reloadLimitDays);
+                from = System.currentTimeMillis() - (BigInteger.valueOf(86400000).multiply(BigInteger.valueOf(reloadLimitDays))).longValue();
             }
             statement.setLong(1, serviceID);
             statement.setString(2, StringUtils.dateToMillis(new Date(from)));

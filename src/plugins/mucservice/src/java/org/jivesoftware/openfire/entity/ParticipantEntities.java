96fb6e8e3f3b8bbe98e20b6577a8307baea263ab
Redor <r.soldatow@gmail.com>
2014-07-02 14:08:30 +0200

    - Participants Entities

---
+++ src/plugins/mucservice/src/java/org/jivesoftware/openfire/entity/ParticipantEntities.java
@@ -0,0 +1,27 @@
+package org.jivesoftware.openfire.entity;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "participants")
+public class ParticipantEntities {
+	List<ParticipantEntity> participants;
+
+	public ParticipantEntities() {
+	}
+
+	public ParticipantEntities(List<ParticipantEntity> participants) {
+		this.participants = participants;
+	}
+
+	@XmlElement(name = "participant")
+	public List<ParticipantEntity> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(List<ParticipantEntity> participants) {
+		this.participants = participants;
+	}
+}

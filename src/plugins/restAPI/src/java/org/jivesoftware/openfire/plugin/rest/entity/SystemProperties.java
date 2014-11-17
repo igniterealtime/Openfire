81cf5c87957df13b1755c7b73365b9f3e7d4f65a
Roman S. <r.soldatow@gmail.com>
2014-11-17 22:26:45 +0100

    REST API plugin version 0.1.0

---
+++ src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/entity/SystemProperties.java
@@ -0,0 +1,43 @@
+package org.jivesoftware.openfire.plugin.rest.entity;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * The Class SystemProperties.
+ */
+@XmlRootElement(name = "properties")
+public class SystemProperties {
+
+	/** The properties. */
+	List<SystemProperty> properties;
+
+	/**
+	 * Instantiates a new system properties.
+	 */
+	public SystemProperties() {
+
+	}
+
+	/**
+	 * Gets the properties.
+	 *
+	 * @return the properties
+	 */
+	@XmlElement(name = "property")
+	public List<SystemProperty> getProperties() {
+		return properties;
+	}
+
+
+	/**
+	 * Sets the properties.
+	 *
+	 * @param properties the new properties
+	 */
+	public void setProperties(List<SystemProperty> properties) {
+		this.properties = properties;
+	}
+}
---
+++ src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/exceptions/ExceptionType.java
@@ -0,0 +1,18 @@
+package org.jivesoftware.openfire.plugin.rest.exceptions;
+
+/**
+ * The Class ExceptionType.
+ */
+public final class ExceptionType {
+	/** The Constant PROPERTY_NOT_FOUND. */
+	public static final String PROPERTY_NOT_FOUND = "PropertyNotFoundException";
+	
+	/** The Constant ILLEGAL_ARGUMENT_EXCEPTION. */
+	public static final String ILLEGAL_ARGUMENT_EXCEPTION = "IllegalArgumentException";
+
+	/**
+	 * Instantiates a new exception type.
+	 */
+	private ExceptionType() {
+	}
+}

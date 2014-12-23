da82ca9bcf3ab48fe9ad5802f26ad50ab806b55f
Tom Evans <tevans@practicefusion.com>
2014-12-23 10:14:48 -0800

    OF-629: Here Lies XMPP Session
    Update the advertised stream features to mark the xmpp-session
    capability as optional (per
    http://tools.ietf.org/html/draft-cridland-xmpp-session-01). Current
    implementation is already no-op.

--- src/java/org/jivesoftware/openfire/session/LocalClientSession.java
+++ src/java/org/jivesoftware/openfire/session/LocalClientSession.java
@@ -804,7 +804,7 @@ public class LocalClientSession extends LocalSession implements ClientSession {
             // If the session has been authenticated then offer resource binding
             // and session establishment
             sb.append("<bind xmlns=\"urn:ietf:params:xml:ns:xmpp-bind\"/>");
-            sb.append("<session xmlns=\"urn:ietf:params:xml:ns:xmpp-session\"/>");
+            sb.append("<session xmlns=\"urn:ietf:params:xml:ns:xmpp-session\"><optional/></session>");
         }
         return sb.toString();
     }

8113cf28373445267f747e289263a426cea56659
Tom Evans <twevans@hotmail.com>
2015-04-29 15:01:12 -0700

    OF-908: Avoid chunked encoding for BOSH
    Fixes alternate (synchronous) write to the HTTP response output stream.

--- src/java/org/jivesoftware/openfire/http/HttpBindServlet.java
+++ src/java/org/jivesoftware/openfire/http/HttpBindServlet.java
@@ -299,9 +299,9 @@ public class HttpBindServlet extends HttpServlet {
         if (async) {
             response.getOutputStream().setWriteListener(new WriteListenerImpl(context, byteContent));
         } else {
-            // BOSH communication should not have Chunked encoding. Ensure that the
-            // buffer can hold the entire response to prevent chunking.
-            context.getResponse().setBufferSize(byteContent.length);
+            // BOSH communication should not use Chunked encoding.
+            // This is prevented by explicitly setting the Content-Length header.
+            context.getResponse().setContentLength(byteContent.length);
             context.getResponse().getOutputStream().write(byteContent);
             context.getResponse().getOutputStream().flush();
             context.complete();

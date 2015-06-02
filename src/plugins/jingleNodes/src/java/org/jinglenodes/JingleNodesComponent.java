f01aa270d8556413b65a995b5f64e82291a0bcca
Chien Cheng-Ming <linus.chien@gmail.com>
2015-06-02 08:57:51 +0800

    replace tab with 4*space

--- src/plugins/jingleNodes/src/java/org/jinglenodes/JingleNodesComponent.java
+++ src/plugins/jingleNodes/src/java/org/jinglenodes/JingleNodesComponent.java
@@ -37,104 +37,104 @@ import org.xmpp.packet.IQ;
 import org.xmpp.packet.PacketError;
 
 class JingleNodesComponent extends AbstractComponent {
-	private static final Logger Log = LoggerFactory.getLogger(JingleNodesComponent.class);
+    private static final Logger Log = LoggerFactory.getLogger(JingleNodesComponent.class);
 
-	private static final String UDP = "udp";
-	private static final String PROTOCOL = "protocol";
-	private static final String HOST = "host";
-	private static final String LOCAL_PORT = "localport";
-	private static final String REMOTE_PORT = "remoteport";
+    private static final String UDP = "udp";
+    private static final String PROTOCOL = "protocol";
+    private static final String HOST = "host";
+    private static final String LOCAL_PORT = "localport";
+    private static final String REMOTE_PORT = "remoteport";
 
-	private final JingleNodesPlugin plugin;
+    private final JingleNodesPlugin plugin;
 
-	public JingleNodesComponent(final JingleNodesPlugin plugin) {
-		this.plugin = plugin;
-	}
+    public JingleNodesComponent(final JingleNodesPlugin plugin) {
+        this.plugin = plugin;
+    }
 
-	public String getName() {
-		return "JingleRelayNode";
-	}
+    public String getName() {
+        return "JingleRelayNode";
+    }
 
-	public String getDescription() {
-		return "Jingle Relay Service";
-	}
+    public String getDescription() {
+        return "Jingle Relay Service";
+    }
 
-	@Override
-	protected String[] discoInfoFeatureNamespaces() {
-		return new String[] { JingleChannelIQ.NAMESPACE, JingleTrackerIQ.NAMESPACE };
-	}
+    @Override
+    protected String[] discoInfoFeatureNamespaces() {
+        return new String[] { JingleChannelIQ.NAMESPACE, JingleTrackerIQ.NAMESPACE };
+    }
 
-	@Override
-	protected String discoInfoIdentityCategoryType() {
-		return "relay";
-	}
+    @Override
+    protected String discoInfoIdentityCategoryType() {
+        return "relay";
+    }
 
-	@Override
-	protected IQ handleIQGet(IQ iq) throws Exception {
-		final IQ reply = IQ.createResultIQ(iq);
+    @Override
+    protected IQ handleIQGet(IQ iq) throws Exception {
+        final IQ reply = IQ.createResultIQ(iq);
 
-		final Element element = iq.getChildElement();
-		final String namespace = element.getNamespaceURI();
+        final Element element = iq.getChildElement();
+        final String namespace = element.getNamespaceURI();
 
-		if (JingleChannelIQ.NAME.equals(element.getName()) && JingleChannelIQ.NAMESPACE.equals(namespace) && UDP.equals(element.attributeValue(PROTOCOL))) {
-			final Element childElement = iq.getChildElement().createCopy();
-			final RelayChannel channel = plugin.createRelayChannel();
+        if (JingleChannelIQ.NAME.equals(element.getName()) && JingleChannelIQ.NAMESPACE.equals(namespace) && UDP.equals(element.attributeValue(PROTOCOL))) {
+            final Element childElement = iq.getChildElement().createCopy();
+            final RelayChannel channel = plugin.createRelayChannel();
 
-			if (channel != null) {
+            if (channel != null) {
 
-				childElement.addAttribute(HOST, LocalIPResolver.getLocalIP());
-				childElement.addAttribute(LOCAL_PORT, Integer.toString(channel.getPortA()));
-				childElement.addAttribute(REMOTE_PORT, Integer.toString(channel.getPortB()));
+                childElement.addAttribute(HOST, LocalIPResolver.getLocalIP());
+                childElement.addAttribute(LOCAL_PORT, Integer.toString(channel.getPortA()));
+                childElement.addAttribute(REMOTE_PORT, Integer.toString(channel.getPortB()));
 
-				reply.setChildElement(childElement);
+                reply.setChildElement(childElement);
 
-				Log.info(
-						"Created relay channel {}:{}, {}:{}, {}:{}",
-						new Object[] { HOST, LocalIPResolver.getLocalIP(), LOCAL_PORT, Integer.toString(channel.getPortA()), REMOTE_PORT,
-								Integer.toString(channel.getPortB()) });
+                Log.info(
+                        "Created relay channel {}:{}, {}:{}, {}:{}",
+                        new Object[] { HOST, LocalIPResolver.getLocalIP(), LOCAL_PORT, Integer.toString(channel.getPortA()), REMOTE_PORT,
+                                Integer.toString(channel.getPortB()) });
 
-			} else {
-				reply.setError(PacketError.Condition.internal_server_error);
-			}
-			return reply;
-		} else if (JingleTrackerIQ.NAME.equals(element.getName()) && JingleTrackerIQ.NAMESPACE.equals(namespace)) {
+            } else {
+                reply.setError(PacketError.Condition.internal_server_error);
+            }
+            return reply;
+        } else if (JingleTrackerIQ.NAME.equals(element.getName()) && JingleTrackerIQ.NAMESPACE.equals(namespace)) {
 
-			final List<TrackerEntry> entries = new ArrayList<TrackerEntry>();
-			entries.add(new TrackerEntry(TrackerEntry.Type.relay, TrackerEntry.Policy._roster, plugin.getServiceName() + "." + getDomain(), UDP));
+            final List<TrackerEntry> entries = new ArrayList<TrackerEntry>();
+            entries.add(new TrackerEntry(TrackerEntry.Type.relay, TrackerEntry.Policy._roster, plugin.getServiceName() + "." + getDomain(), UDP));
 
-			final String elements = getChildElementXML(entries);
+            final String elements = getChildElementXML(entries);
 
-			final Element e = DocumentHelper.parseText(elements).getRootElement();
+            final Element e = DocumentHelper.parseText(elements).getRootElement();
 
-			reply.setChildElement(e);
+            reply.setChildElement(e);
 
-			return reply;
-		}
+            return reply;
+        }
 
-		return null; // feature not implemented.
-	}
+        return null; // feature not implemented.
+    }
 
-	public String getChildElementXML(final List<TrackerEntry> entries) {
-		final StringBuilder str = new StringBuilder();
+    public String getChildElementXML(final List<TrackerEntry> entries) {
+        final StringBuilder str = new StringBuilder();
 
-		str.append("<").append(JingleTrackerIQ.NAME).append(" xmlns='").append(JingleTrackerIQ.NAMESPACE).append("'>");
-		for (final TrackerEntry entry : entries) {
-			str.append("<").append(entry.getType().toString());
-			str.append(" policy='").append(entry.getPolicy().toString()).append("'");
-			str.append(" address='").append(entry.getJid()).append("'");
-			str.append(" protocol='").append(entry.getProtocol()).append("'");
-			if (entry.isVerified()) {
-				str.append(" verified='").append(entry.isVerified()).append("'");
-			}
-			str.append("/>");
-		}
-		str.append("</").append(JingleTrackerIQ.NAME).append(">");
+        str.append("<").append(JingleTrackerIQ.NAME).append(" xmlns='").append(JingleTrackerIQ.NAMESPACE).append("'>");
+        for (final TrackerEntry entry : entries) {
+            str.append("<").append(entry.getType().toString());
+            str.append(" policy='").append(entry.getPolicy().toString()).append("'");
+            str.append(" address='").append(entry.getJid()).append("'");
+            str.append(" protocol='").append(entry.getProtocol()).append("'");
+            if (entry.isVerified()) {
+                str.append(" verified='").append(entry.isVerified()).append("'");
+            }
+            str.append("/>");
+        }
+        str.append("</").append(JingleTrackerIQ.NAME).append(">");
 
-		return str.toString();
-	}
+        return str.toString();
+    }
 
-	@Override
-	public String getDomain() {
-		return XMPPServer.getInstance().getServerInfo().getXMPPDomain();
-	}
+    @Override
+    public String getDomain() {
+        return XMPPServer.getInstance().getServerInfo().getXMPPDomain();
+    }
 }
--- src/plugins/jingleNodes/src/java/org/jinglenodes/JingleNodesPlugin.java
+++ src/plugins/jingleNodes/src/java/org/jinglenodes/JingleNodesPlugin.java
@@ -41,122 +41,122 @@ import org.xmpp.jnodes.nio.PublicIPResolver;
 
 public class JingleNodesPlugin implements Plugin {
 
-	private static final Logger Log = LoggerFactory.getLogger(JingleNodesPlugin.class);
+    private static final Logger Log = LoggerFactory.getLogger(JingleNodesPlugin.class);
 
-	public static final String JN_PUB_IP_PROPERTY = "jinglenodes.publicip";
-	public static final String JN_MIN_PORT_PROPERTY = "jinglenodes.minport";
-	public static final String JN_MAX_PORT_PROPERTY = "jinglenodes.maxport";
-	public static final String JN_STUN_SERVER_PROPERTY = "jinglenodes.stunserver";
-	private ComponentManager componentManager;
+    public static final String JN_PUB_IP_PROPERTY = "jinglenodes.publicip";
+    public static final String JN_MIN_PORT_PROPERTY = "jinglenodes.minport";
+    public static final String JN_MAX_PORT_PROPERTY = "jinglenodes.maxport";
+    public static final String JN_STUN_SERVER_PROPERTY = "jinglenodes.stunserver";
+    private ComponentManager componentManager;
 
-	private final ConcurrentHashMap<String, RelayChannel> channels = new ConcurrentHashMap<String, RelayChannel>();
-	private final long timeout = 60000;
-	private final AtomicInteger ids = new AtomicInteger(0);
-	private final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-	private final String serviceName = "relay";
+    private final ConcurrentHashMap<String, RelayChannel> channels = new ConcurrentHashMap<String, RelayChannel>();
+    private final long timeout = 60000;
+    private final AtomicInteger ids = new AtomicInteger(0);
+    private final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+    private final String serviceName = "relay";
 
-	private final boolean bindAllInterfaces;
+    private final boolean bindAllInterfaces;
 
-	private boolean hasPublicIP = false;
+    private boolean hasPublicIP = false;
 
-	private int minPort = JiveGlobals.getIntProperty(JN_MIN_PORT_PROPERTY, 30000);
-	private int maxPort = JiveGlobals.getIntProperty(JN_MAX_PORT_PROPERTY, 40000);
-	private String stunServer = JiveGlobals.getProperty(JN_STUN_SERVER_PROPERTY, "stun.stunprotocol.org");
+    private int minPort = JiveGlobals.getIntProperty(JN_MIN_PORT_PROPERTY, 30000);
+    private int maxPort = JiveGlobals.getIntProperty(JN_MAX_PORT_PROPERTY, 40000);
+    private String stunServer = JiveGlobals.getProperty(JN_STUN_SERVER_PROPERTY, "stun.stunprotocol.org");
 
-	public JingleNodesPlugin() {
-		final String os = System.getProperty("os.name");
-		bindAllInterfaces = !(os != null && os.toLowerCase().indexOf("win") > -1);
-	}
+    public JingleNodesPlugin() {
+        final String os = System.getProperty("os.name");
+        bindAllInterfaces = !(os != null && os.toLowerCase().indexOf("win") > -1);
+    }
 
-	public void initializePlugin(PluginManager manager, File pluginDirectory) {
-		componentManager = ComponentManagerFactory.getComponentManager();
-		JingleNodesComponent component = new JingleNodesComponent(this);
-		try {
-			componentManager.addComponent(serviceName, component);
-		} catch (ComponentException e) {
-			Log.error("Could NOT load " + component.getName());
-		}
-		setup();
+    public void initializePlugin(PluginManager manager, File pluginDirectory) {
+        componentManager = ComponentManagerFactory.getComponentManager();
+        JingleNodesComponent component = new JingleNodesComponent(this);
+        try {
+            componentManager.addComponent(serviceName, component);
+        } catch (ComponentException e) {
+            Log.error("Could NOT load " + component.getName());
+        }
+        setup();
 
-		Log.info("hasPublicIP: " + hasPublicIP);
-		Log.info("minPort: " + minPort);
-		Log.info("maxPort: " + maxPort);
-		Log.info("stunServer: " + stunServer);
-	}
+        Log.info("hasPublicIP: " + hasPublicIP);
+        Log.info("minPort: " + minPort);
+        Log.info("maxPort: " + maxPort);
+        Log.info("stunServer: " + stunServer);
+    }
 
-	private void setup() {
-		executor.scheduleWithFixedDelay(new Runnable() {
-			public void run() {
-				for (final RelayChannel c : channels.values()) {
-					final long current = System.currentTimeMillis();
-					final long da = current - c.getLastReceivedTimeA();
-					final long db = current - c.getLastReceivedTimeB();
+    private void setup() {
+        executor.scheduleWithFixedDelay(new Runnable() {
+            public void run() {
+                for (final RelayChannel c : channels.values()) {
+                    final long current = System.currentTimeMillis();
+                    final long da = current - c.getLastReceivedTimeA();
+                    final long db = current - c.getLastReceivedTimeB();
 
-					if (da > timeout || db > timeout) {
-						removeChannel(c);
-					}
-				}
-			}
-		}, timeout, timeout, TimeUnit.MILLISECONDS);
-		Log.info("Jingle Nodes Loaded.");
+                    if (da > timeout || db > timeout) {
+                        removeChannel(c);
+                    }
+                }
+            }
+        }, timeout, timeout, TimeUnit.MILLISECONDS);
+        Log.info("Jingle Nodes Loaded.");
 
-		verifyNetwork();
-	}
+        verifyNetwork();
+    }
 
-	public void verifyNetwork() {
-		final String localAddress = JiveGlobals.getProperty(JN_PUB_IP_PROPERTY, LocalIPResolver.getLocalIP());
-		LocalIPResolver.setOverrideIp(localAddress);
-		final InetSocketAddress publicAddress = PublicIPResolver.getPublicAddress(stunServer, 3478);
-		hasPublicIP = publicAddress != null && publicAddress.getAddress().getHostAddress().equals(localAddress);
-	}
+    public void verifyNetwork() {
+        final String localAddress = JiveGlobals.getProperty(JN_PUB_IP_PROPERTY, LocalIPResolver.getLocalIP());
+        LocalIPResolver.setOverrideIp(localAddress);
+        final InetSocketAddress publicAddress = PublicIPResolver.getPublicAddress(stunServer, 3478);
+        hasPublicIP = publicAddress != null && publicAddress.getAddress().getHostAddress().equals(localAddress);
+    }
 
-	private void closeAllChannels() {
-		for (final RelayChannel c : channels.values()) {
-			removeChannel(c);
-		}
-	}
+    private void closeAllChannels() {
+        for (final RelayChannel c : channels.values()) {
+            removeChannel(c);
+        }
+    }
 
-	public RelayChannel createRelayChannel() {
-		RelayChannel rc = null;
-		try {
+    public RelayChannel createRelayChannel() {
+        RelayChannel rc = null;
+        try {
 
-			rc = RelayChannel.createLocalRelayChannel(bindAllInterfaces ? "0.0.0.0" : LocalIPResolver.getLocalIP(), minPort, maxPort);
-			final int id = ids.incrementAndGet();
-			final String sId = String.valueOf(id);
-			rc.setAttachment(sId);
+            rc = RelayChannel.createLocalRelayChannel(bindAllInterfaces ? "0.0.0.0" : LocalIPResolver.getLocalIP(), minPort, maxPort);
+            final int id = ids.incrementAndGet();
+            final String sId = String.valueOf(id);
+            rc.setAttachment(sId);
 
-			channels.put(sId, rc);
-		} catch (IOException e) {
-			Log.error("Could Not Create Channel.", e);
-		}
+            channels.put(sId, rc);
+        } catch (IOException e) {
+            Log.error("Could Not Create Channel.", e);
+        }
 
-		return rc;
-	}
+        return rc;
+    }
 
-	private void removeChannel(final RelayChannel c) {
-		channels.remove((String) c.getAttachment());
-		c.close();
-	}
+    private void removeChannel(final RelayChannel c) {
+        channels.remove((String) c.getAttachment());
+        c.close();
+    }
 
-	public String getServiceName() {
-		return serviceName;
-	}
+    public String getServiceName() {
+        return serviceName;
+    }
 
-	public void destroyPlugin() {
-		try {
-			componentManager.removeComponent(serviceName);
-		} catch (ComponentException e) {
-			Log.error("Could NOT Remove " + serviceName + " Component");
-		}
-		closeAllChannels();
-		executor.shutdownNow();
-	}
+    public void destroyPlugin() {
+        try {
+            componentManager.removeComponent(serviceName);
+        } catch (ComponentException e) {
+            Log.error("Could NOT Remove " + serviceName + " Component");
+        }
+        closeAllChannels();
+        executor.shutdownNow();
+    }
 
-	public boolean hasPublicIP() {
-		return hasPublicIP;
-	}
+    public boolean hasPublicIP() {
+        return hasPublicIP;
+    }
 
-	public int getActiveChannelCount() {
-		return channels.size();
-	}
+    public int getActiveChannelCount() {
+        return channels.size();
+    }
 }

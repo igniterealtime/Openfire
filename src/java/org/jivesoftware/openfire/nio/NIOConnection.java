40577476b3888242ca2091972ff569be7db68d57
Dave Cridland <dave.cridland@surevine.com>
2015-06-10 15:19:06 +0100

    OF-916 Add SslFilter before Executor
    Addressing DIRMINA-1012

--- src/java/org/jivesoftware/openfire/nio/NIOConnection.java
+++ src/java/org/jivesoftware/openfire/nio/NIOConnection.java
@@ -449,7 +449,7 @@ public class NIOConnection implements Connection {
             // good
             filter.setWantClientAuth(true);
         }
-        ioSession.getFilterChain().addAfter(EXECUTOR_FILTER_NAME, TLS_FILTER_NAME, filter);
+        ioSession.getFilterChain().addBefore(EXECUTOR_FILTER_NAME, TLS_FILTER_NAME, filter);
         ioSession.setAttribute(SslFilter.DISABLE_ENCRYPTION_ONCE, Boolean.TRUE);
 
         if (!clientMode) {

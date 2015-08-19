851398dc0cb3198cc76dc531e12bc72bf2ab7fdb
Roman S <r.soldatow@gmail.com>
2015-08-19 22:43:34 +0200

    Update the REST API plugin to 1.1.4
    - Added: get concurrent sessions (local or cluster wide)

---
+++ src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/controller/StatisticsController.java
@@ -0,0 +1,34 @@
+package org.jivesoftware.openfire.plugin.rest.controller;
+
+import org.jivesoftware.openfire.SessionManager;
+import org.jivesoftware.openfire.plugin.rest.entity.SessionsCount;
+
+/**
+ * The Class StatisticsController.
+ */
+public class StatisticsController {
+	
+	/** The Constant INSTANCE. */
+	public static final StatisticsController INSTANCE = new StatisticsController();
+
+	/**
+	 * Gets the instance.
+	 *
+	 * @return the instance
+	 */
+	public static StatisticsController getInstance() {
+		return INSTANCE;
+	}
+	
+	/**
+	 * Gets the concurent sessions.
+	 *
+	 * @return the concurent sessions
+	 */
+	public SessionsCount getConcurentSessions() {
+		int userSessionsCountLocal = SessionManager.getInstance().getUserSessionsCount(true);
+		int userSessionsCountCluster = SessionManager.getInstance().getUserSessionsCount(false);
+		
+		return new SessionsCount(userSessionsCountLocal, userSessionsCountCluster);
+	}
+}

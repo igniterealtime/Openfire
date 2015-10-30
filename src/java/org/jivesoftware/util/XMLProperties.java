igniterealtime/Openfire

commit e0bc007ff16d669512467ad5f62c65f0a7194402

Use try-with-resources statement to properly close resources.
- Not all resources were closed in finally block (see Launcher, PluginManager, UpdateManager, FaviconServlet, ...)
- Get rid of verbose finally blocks and empty catch blocks, which makes the code more readable.
- Generally, this is the more modern approach to deal with resources.

src/java/org/jivesoftware/util/XMLProperties.java
@@ -742,54 +742,24 @@ public class XMLProperties {
     		Log.error("Unable to save XML properties; no file specified");
     		return;
     	}
-        boolean error = false;
         // Write data out to a temporary file first.
-        File tempFile = null;
-        Writer writer = null;
-        try {
-            tempFile = new File(file.getParentFile(), file.getName() + ".tmp");
-            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(tempFile), "UTF-8"));
+        File tempFile = new File(file.getParentFile(), file.getName() + ".tmp");
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(tempFile), "UTF-8"))) {
             OutputFormat prettyPrinter = OutputFormat.createPrettyPrint();
             XMLWriter xmlWriter = new XMLWriter(writer, prettyPrinter);
             xmlWriter.write(document);
-        }
-        catch (Exception e) {
-            Log.error(e.getMessage(), e);
-            // There were errors so abort replacing the old property file.
-            error = true;
-        }
-        finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                }
-                catch (IOException e1) {
-                    Log.error(e1.getMessage(), e1);
-                    error = true;
-                }
-            }
-        }
-
-        // No errors occurred, so delete the main file.
-        if (!error) {
             // Delete the old file so we can replace it.
             if (!file.delete()) {
                 Log.error("Error deleting property file: " + file.getAbsolutePath());
                 return;
             }
             // Copy new contents to the file.
-            try {
-                copy(tempFile, file);
-            }
-            catch (Exception e) {
-                Log.error(e.getMessage(), e);
-                // There were errors so abort replacing the old property file.
-                error = true;
-            }
+            copy(tempFile, file);
+
             // If no errors, delete the temp file.
-            if (!error) {
-                tempFile.delete();
-            }
+            tempFile.delete();
+        } catch (Exception e) {
+            Log.error(e.getMessage(), e);
         }
     }
 
@@ -826,25 +796,9 @@ public class XMLProperties {
      * @throws IOException If there was a problem making the copy
      */
     private static void copy(File inFile, File outFile) throws IOException {
-        FileInputStream fin = null;
-        FileOutputStream fout = null;
-        try {
-            fin = new FileInputStream(inFile);
-            fout = new FileOutputStream(outFile);
-            copy(fin, fout);
-        }
-        finally {
-            try {
-                if (fin != null) fin.close();
-            }
-            catch (IOException e) {
-                // do nothing
-            }
-            try {
-                if (fout != null) fout.close();
-            }
-            catch (IOException e) {
-                // do nothing
+        try (FileInputStream fin = new FileInputStream(inFile)) {
+            try (FileOutputStream fout = new FileOutputStream(outFile)) {
+                copy(fin, fout);
             }
         }
     }

src/java/org/jivesoftware/database/DbConnectionManager.java
@@ -666,10 +666,8 @@ public class DbConnectionManager {
      */
     public static String getLargeTextField(ResultSet rs, int columnIndex) throws SQLException {
         if (isStreamTextRequired()) {
-            Reader bodyReader = null;
-            String value = null;
-            try {
-                bodyReader = rs.getCharacterStream(columnIndex);
+            String value;
+            try (Reader bodyReader = rs.getCharacterStream(columnIndex)) {
                 if (bodyReader == null) {
                     return null;
                 }
@@ -685,16 +683,6 @@ public class DbConnectionManager {
             catch (Exception e) {
                 Log.error(e.getMessage(), e);
                 throw new SQLException("Failed to load text field");
-            }
-            finally {
-                try {
-                    if (bodyReader != null) {
-                        bodyReader.close();
-                    }
-                }
-                catch (Exception e) {
-                    // Ignore.
-                }
             }
             return value;
         }

src/java/org/jivesoftware/database/SchemaManager.java
@@ -235,25 +235,17 @@ public class SchemaManager {
             // Resource will be like "/database/openfire_hsqldb.sql"
             String resourceName = schemaKey + "_" +
                     DbConnectionManager.getDatabaseType() + ".sql";
-            InputStream resource = resourceLoader.loadResource(resourceName);
-            if (resource == null) {
-                return false;
-            }
-            try {
+
+            try (InputStream resource = resourceLoader.loadResource(resourceName)) {
+                if (resource == null) {
+                    return false;
+                }
                 // For plugins, we will automatically convert jiveVersion to ofVersion
                 executeSQLScript(con, resource, !schemaKey.equals("openfire") && !schemaKey.equals("wildfire"));
             }
             catch (Exception e) {
                 Log.error(e.getMessage(), e);
                 return false;
-            }
-            finally {
-                try {
-                    resource.close();
-                }
-                catch (Exception e) {
-                    // Ignore.
-                }
             }
             Log.info(LocaleUtils.getLocalizedString("upgrade.database.success"));
             System.out.println(LocaleUtils.getLocalizedString("upgrade.database.success"));
@@ -281,34 +273,23 @@ public class SchemaManager {
 
             // Run all upgrade scripts until we're up to the latest schema.
             for (int i = currentVersion + 1; i <= requiredVersion; i++) {
-                InputStream resource = getUpgradeResource(resourceLoader, i, schemaKey);
-                
-                // apply the 'database-patches-done-in-java'
-				try {
-					if (i == 21 && schemaKey.equals("openfire")) {
-						OF33.executeFix(con);
-					}
-				} catch (Exception e) {
-					Log.error(e.getMessage(), e);
-					return false;
-				}
-                if (resource == null) {
-                    continue;
-                }
-                try {
+                try (InputStream resource = getUpgradeResource(resourceLoader, i, schemaKey)) {
+                    // apply the 'database-patches-done-in-java'
+                    try {
+                        if (i == 21 && schemaKey.equals("openfire")) {
+                            OF33.executeFix(con);
+                        }
+                    } catch (Exception e) {
+                        Log.error(e.getMessage(), e);
+                        return false;
+                    }
+                    if (resource == null) {
+                        continue;
+                    }
                     executeSQLScript(con, resource, !schemaKey.equals("openfire") && !schemaKey.equals("wildfire"));
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     Log.error(e.getMessage(), e);
                     return false;
-                }
-                finally {
-                    try {
-                        resource.close();
-                    }
-                    catch (Exception e) {
-                        // Ignore.
-                    }
                 }
             }
             Log.info(LocaleUtils.getLocalizedString("upgrade.database.success"));
@@ -371,9 +352,7 @@ public class SchemaManager {
     private static void executeSQLScript(Connection con, InputStream resource, Boolean autoreplace) throws IOException,
             SQLException
     {
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(resource));
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(resource))) {
             boolean done = false;
             while (!done) {
                 StringBuilder command = new StringBuilder();
@@ -415,16 +394,6 @@ public class SchemaManager {
                     finally {
                         DbConnectionManager.closeStatement(pstmt);
                     }
-                }
-            }
-        }
-        finally {
-            if (in != null) {
-                try {
-                    in.close();
-                }
-                catch (Exception e) {
-                    Log.error(e.getMessage(), e);
                 }
             }
         }

src/java/org/jivesoftware/openfire/XMPPServer.java
@@ -474,13 +474,12 @@ public class XMPPServer {
 
     @SuppressWarnings("unchecked")
 	private void loadModules() {
-    	FileReader in = null;
-    	try {
-    		File modulesXml = new File(JiveGlobals.getHomeDirectory(), "conf/modules.xml");
-            logger.info("Loading modules from " + modulesXml.getAbsolutePath());
-            SAXReader xmlReader = new SAXReader();
-            xmlReader.setEncoding("UTF-8");
-            in = new FileReader(modulesXml);
+
+        File modulesXml = new File(JiveGlobals.getHomeDirectory(), "conf/modules.xml");
+        logger.info("Loading modules from " + modulesXml.getAbsolutePath());
+        SAXReader xmlReader = new SAXReader();
+        xmlReader.setEncoding("UTF-8");
+    	try (FileReader in = new FileReader(modulesXml)) {
             Document document = xmlReader.read(in);
             Element root = document.getRootElement();
             Iterator<Node> itr = root.nodeIterator();
@@ -495,14 +494,6 @@ public class XMPPServer {
     	} catch (Exception e) {
     		e.printStackTrace();
     		logger.error(LocaleUtils.getLocalizedString("admin.error"), e);
-    	} finally {
-    		if (in != null) {
-    			try {
-					in.close();
-				} catch (IOException e) {
-					// Squash
-				}
-    		}
     	}
         
         // Keep a reference to the internal component manager
@@ -773,9 +764,7 @@ public class XMPPServer {
         // we have to attempt to load the value from openfire_init.xml,
         // which must be in the classpath.
         if (openfireHome == null) {
-            InputStream in = null;
-            try {
-                in = getClass().getResourceAsStream("/openfire_init.xml");
+            try (InputStream in = getClass().getResourceAsStream("/openfire_init.xml")) {
                 if (in != null) {
                     SAXReader reader = new SAXReader();
                     Document doc = reader.read(in);
@@ -793,17 +782,6 @@ public class XMPPServer {
             catch (Exception e) {
                 System.err.println("Error loading openfire_init.xml to find home.");
                 e.printStackTrace();
-            }
-            finally {
-                try {
-                    if (in != null) {
-                        in.close();
-                    }
-                }
-                catch (Exception e) {
-                    System.err.println("Could not close open connection");
-                    e.printStackTrace();
-                }
             }
         }
 

src/java/org/jivesoftware/openfire/container/PluginIconServlet.java
@@ -69,37 +69,17 @@ public class PluginIconServlet extends HttpServlet {
                 else {
                     response.setContentType("image/gif");
                 }
-                InputStream in = null;
-                OutputStream ost = null;
-                try {
-                    in = new FileInputStream(icon);
-                    ost = response.getOutputStream();
-
-                    byte[] buf = new byte[1024];
-                    int len;
-                    while ((len = in.read(buf)) >= 0) {
-                        ost.write(buf, 0, len);
+                try (InputStream in = new FileInputStream(icon)) {
+                    try (OutputStream ost = response.getOutputStream()) {
+                        byte[] buf = new byte[1024];
+                        int len;
+                        while ((len = in.read(buf)) >= 0) {
+                            ost.write(buf, 0, len);
+                        }
+                        ost.flush();
                     }
-                    ost.flush();
-                }
-                catch (IOException ioe) {
-
-                }
-                finally {
-                    if (in != null) {
-                        try {
-                            in.close();
-                        }
-                        catch (Exception e) {
-                        }
-                    }
-                    if (ost != null) {
-                        try {
-                            ost.close();
-                        }
-                        catch (Exception e) {
-                        }
-                    }
+                } catch (IOException ioe) {
+                    throw new ServletException(ioe);
                 }
             }
         }

src/java/org/jivesoftware/openfire/container/PluginManager.java
@@ -177,12 +177,12 @@ public class PluginManager {
             // Absolute path to the plugin file
             String absolutePath = pluginDirectory + File.separator + pluginFilename;
             // Save input stream contents to a temp file
-            OutputStream out = new FileOutputStream(absolutePath + ".part");
-            while ((len = in.read(b)) != -1) {
-                     //write byte to file
-                     out.write(b, 0, len);
+            try (OutputStream out = new FileOutputStream(absolutePath + ".part")) {
+                while ((len = in.read(b)) != -1) {
+                    //write byte to file
+                    out.write(b, 0, len);
+                }
             }
-            out.close();
             // Delete old .jar (if it exists)
             new File(absolutePath).delete();
             // Rename temp file to .jar
@@ -1097,8 +1097,7 @@ public class PluginManager {
          * @param dir the directory to extract the plugin to.
          */
         private void unzipPlugin(String pluginName, File file, File dir) {
-            try {
-                ZipFile zipFile = new JarFile(file);
+            try (ZipFile zipFile = new JarFile(file)) {
                 // Ensure that this JAR is a plugin.
                 if (zipFile.getEntry("plugin.xml") == null) {
                     return;
@@ -1116,20 +1115,18 @@ public class PluginManager {
                     }
                     if (!entry.isDirectory()) {
                         entryFile.getParentFile().mkdirs();
-                        FileOutputStream out = new FileOutputStream(entryFile);
-                        InputStream zin = zipFile.getInputStream(entry);
-                        byte[] b = new byte[512];
-                        int len;
-                        while ((len = zin.read(b)) != -1) {
-                            out.write(b, 0, len);
+                        try (FileOutputStream out = new FileOutputStream(entryFile)) {
+                            try (InputStream zin = zipFile.getInputStream(entry)) {
+                                byte[] b = new byte[512];
+                                int len;
+                                while ((len = zin.read(b)) != -1) {
+                                    out.write(b, 0, len);
+                                }
+                                out.flush();
+                            }
                         }
-                        out.flush();
-                        out.close();
-                        zin.close();
                     }
                 }
-                zipFile.close();
-
             }
             catch (Exception e) {
                 Log.error(e.getMessage(), e);

src/java/org/jivesoftware/openfire/container/PluginServlet.java
@@ -405,34 +405,18 @@ public class PluginServlet extends HttpServlet {
             // response.setHeader("Content-disposition", "filename=\"" + file + "\";");
             response.setContentType(contentType);
             // Write out the resource to the user.
-            InputStream in = null;
-            ServletOutputStream out = null;
-            try {
-                in = new BufferedInputStream(new FileInputStream(file));
-                out = response.getOutputStream();
+            try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
+                try (ServletOutputStream out = response.getOutputStream()) {
 
-                // Set the size of the file.
-                response.setContentLength((int)file.length());
+                    // Set the size of the file.
+                    response.setContentLength((int) file.length());
 
-                // Use a 1K buffer.
-                byte[] buf = new byte[1024];
-                int len;
-                while ((len = in.read(buf)) != -1) {
-                    out.write(buf, 0, len);
-                }
-            }
-            finally {
-                try {
-                    in.close();
-                }
-                catch (Exception ignored) {
-                    // Ignore.
-                }
-                try {
-                    out.close();
-                }
-                catch (Exception ignored) {
-                    // Ignore.
+                    // Use a 1K buffer.
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = in.read(buf)) != -1) {
+                        out.write(buf, 0, len);
+                    }
                 }
             }
         }

src/java/org/jivesoftware/openfire/filetransfer/proxy/DefaultProxyTransfer.java
@@ -125,43 +125,23 @@ public class DefaultProxyTransfer implements ProxyTransfer {
         if (!isActivatable()) {
             throw new IOException("Transfer missing party");
         }
-        InputStream in = null;
-        OutputStream out = null;
 
-        try {
-            in = getInputStream();
-            out = new ProxyOutputStream(getOutputStream());
+        try (InputStream in = getInputStream()) {
+            try (OutputStream out = new ProxyOutputStream(getOutputStream())) {
 
-            final byte[] b = new byte[BUFFER_SIZE];
-            int count = 0;
-            amountWritten = 0;
+                final byte[] b = new byte[BUFFER_SIZE];
+                int count = 0;
+                amountWritten = 0;
 
-            do {
-                // write to the output stream
-                out.write(b, 0, count);
+                do {
+                    // write to the output stream
+                    out.write(b, 0, count);
 
-                amountWritten += count;
+                    amountWritten += count;
 
-                // read more bytes from the input stream
-                count = in.read(b);
-            } while (count >= 0);
-        }
-        finally {
-            if (in != null) {
-                try {
-                    in.close();
-                }
-                catch (Exception e) {
-                    Log.error(e.getMessage(), e);
-                }
-            }
-            if (out != null) {
-                try {
-                    out.close();
-                }
-                catch (Exception e) {
-                    Log.error(e.getMessage(), e);
-                }
+                    // read more bytes from the input stream
+                    count = in.read(b);
+                } while (count >= 0);
             }
         }
     }

commit 1989f919edd92afa477d11ef46b15b81f6539ccd

Fix error, which was introduced with e0bc007.

src/java/org/jivesoftware/util/XMLProperties.java
@@ -742,24 +742,40 @@ public class XMLProperties {
     		Log.error("Unable to save XML properties; no file specified");
     		return;
     	}
+        boolean error = false;
         // Write data out to a temporary file first.
-        File tempFile = new File(file.getParentFile(), file.getName() + ".tmp");
+        File tempFile = new File(file.getParentFile(), file.getName() + ".tmp");;
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(tempFile), "UTF-8"))) {
             OutputFormat prettyPrinter = OutputFormat.createPrettyPrint();
             XMLWriter xmlWriter = new XMLWriter(writer, prettyPrinter);
             xmlWriter.write(document);
+        }
+        catch (Exception e) {
+            Log.error(e.getMessage(), e);
+            // There were errors so abort replacing the old property file.
+            error = true;
+        }
+
+        // No errors occurred, so delete the main file.
+        if (!error) {
             // Delete the old file so we can replace it.
             if (!file.delete()) {
                 Log.error("Error deleting property file: " + file.getAbsolutePath());
                 return;
             }
             // Copy new contents to the file.
-            copy(tempFile, file);
-
+            try {
+                copy(tempFile, file);
+            }
+            catch (Exception e) {
+                Log.error(e.getMessage(), e);
+                // There were errors so abort replacing the old property file.
+                error = true;
+            }
             // If no errors, delete the temp file.
-            tempFile.delete();
-        } catch (Exception e) {
-            Log.error(e.getMessage(), e);
+            if (!error) {
+                tempFile.delete();
+            }
         }
     }
 
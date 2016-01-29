a5b1f7a30f28802db7a95090384c4fffbb259097
xingqisheng <xingqisheng@vidmt.com>
2016-01-29 10:54:07 +0800

    添加配置文件

---
+++ src/bin/set-conf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp ./openfired-official.vmoptions ./openfired.vmoptions
+cp ../conf/openfire-official.xml ../conf/openfire.xml
+cp ../lib/log4j-official.xml ../lib/log4j.xml
+
---
+++ src/bin/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+OPENFIRE_OPTS="-Xms8192m -Xmx12288m"
+nohup ./openfire.sh >/dev/null&

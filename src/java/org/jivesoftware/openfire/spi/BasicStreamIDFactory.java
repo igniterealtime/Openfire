5907308beb5bcbf88e163bc78c587a022eb347b1
Guus der Kinderen <guus.der.kinderen@gmail.com>
2016-05-19 15:54:55 +0200

    OF-1125: Fix StreamId equals()

--- src/java/org/jivesoftware/openfire/spi/BasicStreamIDFactory.java
+++ src/java/org/jivesoftware/openfire/spi/BasicStreamIDFactory.java
@@ -80,8 +80,11 @@ public class BasicStreamIDFactory implements StreamIDFactory {
         }
 
         @Override
-        public boolean equals(Object obj) {
-            return id.equals( obj );
+        public boolean equals( Object o )
+        {
+            if ( this == o ) return true;
+            if ( o == null || getClass() != o.getClass() ) return false;
+            return id.equals( ((BasicStreamID) o).id );
         }
     }
 }

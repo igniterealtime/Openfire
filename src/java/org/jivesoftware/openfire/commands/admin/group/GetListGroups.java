24b28db2404fb1a1e25d7e6f80d8c1312459582f
Tagir Valeev <lany@ngs.ru>
2016-07-05 17:42:31 +0600

    max_items reading is fixed

--- src/java/org/jivesoftware/openfire/commands/admin/group/GetListGroups.java
+++ src/java/org/jivesoftware/openfire/commands/admin/group/GetListGroups.java
@@ -99,7 +99,7 @@ public class GetListGroups extends AdHocCommand {
             }
         }
         int maxItems = 100000;
-        if (max_items != null && "none".equals(max_items)) {
+        if (max_items != null && !"none".equals(max_items)) {
             try {
                 maxItems = Integer.parseInt(max_items);
             }

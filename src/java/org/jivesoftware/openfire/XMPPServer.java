7c69753fd5c8954a2c92ba22c9c332446a72d30b
akrherz <akrherz@iastate.edu>
2016-08-17 11:30:58 -0500

    March 4.0 branch to version 4.0.4-alpha

--- src/java/org/jivesoftware/openfire/XMPPServer.java
+++ src/java/org/jivesoftware/openfire/XMPPServer.java
@@ -315,7 +315,7 @@ public class XMPPServer {
             host = "127.0.0.1";        	
         }
 
-        version = new Version(4, 0, 3, Version.ReleaseStatus.Release, -1);
+        version = new Version(4, 0, 4, Version.ReleaseStatus.Alpha, -1);
         if ("true".equals(JiveGlobals.getXMLProperty("setup"))) {
             setupMode = false;
         }

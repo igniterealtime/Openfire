7bec20e3e88cca368539c8f7a66a34f565deca6d
Guus der Kinderen <guus.der.kinderen@gmail.com>
2016-10-07 12:35:41 +0200

    OF-1174: Fixed negation bug.

--- src/java/org/jivesoftware/openfire/container/PluginMonitor.java
+++ src/java/org/jivesoftware/openfire/container/PluginMonitor.java
@@ -154,7 +154,7 @@ public class PluginMonitor
                             if ( Files.exists( dir ) && Files.getLastModifiedTime( jarFile ).toMillis() > Files.getLastModifiedTime( dir ).toMillis() )
                             {
                                 // If this is the first time that the monitor process is running, then plugins won't be loaded yet. Therefore, just delete the directory.
-                                if ( pluginManager.isExecuted() )
+                                if ( !pluginManager.isExecuted() )
                                 {
                                     int count = 0;
                                     // Attempt to delete the folder for up to 5 seconds.

a1293cb661b661d6ebc8996da558cc6dc4f5bd3c
akrherz <akrherz@iastate.edu>
2016-12-13 11:36:46 -0600

    Update database schema version to 24

--- src/database/openfire_hsqldb.sql
+++ src/database/openfire_hsqldb.sql
@@ -378,7 +378,7 @@ INSERT INTO ofID (idType, id) VALUES (19, 1);
 INSERT INTO ofID (idType, id) VALUES (23, 1);
 INSERT INTO ofID (idType, id) VALUES (26, 2);
 
-INSERT INTO ofVersion (name, version) VALUES ('openfire', 23);
+INSERT INTO ofVersion (name, version) VALUES ('openfire', 24);
 
 // Entry for admin user
 INSERT INTO ofUser (username, plainPassword, name, email, creationDate, modificationDate)

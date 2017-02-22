0994a25ce9727c69afc85c09957cae24488e14e0
akrherz <akrherz@iastate.edu>
2017-02-22 11:32:17 -0600

    Revert "change sync key as per @guusdk suggestion"
    This reverts commit 508e39e2bb9c929b351e130aeb68e9d9e69924f3 and hopefully resolves OF-1263 and OF-1269

--- src/java/org/jivesoftware/openfire/group/GroupManager.java
+++ src/java/org/jivesoftware/openfire/group/GroupManager.java
@@ -216,7 +216,7 @@ public class GroupManager {
                     JID userJid = new JID(user);
                     if (XMPPServer.getInstance().isLocal(userJid)) {
                         String username = userJid.getNode();
-                        synchronized ((getClass().getSimpleName() + username).intern()) {
+                        synchronized (username.intern()) {
                             groupMetaCache.remove(username);
                         }
                     }
@@ -481,7 +481,7 @@ public class GroupManager {
     public Collection<Group> getSharedGroups(String userName) {
         Collection<String> groupNames = (Collection<String>)groupMetaCache.get(userName);
         if (groupNames == null) {
-            synchronized((getClass().getSimpleName() + userName).intern()) {
+            synchronized(userName.intern()) {
                 groupNames = (Collection<String>)groupMetaCache.get(userName);
                 if (groupNames == null) {
                 	// assume this is a local user

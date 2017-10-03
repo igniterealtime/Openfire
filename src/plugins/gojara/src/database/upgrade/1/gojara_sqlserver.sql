566c1f4040f8b0d05a9672f907618b7ddb344830
Florian Kinder <florian.kinder@fankserver.com>
2017-10-03 15:39:48 +0200

    Fixed ofGojaraSessions missing after install

--- src/plugins/gojara/src/database/upgrade/1/gojara_sqlserver.sql
+++ src/plugins/gojara/src/database/upgrade/1/gojara_sqlserver.sql
@@ -1,7 +1,7 @@
--- Create SessionEntry 
+-- Create SessionEntry
 CREATE TABLE ofGojaraSessions(
-  username			text NOT NULL,
-  transport			text NOT NULL,
+  username			varchar(max) NOT NULL,
+  transport			varchar(max) NOT NULL,
   lastActivity		bigint(20) NOT NULL,
   PRIMARY KEY (username, transport)
 );
@@ -9,4 +9,3 @@ CREATE INDEX ofGojara_lastActivity_idx  ON ofGojaraSessions(lastActivity);
 
 -- Update database version
 UPDATE ofVersion SET version=1 WHERE name = 'gojara';
-

7b04e47e07cb8cdf02c8267cf543fca84eb21328
Guus der Kinderen <guus.der.kinderen@gmail.com>
2017-11-25 10:08:28 +0100

    OF-1393: Include the letter 'Z'

--- src/java/org/jivesoftware/util/StringUtils.java
+++ src/java/org/jivesoftware/util/StringUtils.java
@@ -544,7 +544,7 @@ public final class StringUtils {
         // Create a char buffer to put random letters and numbers in.
         char[] randBuffer = new char[length];
         for (int i = 0; i < randBuffer.length; i++) {
-            randBuffer[i] = numbersAndLetters[randGen.nextInt(numbersAndLetters.length - 1)];
+            randBuffer[i] = numbersAndLetters[randGen.nextInt(numbersAndLetters.length)];
         }
         return new String(randBuffer);
     }

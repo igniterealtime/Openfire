d5ea16fb073fb0988d9580be62411b7af5514644
Dele Olajide <dele@4ng.net>
2018-01-03 16:44:03 +0000

    OF-1461 - Making fastpath plugin compatible with openfire meetings

--- src/plugins/fastpath/src/java/org/jivesoftware/openfire/fastpath/FastpathPlugin.java
+++ src/plugins/fastpath/src/java/org/jivesoftware/openfire/fastpath/FastpathPlugin.java
@@ -18,6 +18,7 @@ package org.jivesoftware.openfire.fastpath;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Collection;
 
 import org.jivesoftware.openfire.cluster.ClusterEventListener;
 import org.jivesoftware.openfire.cluster.ClusterManager;
@@ -44,7 +45,7 @@ import org.xmpp.packet.JID;
 public class FastpathPlugin implements Plugin, ClusterEventListener {
 
     private static final Logger Log = LoggerFactory.getLogger(FastpathPlugin.class);
-    
+
     /**
      * Keep a reference to Fastpath only when the service is up and running in this JVM.
      */
@@ -132,6 +133,14 @@ public class FastpathPlugin implements Plugin, ClusterEventListener {
         }
         // Clean up the reference to the workgroup manager as a way to say that FP is no longer running in this JVM
         workgroupManager = null;
+    }
+
+    public WorkgroupManager getWorkgroupManager() {
+        return workgroupManager;
+    }
+
+    public Collection<Workgroup> getWorkgroups() {
+        return workgroupManager.getWorkgroups();
     }
 
     public void joinedCluster() {

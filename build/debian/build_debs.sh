40ba5b2fe612244f51bdec4f13165bf15e24f4f8
Dave Cridland <dave@cridland.net>
2018-08-15 17:42:30 +0100

    Alter Debian package script for TeamCity
    Also remove ant as a build dependency.

--- build/debian/build_debs.sh
+++ build/debian/build_debs.sh
@@ -3,14 +3,19 @@
 # set -x
 
 pushd .
-export OPENFIRE_FULLVERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+# This allows us to pass in the Openfire version from the command-line.
+if [ $1 ]; then
+    export OPENFIRE_FULLVERSION=$1
+else
+    export OPENFIRE_FULLVERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+fi
 export OPENFIRE_VERSION=$(echo "${OPENFIRE_FULLVERSION}" | cut -d'-' -f1)
 export DEBIAN_BUILDDATE="$(date +'%a, %d %b %Y %H:%M:%S %z')"
-export WORKDIR=$HOME/debian/openfire-${OPENFIRE_VERSION}
+export WORKDIR=tmp/debian/openfire-${OPENFIRE_VERSION}
 
-if [ -d "$HOME/debian" ]; then
-    echo "Removing previous workdir $HOME/debian"
-    rm -rf $HOME/debian
+if [ -d "tmp/debian" ]; then
+    echo "Removing previous workdir tmp/debian"
+    rm -rf tmp/debian
 fi
 mkdir -p $WORKDIR
 
@@ -31,8 +36,9 @@ cd $WORKDIR/debian
 sed -i.bak s/@version@/${OPENFIRE_VERSION}/g changelog
 sed -i.bak s/@builddate@/"${DEBIAN_BUILDDATE}"/g changelog
 cd ..
-dpkg-buildpackage -rfakeroot -us -uc
+# Disable build-dependencies, because they're often not useful here.
+dpkg-buildpackage -d -rfakeroot -us -uc
 
 pushd
 mkdir -p distribution/target/debian
-mv $HOME/debian/openfire*deb distribution/target/debian/
+mv tmp/debian/openfire*deb distribution/target/debian/

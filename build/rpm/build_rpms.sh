0991e9720bf0d86564767c041e7cf1e8515e3c2a
akrherz <akrherz@iastate.edu>
2018-11-30 10:45:48 -0600

    correct search.jar location for debian/RPM build scripts

--- build/rpm/build_rpms.sh
+++ build/rpm/build_rpms.sh
@@ -47,7 +47,7 @@ else
 fi
 
 # Need to copy in plugins that are defaulted to distribute
-cp -f src/plugins/search/target/search.jar distribution/target/distribution-base/plugins/
+cp -f plugins/search/target/search.jar distribution/target/distribution-base/plugins/
 
 # generate our psuedo source tree, which is actually dist tree from maven
 cd distribution/target

e01d316d2968739a7ad6c53dba8b158abdda3322
Greg Thomas <greg.d.thomas@gmail.com>
2018-12-10 22:06:52 +0000

    Keep a (weak) reference to the key in the value, so all instances of the class can share the same key (as in key1 == key2) preventing the GC from removing entries from the WeakHashMap erroneously.

--- xmppserver/src/main/java/org/jivesoftware/util/AutoCloseableReentrantLock.java
+++ xmppserver/src/main/java/org/jivesoftware/util/AutoCloseableReentrantLock.java
@@ -1,5 +1,6 @@
 package org.jivesoftware.util;
 
+import java.lang.ref.WeakReference;
 import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -7,25 +8,36 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class AutoCloseableReentrantLock implements QuietAutoCloseable {
 
-    private static final Map<String, ReentrantLock> LOCK_MAP = Collections.synchronizedMap(new WeakHashMap<>());
-    private String key;
+    private static final Map<String, LockHolder> LOCK_MAP = Collections.synchronizedMap(new WeakHashMap<>());
+    private String keyReference;
 
     public AutoCloseableReentrantLock(final Class clazz, final String part) {
-        this.key = clazz.getName() + '#' + part;
-        LOCK_MAP.computeIfAbsent(key, missingKey -> new ReentrantLock()).lock();
+        final String keyString = clazz.getName() + '#' + part;
+        final LockHolder lockHolder = LOCK_MAP.computeIfAbsent(keyString, LockHolder::new);
+        this.keyReference = lockHolder.key.get();
+        lockHolder.lock.lock();
     }
 
     @Override
     public synchronized void close() {
         // The contract does not state the close() should be idempotent, but it's probably useful
-        if (key != null) {
-            final ReentrantLock lock = LOCK_MAP.get(key);
+        if (keyReference != null) {
+            final ReentrantLock lock = LOCK_MAP.get(keyReference).lock;
             if (lock != null && lock.isHeldByCurrentThread()) {
                 lock.unlock();
-                // Finally, clear the key so the GC can remove the entry from the WeakHashMap
-                key = null;
+                // Finally, clear the reference to the key so the GC can remove the entry from the WeakHashMap
+                keyReference = null;
             }
         }
+    }
 
+    private static class LockHolder {
+        private final WeakReference<String> key;
+        private final ReentrantLock lock;
+
+        private LockHolder(final String key) {
+            this.key = new WeakReference<>(key);
+            this.lock = new ReentrantLock();
+        }
     }
 }

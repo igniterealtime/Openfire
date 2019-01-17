912470465cbbb8577e39fbcd71903288a787fe57
Guus der Kinderen <guus.der.kinderen@gmail.com>
2019-01-17 23:20:35 +0100

    OF-1673: Add support for XEP-0359 (MUC)
    This commit adds a generic framework that implements XEP-0359 Unique and Stable Stanza IDs,
    and applies that framework to MUC rooms.

--- xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/LocalMUCUser.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/LocalMUCUser.java
@@ -17,10 +17,12 @@
 package org.jivesoftware.openfire.muc.spi;
 
 import org.dom4j.Element;
+import org.dom4j.QName;
 import org.jivesoftware.openfire.PacketException;
 import org.jivesoftware.openfire.PacketRouter;
 import org.jivesoftware.openfire.auth.UnauthorizedException;
 import org.jivesoftware.openfire.muc.*;
+import org.jivesoftware.openfire.stanzaid.StanzaIDUtil;
 import org.jivesoftware.openfire.user.UserAlreadyExistsException;
 import org.jivesoftware.util.LocaleUtils;
 import org.jivesoftware.util.NotFoundException;
@@ -163,6 +165,9 @@ public class LocalMUCUser implements MUCUser {
 
     @Override
     public void process(Packet packet) throws UnauthorizedException, PacketException {
+
+        StanzaIDUtil.ensureUniqueAndStableStanzaID( packet, packet.getTo() );
+
         if (packet instanceof IQ) {
             process((IQ)packet);
         }
---
+++ xmppserver/src/main/java/org/jivesoftware/openfire/stanzaid/StanzaIDUtil.java
@@ -0,0 +1,80 @@
+package org.jivesoftware.openfire.stanzaid;
+
+import org.dom4j.Element;
+import org.dom4j.QName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmpp.packet.JID;
+import org.xmpp.packet.Packet;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+/**
+ * Utility methods that implement XEP-0359: Unique and Stable Stanza IDs.
+ *
+ * @see <a href="https://xmpp.org/extensions/xep-0359.html>XEP-0359</a>
+ * @author Guus der Kinderen, guus.der.kinderen@gmail.com
+ */
+public class StanzaIDUtil
+{
+    private static final Logger Log = LoggerFactory.getLogger( StanzaIDUtil.class );
+
+    /**
+     * Modifies the stanza that's passed as a packet by adding a Stanza ID.
+     *
+     * @param packet The inbound packet (cannot be null).
+     * @param self The ID of the 'local' entity that will generate the stanza ID (cannot be null).
+     * @see <a href="https://xmpp.org/extensions/xep-0359.html>XEP-0359</a>
+     */
+    public static Packet ensureUniqueAndStableStanzaID( final Packet packet, final JID self )
+    {
+        // Stanza ID generating entities, which encounter a <stanza-id/> element where the 'by' attribute matches the 'by'
+        // attribute they would otherwise set, MUST delete that element even if they are not adding their own stanza ID.
+        final Iterator<Element> existingElementIterator = packet.getElement().elementIterator( QName.get( "stanza-id", "urn:xmpp:sid:0" ) );
+        while (existingElementIterator.hasNext()) {
+            final Element element = existingElementIterator.next();
+
+            if (self.toString().equals( element.attributeValue( "by" ) ) ) {
+                Log.warn( "Removing a 'stanza-id' element from an inbound stanza, as its 'by' attribute value matches the value that we would set. Offending stanza: {}", packet );
+                existingElementIterator.remove();
+            }
+        }
+
+        final String id = generateUniqueAndStableStanzaID( packet );
+
+        final Element stanzaIdElement = packet.getElement().addElement( QName.get( "stanza-id", "urn:xmpp:sid:0" ) );
+        stanzaIdElement.addAttribute( "id", id );
+        stanzaIdElement.addAttribute( "by", self.toString() );
+
+        return packet;
+    }
+
+    /**
+     * Returns a value that is an appropriate unique and stable stanza ID in
+     * context of XEP-0359: it's either the origin-id value, or a UUID.
+     *
+     * @param packet The stanza for what to return the ID (cannot be null).
+     * @return The ID (never null or empty string).
+     */
+    public static String generateUniqueAndStableStanzaID( final Packet packet )
+    {
+        String result = null;
+
+        final Iterator<Element> existingElementIterator = packet.getElement().elementIterator( QName.get( "origin-id", "urn:xmpp:sid:0" ) );
+        while (existingElementIterator.hasNext() && (result == null || result.isEmpty() ) )
+        {
+            final Element element = existingElementIterator.next();
+            result = element.attributeValue( "id" );
+        }
+
+        if ( result == null || result.isEmpty() ) {
+            result = UUID.randomUUID().toString();
+            Log.debug( "Using newly generated value '{}' for stanza that has id '{}'.", result, packet.getID() );
+        } else {
+            Log.debug( "Using origin-id provided value '{}' for stanza that has id '{}'.", result, packet.getID() );
+        }
+
+        return result;
+    }
+}

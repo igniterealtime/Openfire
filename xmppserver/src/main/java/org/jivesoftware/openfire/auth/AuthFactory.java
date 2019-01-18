5d8c2f41bcc63c1f6f4a99191d56eefc7d5f7c5b
Anatoly Sablin <sablintolya@gmail.com>
2019-01-18 20:52:38 +0300

    OF-1285 Create a simple option of resetting admin password. Fix typo. Add warning to the login page. Move oneTimeAccessToken tag to the comment with example. Remove entire property after successful login.

--- xmppserver/src/main/java/org/jivesoftware/openfire/auth/AuthFactory.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/auth/AuthFactory.java
@@ -322,9 +322,9 @@ public class AuthFactory {
     public static AuthToken checkOneTimeAccessToken(String userToken) throws UnauthorizedException {
         String accessToken = JiveGlobals.getXMLProperty(ONE_TIME_PROPERTY);
         if (isOneTimeAccessTokenEnabled() && accessToken.equals(userToken)) {
-            // Remove the one time token but left the property.
-            // This invocation will overwrite the openfire.xml with the empty OneTimeAccessToken tag.
-            JiveGlobals.setXMLProperty(ONE_TIME_PROPERTY, "");
+            // Remove the one time token.
+            // This invocation will overwrite the openfire.xml with removing the OneTimeAccessToken tag.
+            JiveGlobals.deleteXMLProperty(ONE_TIME_PROPERTY);
             Log.info("Login with the one time access token.");
             return AuthToken.generateOneTimeToken(accessToken);
         } else {

5bdc0e0f97169ff20e0feb39040c7f65b6a898c3
Guus der Kinderen <guus@goodbytes.nl>
2019-06-04 10:44:15 +0200

    OF-1790: Update expected db version.
    As a wise man once said:
    
    > Don't forget to bump the expected DB version to 30 so Openfire actually runs the script!

--- xmppserver/src/main/java/org/jivesoftware/database/SchemaManager.java
+++ xmppserver/src/main/java/org/jivesoftware/database/SchemaManager.java
@@ -68,7 +68,7 @@ public class SchemaManager {
     /**
      * Current Openfire database schema version.
      */
-    private static final int DATABASE_VERSION = 29;
+    private static final int DATABASE_VERSION = 30;
 
     /**
      * Checks the Openfire database schema to ensure that it's installed and up to date.

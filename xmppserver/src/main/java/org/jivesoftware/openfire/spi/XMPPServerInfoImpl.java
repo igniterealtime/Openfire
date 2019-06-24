2f866482b4e6d7c0c607e03221eccfd25a5f3b36
akrherz <akrherz@iastate.edu>
2019-06-24 13:53:32 -0500

    march master branch back to 4.4.0-SNAPSHOT

--- xmppserver/src/main/java/org/jivesoftware/openfire/spi/XMPPServerInfoImpl.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/spi/XMPPServerInfoImpl.java
@@ -39,7 +39,7 @@ public class XMPPServerInfoImpl implements XMPPServerInfo {
 
     private final Date startDate;
 
-    public static final Version VERSION = new Version(4, 4, 0, Version.ReleaseStatus.Beta, -1 );
+    public static final Version VERSION = new Version(4, 4, 0, Version.ReleaseStatus.Alpha, -1 );
 
     /**
      * Simple constructor

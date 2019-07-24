c5db97e0f90e0af5233bd01c14394405a2d717e2
Guus der Kinderen <guus@goodbytes.nl>
2019-07-24 14:04:01 +0200

    OF-1815: Add MUC search 'muclumbus' support.
    This commit adds support for the search functionality as described on and used by https://search.jabbercat.org/

--- xmppserver/src/main/java/org/jivesoftware/util/StringUtils.java
+++ xmppserver/src/main/java/org/jivesoftware/util/StringUtils.java
@@ -1203,4 +1203,47 @@ public final class StringUtils {
             return Optional.empty();
         }
     }
+
+    /**
+     * Simple Java program to tokenize string as a shell would - similar to shlex in Python
+     *
+     * @param string The string to be split.
+     * @return The splitted string.
+     * @see <a href="https://gist.github.com/raymyers/8077031">https://gist.github.com/raymyers/8077031</a>
+     */
+    public static List<String> shellSplit(CharSequence string) {
+        List<String> tokens = new ArrayList<>();
+        if ( string == null ) {
+            return tokens;
+        }
+        boolean escaping = false;
+        char quoteChar = ' ';
+        boolean quoting = false;
+        StringBuilder current = new StringBuilder() ;
+        for (int i = 0; i<string.length(); i++) {
+            char c = string.charAt(i);
+            if (escaping) {
+                current.append(c);
+                escaping = false;
+            } else if (c == '\\' && !(quoting && quoteChar == '\'')) {
+                escaping = true;
+            } else if (quoting && c == quoteChar) {
+                quoting = false;
+            } else if (!quoting && (c == '\'' || c == '"')) {
+                quoting = true;
+                quoteChar = c;
+            } else if (!quoting && Character.isWhitespace(c)) {
+                if (current.length() > 0) {
+                    tokens.add(current.toString());
+                    current = new StringBuilder();
+                }
+            } else {
+                current.append(c);
+            }
+        }
+        if (current.length() > 0) {
+            tokens.add(current.toString());
+        }
+        return tokens;
+    }
 }

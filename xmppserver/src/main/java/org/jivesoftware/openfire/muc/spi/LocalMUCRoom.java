afb19a03a9bfa4a325b652777ca5c517470a94af
cpetzka <31418387+cpetzka@users.noreply.github.com>
2019-07-26 12:34:24 +0200

    fix a ClassCastException in LocalMUCRoom.java

--- xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/LocalMUCRoom.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/LocalMUCRoom.java
@@ -549,7 +549,7 @@ public class LocalMUCRoom implements MUCRoom, GroupEventListener {
     }
 
     @Override
-    public LocalMUCRole joinRoom(String nickname, String password, HistoryRequest historyRequest,
+    public MUCRole joinRoom(String nickname, String password, HistoryRequest historyRequest,
             LocalMUCUser user, Presence presence) throws UnauthorizedException,
             UserAlreadyExistsException, RoomLockedException, ForbiddenException,
             RegistrationRequiredException, ConflictException, ServiceUnavailableException,
@@ -560,7 +560,7 @@ public class LocalMUCRoom implements MUCRoom, GroupEventListener {
                 throw new UnauthorizedException();
             }
         }
-        LocalMUCRole joinRole = null;
+        MUCRole joinRole = null;
         lock.writeLock().lock();
         boolean clientOnlyJoin = false;
         // A "client only join" here is one where the client is already joined, but has re-joined.
@@ -682,7 +682,7 @@ public class LocalMUCRoom implements MUCRoom, GroupEventListener {
                 occupantsByFullJID.put(user.getAddress(), joinRole);
             } else {
                 // Grab the existing one.
-                joinRole = (LocalMUCRole) occupantsByFullJID.get(user.getAddress());
+                joinRole = occupantsByFullJID.get(user.getAddress());
                 joinRole.setPresence( presence ); // OF-1581: Use latest presence information.
            }
         }
@@ -764,7 +764,7 @@ public class LocalMUCRoom implements MUCRoom, GroupEventListener {
      *
      * @param joinRole the role of the new occupant in the room.
      */
-    private void sendInitialPresences(LocalMUCRole joinRole) {
+    private void sendInitialPresences(MUCRole joinRole) {
         for (MUCRole occupant : occupantsByFullJID.values()) {
             if (occupant == joinRole) {
                 continue;

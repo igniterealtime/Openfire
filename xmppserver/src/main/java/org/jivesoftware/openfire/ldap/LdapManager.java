14f0e3b732557ab9f56a67824b458fdd7916ba4e
Guus der Kinderen <guus@goodbytes.nl>
2019-12-11 21:25:49 +0100

    OF-1937: Reduce connection timeout for LDAP.
    We've found that with a connection timeout of larger than 4 seconds, Spark fails to authenticate against LDAP, when LDAP integration is making use of more than one LDAP server with the first of the two unavailable.
    
    This commit reduces the connection timeout to a value that allows Spark to authenticate. It does not, however, prevent that Openfire operates with degraded performance.

--- xmppserver/src/main/java/org/jivesoftware/openfire/ldap/LdapManager.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/ldap/LdapManager.java
@@ -654,7 +654,7 @@ public class LdapManager {
         if (connTimeout > 0) {
             env.put("com.sun.jndi.ldap.connect.timeout", String.valueOf(connTimeout));
         } else {
-            env.put("com.sun.jndi.ldap.connect.timeout", "10000");
+            env.put("com.sun.jndi.ldap.connect.timeout", "4000");
         }
 
         if (readTimeout > 0) {
@@ -762,7 +762,7 @@ public class LdapManager {
             if (connTimeout > 0) {
                     env.put("com.sun.jndi.ldap.connect.timeout", String.valueOf(connTimeout));
                 } else {
-                    env.put("com.sun.jndi.ldap.connect.timeout", "10000");
+                    env.put("com.sun.jndi.ldap.connect.timeout", "4000");
                 }
 
             if (readTimeout > 0) {
@@ -848,7 +848,11 @@ public class LdapManager {
                         env.put(Context.SECURITY_CREDENTIALS, password);
                     }
 
-                        env.put("com.sun.jndi.ldap.connect.timeout", "10000");
+                    if (connTimeout > 0) {
+                        env.put("com.sun.jndi.ldap.connect.timeout", String.valueOf(connTimeout));
+                    } else {
+                        env.put("com.sun.jndi.ldap.connect.timeout", "4000");
+                    }
 
                     if (ldapDebugEnabled) {
                         env.put("com.sun.jndi.ldap.trace.ber", System.err);

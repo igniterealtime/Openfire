acb85af581868339e4e6f3e09d3f403ea67138e5
Guus der Kinderen <guus@goodbytes.nl>
2019-12-31 22:27:06 +0100

    OF-1956: s2s connection acceptor retry startup after bind exception

--- xmppserver/src/main/java/org/jivesoftware/openfire/spi/LegacyConnectionAcceptor.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/spi/LegacyConnectionAcceptor.java
@@ -2,8 +2,13 @@ package org.jivesoftware.openfire.spi;
 
 import org.jivesoftware.openfire.Connection;
 import org.jivesoftware.openfire.net.SocketAcceptThread;
+import org.jivesoftware.util.SystemProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.BindException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * A connection acceptor that employs the legacy, pre-MINA/NIO socket implementation of Openfire.
@@ -15,6 +20,14 @@ import org.slf4j.LoggerFactory;
 public class LegacyConnectionAcceptor extends ConnectionAcceptor
 {
     private final Logger Log = LoggerFactory.getLogger( LegacyConnectionAcceptor.class );
+
+    public static final SystemProperty<Duration> STARTUP_RETRY_DELAY = SystemProperty.Builder.ofType(Duration.class)
+        .setKey("xmpp.server.startup.retry.delay")
+        .setDefaultValue(Duration.ofMillis(2000))
+        .setMinValue(Duration.ofMillis(-1))
+        .setChronoUnit(ChronoUnit.MILLIS)
+        .setDynamic(Boolean.TRUE)
+        .build();
 
     private SocketAcceptThread socketAcceptThread;
 
@@ -50,23 +63,41 @@ public class LegacyConnectionAcceptor extends ConnectionAcceptor
             Log.warn( "Configuration allows for up to " + configuration.getMaxThreadPoolSize() + " threads, although implementation is limited to exactly one." );
         }
 
+        doStart(true);
+    }
+
+    private synchronized void doStart(boolean allowRetry) {
         try {
             socketAcceptThread = new SocketAcceptThread(configuration.getPort(), configuration.getBindAddress(), configuration.getTlsPolicy() == Connection.TLSPolicy.legacyMode);
             socketAcceptThread.setDaemon(true);
             socketAcceptThread.setPriority(Thread.MAX_PRIORITY);
             socketAcceptThread.start();
-
         }
         catch (Exception e) {
-            System.err.println( "Error starting " + configuration.getPort() + ": " + e.getMessage() );
-            Log.error( "Error starting: " + configuration.getPort(), e );
+            System.err.println("Error starting " + configuration.getPort() + ": " + e.getMessage());
+            Log.error("Error starting: " + configuration.getPort(), e);
 
             // Reset for future use.
-            if (socketAcceptThread != null ) {
+            if (socketAcceptThread != null) {
                 try {
                     socketAcceptThread.shutdown();
                 } finally {
                     socketAcceptThread = null;
+                }
+            }
+
+            if (allowRetry && e instanceof BindException) {
+                // OF-1956: Retry a failed bind after a short delay, to work around restart race conditions.
+                final Duration delay = STARTUP_RETRY_DELAY.getValue();
+                if (delay != null && !delay.isNegative()) {
+                    System.err.println("Retrying to start " + configuration.getPort() + " after a delay of " + delay);
+                    Log.error("Retrying to start " + configuration.getPort() + " after a delay of " + delay);
+                    try {
+                        Thread.sleep( delay.toMillis());
+                        doStart(false);
+                    } catch (InterruptedException ex) {
+                        Log.error("Restart interrupted.", ex);
+                    }
                 }
             }
         }

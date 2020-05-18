ba6c5c10208e17294a1b19f48bda0013d9416326
Guus der Kinderen <guus@goodbytes.nl>
2020-05-18 20:02:42 +0200

    OF-2026: Do not require XEP-0359 values to be UUIDs

--- xmppserver/src/test/java/org/jivesoftware/openfire/stanzaid/StanzaIDUtilTest.java
+++ xmppserver/src/test/java/org/jivesoftware/openfire/stanzaid/StanzaIDUtilTest.java
@@ -65,6 +65,25 @@ public class StanzaIDUtilTest
     }
 
     /**
+     * Test if {@link StanzaIDUtil.generateUniqueAndStableStanzaID} uses the 'origin-id' provided value,
+     * if that's present, even when the value is not a UUID.
+     */
+    @Test
+    public void testUseOriginIDWhenPresentNonUUID() throws Exception
+    {
+        // Setup fixture.
+        final Packet input = new Message();
+        final String expected = "not-a-uuid";
+        input.getElement().addElement( "origin-id", "urn:xmpp:sid:0" ).addAttribute( "id", expected );
+
+        // Execute system under test.
+        final String result = StanzaIDUtil.generateUniqueAndStableStanzaID( input );
+
+        // Verify results.
+        assertEquals( expected, result );
+    }
+
+    /**
      * Test if {@link StanzaIDUtil.ensureUniqueAndStableStanzaID} adds a stanza-id element
      * with proper 'by' and UUID value if the provided input does not have a 'origin-id'
      * element.
@@ -183,5 +202,49 @@ public class StanzaIDUtilTest
         }
         Assert.assertEquals( expected, stanzaIDElement.attributeValue( "id" ) );
         assertEquals( self.toString(), stanzaIDElement.attributeValue( "by" ) );
+    }
+
+    /**
+     * Test if {@link StanzaIDUtil#findFirstUniqueAndStableStanzaID(Packet, String)} can parse a stanza that contains a
+     * stanza ID.
+     */
+    @Test
+    public void testParseUUIDValue() throws Exception
+    {
+        // Setup fixture.
+        final Packet input = new Message();
+        final JID self = new JID( "foobar" );
+        final String expected = "de305d54-75b4-431b-adb2-eb6b9e546013";
+        final Element toOverwrite = input.getElement().addElement( "stanza-id", "urn:xmpp:sid:0" );
+        toOverwrite.addAttribute( "id", expected );
+        toOverwrite.addAttribute( "by", self.toString() );
+
+        // Execute system under test.
+        final String result = StanzaIDUtil.findFirstUniqueAndStableStanzaID( input, self.toString() );
+
+        // Verify results.
+        assertEquals( expected, result );
+    }
+
+    /**
+     * Test if {@link StanzaIDUtil#findFirstUniqueAndStableStanzaID(Packet, String)} can parse a stanza that contains a
+     * stanza ID that is not a UUID value. OF-2026
+     */
+    @Test
+    public void testParseNonUUIDValue() throws Exception
+    {
+        // Setup fixture.
+        final Packet input = new Message();
+        final JID self = new JID( "foobar" );
+        final String expected = "not-a-uuid";
+        final Element toOverwrite = input.getElement().addElement( "stanza-id", "urn:xmpp:sid:0" );
+        toOverwrite.addAttribute( "id", expected );
+        toOverwrite.addAttribute( "by", self.toString() );
+
+        // Execute system under test.
+        final String result = StanzaIDUtil.findFirstUniqueAndStableStanzaID( input, self.toString() );
+
+        // Verify results.
+        assertEquals( expected, result );
     }
 }

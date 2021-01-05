efa3c75bf78b1bdaf67c1eaf077e1a6d25756206
Guus der Kinderen <guus.der.kinderen@gmail.com>
2021-01-05 16:40:44 +0100

    OF-2176: Use SystemProperty instead of JiveGlobals

--- xmppserver/src/main/java/org/jivesoftware/admin/SessionListener.java
+++ xmppserver/src/main/java/org/jivesoftware/admin/SessionListener.java
@@ -1,10 +1,13 @@
 package org.jivesoftware.admin;
 
 import org.jivesoftware.util.JiveGlobals;
+import org.jivesoftware.util.SystemProperty;
 
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import javax.servlet.annotation.WebListener;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * A http session listener that implements a configurable session timeout.<p>
@@ -17,11 +20,20 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class SessionListener implements HttpSessionListener {
 
-     private static final int sessionTimeout = JiveGlobals.getIntProperty( "adminConsole.sessionTimeout", 30*60 ); // secs
-
+     public static final SystemProperty<Duration> SESSION_TIMEOUT = SystemProperty.Builder.ofType(Duration.class)
+                                                                        .setKey("adminConsole.sessionTimeout")
+                                                                        .setChronoUnit(ChronoUnit.SECONDS)
+                                                                        .setDefaultValue( Duration.ofMinutes(30) )
+                                                                        .setMinValue(Duration.ZERO)
+                                                                        .setMaxValue(Duration.ofSeconds(Integer.MAX_VALUE) )
+                                                                        .setDynamic(true)
+                                                                        .build();
 
      public void sessionCreated(HttpSessionEvent event){
-         event.getSession().setMaxInactiveInterval(sessionTimeout);
+         // Using Duration#getSeconds() isn't ideal, as that ignores part of the duration (the sign, as well as the
+         // nano-seconds). However, as the value can expected to be a positive integer, this data can safely be ignored.
+         // TODO: After moving to Java 9 or higher, this should be replaced with Duration#toSeconds()
+         event.getSession().setMaxInactiveInterval((int) SESSION_TIMEOUT.getValue().getSeconds());
      }
 
 

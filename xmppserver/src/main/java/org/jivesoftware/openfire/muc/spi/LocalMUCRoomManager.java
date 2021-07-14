53a738dfb64fc259fc69309e8ee469a804ddb06e
Guus der Kinderen <guus.der.kinderen@gmail.com>
2021-07-14 16:33:46 +0200

    OF-2272: Maintain MUCRoom instance in a cache
    Instead of maintaining a local view of the state of all Local- and RemoteMUCRooms, Openfire will now use a clustered Cache to store all MUCRoom instances.
    
    More work around various aspects is needed:
    - a local copy should (probably) be maintained, to allow the cache to be repopulated when leaving/joining the cluster (OF-2221)
    - when modifications to a MUC room are applied, the instance needs to be explicitly re-added to the cache, to let other cluster nodes be aware of the change (OF-2220)

--- xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/LocalMUCRoomManager.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/LocalMUCRoomManager.java
@@ -1,12 +1,17 @@
 package org.jivesoftware.openfire.muc.spi;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.jivesoftware.openfire.event.GroupEventDispatcher;
 import org.jivesoftware.openfire.muc.MUCRoom;
+import org.jivesoftware.openfire.muc.MultiUserChatService;
+import org.jivesoftware.util.cache.Cache;
+import org.jivesoftware.util.cache.CacheFactory;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.stream.Collectors;
 
 /**
  * this class supports the simple MUCRoom management including remove,add and query.
@@ -17,8 +22,15 @@ import org.jivesoftware.openfire.muc.MUCRoom;
  * @author <a href="mailto:583424568@qq.com">wuchang</a>
  * 2016-1-14
  */
-public class LocalMUCRoomManager {
-    private final Map<String, MUCRoom> rooms = new ConcurrentHashMap<>();
+public class LocalMUCRoomManager
+{
+    private final Cache<String, MUCRoom> CACHE_ROOM;
+
+    LocalMUCRoomManager(@Nonnull final MultiUserChatService service) {
+        CACHE_ROOM= CacheFactory.createCache("MUC Service '" + service.getServiceName() + "' Rooms");
+        CACHE_ROOM.setMaxLifetime(-1);
+        CACHE_ROOM.setMaxCacheSize(-1L);
+    }
 
     /**
      * Returns the number of chat rooms that are currently actively loaded in memory.
@@ -26,35 +38,65 @@ public class LocalMUCRoomManager {
      * @return a chat room count.
      */
     public int getNumberChatRooms(){
-        return rooms.size();
+        return CACHE_ROOM.size();
     }
 
-    public void addRoom(final String roomname, final MUCRoom room){
-        rooms.put(roomname, room);
-        GroupEventDispatcher.addListener(room);
+    public Lock getLock(@Nonnull final String roomName) {
+        return CACHE_ROOM.getLock(roomName);
     }
-    
+
+    public void addRoom(final String roomname, final MUCRoom room) {
+        final Lock lock = CACHE_ROOM.getLock(roomname);
+        lock.lock();
+        try {
+            CACHE_ROOM.put(roomname, room);
+        } finally {
+            lock.unlock();
+        }
+
+        GroupEventDispatcher.addListener(room); // TODO this event listener is added only in the node where the room is created. Does this mean that events are not prop
+    }
+
+    // TODO As modifications to rooms won't be persisted in the cache without the room having being explicitly put back in the cache,
+    //      this method probably needs work. Documentation should be added and/or this should return an Unmodifiable collection (although
+    //      that still does not rule out modifications to individual collection items. Can we replace it completely with a 'getRoomNames()'
+    //      method, which would then force usage to acquire a lock before operating on a room.
     public Collection<MUCRoom> getRooms(){
-        return rooms.values();
+        return CACHE_ROOM.values();
     }
-    
+
+    // TODO this should probably not be used without a lock having been acquired and set. Update all usages to do so.
     public MUCRoom getRoom(final String roomname){
-        return rooms.get(roomname);
+        return CACHE_ROOM.get(roomname);
     }
     
     public MUCRoom removeRoom(final String roomname){
         //memory leak will happen if we forget remove it from GroupEventDispatcher
-        final MUCRoom room = rooms.remove(roomname);
-        if (room != null) {
-            GroupEventDispatcher.removeListener(room);
+        final Lock lock = CACHE_ROOM.getLock(roomname);
+        lock.lock();
+        try {
+            final MUCRoom room = CACHE_ROOM.remove(roomname);
+            if (room != null) {
+                GroupEventDispatcher.removeListener(room);
+            }
+            return room;
+        } finally {
+            lock.unlock();
         }
-        return room;
     }
     
     public void cleanupRooms(final Date cleanUpDate) {
-        for (final MUCRoom room : getRooms()) {
-            if (room.getEmptyDate() != null && room.getEmptyDate().before(cleanUpDate)) {
-                removeRoom(room.getName());
+        final Set<String> roomNames = getRooms().stream().map(MUCRoom::getName).collect(Collectors.toSet());
+        for (final String roomName : roomNames) {
+            final Lock lock = CACHE_ROOM.getLock(roomName);
+            lock.lock();
+            try {
+                final MUCRoom room = getRoom(roomName);
+                if (room.getEmptyDate() != null && room.getEmptyDate().before(cleanUpDate)) {
+                    removeRoom(roomName);
+                }
+            } finally {
+                lock.unlock();
             }
         }
     }

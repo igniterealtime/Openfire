360025a451dddb3a4421b14c580b0a180aa50a07
Guus der Kinderen <guus.der.kinderen@gmail.com>
2021-12-01 09:21:24 +0100

    OF-2340: Add missing 'return' statement.

--- xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/MultiUserChatServiceImpl.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/MultiUserChatServiceImpl.java
@@ -553,6 +553,7 @@ public class MultiUserChatServiceImpl implements Component, MultiUserChatService
     {
         if (packet.getError() != null) {
             Log.debug("Avoid generating an error in response to a stanza that itself has an error (to avoid the chance of entering an endless back-and-forth of exchanging errors). Suppress sending an {} error (with message '{}') in response to: {}", error, message, packet);
+            return;
         }
         if ( packet instanceof IQ )
         {

3a04b70195cf20c9940cc3a6e5d1fc598edf15ab
Guus der Kinderen <guus.der.kinderen@gmail.com>
2022-06-08 14:55:14 +0200

    Replace 'size() == 0' with 'isEmpty()'
    For some Collections, a call to size() is not a constant-time operation. As a matter of principle, isEmpty() should be prefered over checking for a size that equals zero.

--- xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpSession.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpSession.java
@@ -1061,7 +1061,7 @@ public class HttpSession extends LocalClientSession {
         builder.append("<body xmlns='http://jabber.org/protocol/httpbind' ack='")
             .append(getLastAcknowledged()).append("'>");
 
-        setLastResponseEmpty(elements.size() == 0);
+        setLastResponseEmpty(elements.isEmpty());
         for (Deliverable child : elements) {
             builder.append(child.getDeliverable());
         }

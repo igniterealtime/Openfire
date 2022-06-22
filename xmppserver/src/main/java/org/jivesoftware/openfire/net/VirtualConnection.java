igniterealtime/Openfire

commit 257806e4ce061462de782d69fe5afeabdcdc9264

OF-2414: Where appropriate, send a stream error before closing a stream.
When a stream is being closed, it's often helpful to signal why this happens, especially when the stream is closed due to an error.

This commit adds stream errors in various places where such context is desirable.

The API to close a connection has been modified to be able to include an optional stream error. This replaces an older structure where a stream error is first sent manually, followed by a call to the 'close' API. With this change, only one attempt to send data is made instead of two (one for the stream error, and another one for the end-stream tag).

xmppserver/src/main/java/org/jivesoftware/openfire/net/VirtualConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,6 @@
 
 package org.jivesoftware.openfire.net;
 
-import java.security.cert.Certificate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
-
 import org.jivesoftware.openfire.Connection;
 import org.jivesoftware.openfire.ConnectionCloseListener;
 import org.jivesoftware.openfire.PacketDeliverer;
@@ -29,8 +24,13 @@ import org.jivesoftware.openfire.session.Session;
 import org.jivesoftware.util.LocaleUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xmpp.packet.StreamError;
 
 import javax.annotation.Nullable;
+import java.security.cert.Certificate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Abstract implementation of the Connection interface that models abstract connections. Abstract
@@ -182,6 +182,16 @@ public abstract class VirtualConnection implements Connection {
      */
     @Override
     public void close() {
+    }
+
+    /**
+     * Closes the session, the virtual connection and notifies listeners that the connection
+     * has been closed.
+     *
+     * @param error If non-null, the end-stream tag will be preceded with this error.
+     */
+    @Override
+    public void close(@Nullable final StreamError error) {
         if (state.compareAndSet(State.OPEN, State.CLOSED)) {
             
             if (session != null) {
@@ -203,7 +213,7 @@ public abstract class VirtualConnection implements Connection {
             listeners.clear();
 
             try {
-                closeVirtualConnection();
+                closeVirtualConnection(error);
             } catch (Exception e) {
                 Log.error(LocaleUtils.getLocalizedString("admin.error.close") + "\n" + toString(), e);
             }
@@ -242,8 +252,10 @@ public abstract class VirtualConnection implements Connection {
     }
 
     /**
-     * Closes the virtual connection. Subsclasses should indicate what closing a virtual
+     * Closes the virtual connection. Subclasses should indicate what closing a virtual
      * connection means. At this point the session has a CLOSED state.
+     *
+     * @param error If non-null, this error will be sent to the peer before the connection is disconnected.
      */
-    public abstract void closeVirtualConnection();
+    public abstract void closeVirtualConnection(@Nullable final StreamError error);
 }

xmppserver/src/main/java/org/jivesoftware/openfire/Connection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,16 @@
 
 package org.jivesoftware.openfire;
 
-import java.io.Closeable;
-import java.net.UnknownHostException;
-import java.security.cert.Certificate;
-
 import org.jivesoftware.openfire.auth.UnauthorizedException;
 import org.jivesoftware.openfire.session.LocalSession;
 import org.jivesoftware.openfire.spi.ConnectionConfiguration;
 import org.xmpp.packet.Packet;
+import org.xmpp.packet.StreamError;
 
 import javax.annotation.Nullable;
+import java.io.Closeable;
+import java.net.UnknownHostException;
+import java.security.cert.Certificate;
 
 /**
  * Represents a connection on the server.
@@ -158,6 +158,21 @@ public interface Connection extends Closeable {
      */
     @Override
     void close();
+
+    /**
+     * Close this session including associated socket connection, optionally citing a
+     * stream error. The events for closing the session are:
+     * <ul>
+     *      <li>Set closing flag to prevent redundant shutdowns.
+     *      <li>Close the socket.
+     *      <li>Notify all listeners that the channel is shut down.
+     * </ul>
+     *
+     * Not all implementations use the same order of events.
+     *
+     * @param error If non-null, the end-stream tag will be preceded with this error.
+     */
+    void close(@Nullable final StreamError error);
 
     /**
      * Notification message indicating that the server is being shutdown. Implementors

xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,10 +40,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
-import org.xmpp.packet.IQ;
-import org.xmpp.packet.Message;
-import org.xmpp.packet.Packet;
-import org.xmpp.packet.Presence;
+import org.xmpp.packet.*;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -972,7 +969,7 @@ public class HttpSession extends LocalClientSession {
         sentElements.add(delivered);
     }
 
-    private void closeSession() {
+    private void closeSession(@Nullable final StreamError error) {
         try {
             // There generally should not be a scenario where there are pending connections, as well as pending elements
             // to deliver, as when a new connection becomes available while there are pending elements, those will be
@@ -1080,6 +1077,24 @@ public class HttpSession extends LocalClientSession {
     }
 
     /**
+     * Creates an empty BOSH 'body' element that including a 'terminate' type attribute (that, in BOSH, signifies the
+     * end of a session), sets the 'condition' attribute to 'remote-stream-error' (to signal that an XMPP error is
+     * transported), and includes the error in the body.
+     *
+     * @param error The error to be included in the body.
+     * @return The string representation of a BOSH 'body' element.
+     */
+    @Nonnull
+    protected String createRemoteStreamErrorBody(@Nonnull final StreamError error)
+    {
+        final Element body = DocumentHelper.createElement( QName.get( "body", "http://jabber.org/protocol/httpbind" ) );
+        body.addAttribute("type", "terminate");
+        body.addAttribute("condition", "remote-stream-error");
+        body.add(error.getElement());
+        return body.asXML();
+    }
+
+    /**
      * Creets a BOSH 'body' element that represents a 'session restart' event, including the stream features that are
      * available to this session.
      *
@@ -1118,8 +1133,8 @@ public class HttpSession extends LocalClientSession {
         }
 
         @Override
-        public void closeVirtualConnection() {
-            ((HttpSession) session).closeSession();
+        public void closeVirtualConnection(@Nullable final StreamError error) {
+            ((HttpSession) session).closeSession(error);
         }
 
         @Override
@@ -1139,7 +1154,7 @@ public class HttpSession extends LocalClientSession {
 
         @Override
         public void systemShutdown() {
-            close();
+            close(new StreamError(StreamError.Condition.system_shutdown));
         }
 
         @Override

xmppserver/src/main/java/org/jivesoftware/openfire/multiplex/ClientSessionConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,9 @@ import org.jivesoftware.openfire.spi.ConnectionManagerImpl;
 import org.jivesoftware.openfire.spi.ConnectionType;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.Packet;
+import org.xmpp.packet.StreamError;
 
+import javax.annotation.Nullable;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -169,9 +171,11 @@ public class ClientSessionConnection extends VirtualConnection {
      * nothing. But if the server originated the request to close the connection then we need
      * to send to the Connection Manager a packet letting him know that the Client Session needs
      * to be terminated.
+     *
+     * @param error If non-null, this error will be sent to the peer before the connection is disconnected.
      */
     @Override
-    public void closeVirtualConnection() {
+    public void closeVirtualConnection(@Nullable final StreamError error) {
         // Figure out who requested the connection to be closed
         StreamID streamID = session.getStreamID();
         if (multiplexerManager.getClientSession(connectionManagerName, streamID) == null) {
@@ -191,6 +195,10 @@ public class ClientSessionConnection extends VirtualConnection {
                         "http://jabber.org/protocol/connectionmanager");
                 child.addAttribute("id", streamID.getID());
                 child.addElement("close");
+                if (error != null) {
+                    // Note that this is ignored by all Connection Manager implementations at the time of writing.
+                    child.add(error.getElement());
+                }
                 multiplexerSession.process(closeRequest);
             }
         }

xmppserver/src/main/java/org/jivesoftware/openfire/net/ServerSocketReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,25 +16,21 @@
 
 package org.jivesoftware.openfire.net;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.util.concurrent.ThreadPoolExecutor;
-
 import org.dom4j.Element;
 import org.jivesoftware.openfire.PacketRouter;
 import org.jivesoftware.openfire.RoutingTable;
 import org.jivesoftware.openfire.auth.UnauthorizedException;
+import org.jivesoftware.openfire.event.ServerSessionEventDispatcher;
 import org.jivesoftware.openfire.interceptor.PacketRejectedException;
 import org.jivesoftware.openfire.session.LocalIncomingServerSession;
-import org.jivesoftware.openfire.event.ServerSessionEventDispatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmpp.packet.IQ;
-import org.xmpp.packet.Message;
-import org.xmpp.packet.Packet;
-import org.xmpp.packet.Presence;
-import org.xmpp.packet.StreamError;
+import org.xmpp.packet.*;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * A SocketReader specialized for server connections. This reader will be used when the open
@@ -159,26 +155,18 @@ public class ServerSocketReader extends SocketReader {
      */
     private void packetReceived(Packet packet) throws PacketRejectedException {
         if (packet.getTo() == null || packet.getFrom() == null) {
-            Log.debug("Closing IncomingServerSession due to packet with no TO or FROM: " +
-                    packet.toXML());
-            // Send a stream error saying that the packet includes no TO or FROM
-            StreamError error = new StreamError(StreamError.Condition.improper_addressing);
-            connection.deliverRawText(error.toXML());
-            // Close the underlying connection
-            connection.close();
+            Log.debug("Closing IncomingServerSession due to packet with no TO or FROM: {}", packet.toXML());
+            // Send a stream error saying that the packet includes no TO or FROM and close the underlying connection.
+            connection.close(new StreamError(StreamError.Condition.improper_addressing, "Missing 'to' or 'from' attribute."));
             open = false;
             throw new PacketRejectedException("Packet with no TO or FROM attributes");
         }
         else if (!((LocalIncomingServerSession) session).isValidDomain(packet.getFrom().getDomain())) {
-            Log.debug("Closing IncomingServerSession due to packet with invalid domain: " +
-                    packet.toXML());
-            // Send a stream error saying that the packet includes an invalid FROM
-            StreamError error = new StreamError(StreamError.Condition.invalid_from);
-            connection.deliverRawText(error.toXML());
-            // Close the underlying connection
-            connection.close();
+            Log.debug("Closing IncomingServerSession due to packet with invalid domain: {}", packet.toXML());
+            // Send a stream error saying that the packet includes an invalid FROM and close the underlying connection.
+            connection.close(new StreamError(StreamError.Condition.invalid_from, "Invalid domain specified in 'from' attribute."));
             open = false;
-            throw new PacketRejectedException("Packet with no TO or FROM attributes");
+            throw new PacketRejectedException("Packet invalid FROM attribute");
         }
     }
 

xmppserver/src/main/java/org/jivesoftware/openfire/net/SocketConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,25 @@
 
 package org.jivesoftware.openfire.net;
 
+import com.jcraft.jzlib.JZlib;
+import com.jcraft.jzlib.ZOutputStream;
+import org.jivesoftware.openfire.*;
+import org.jivesoftware.openfire.auth.UnauthorizedException;
+import org.jivesoftware.openfire.session.IncomingServerSession;
+import org.jivesoftware.openfire.session.LocalSession;
+import org.jivesoftware.openfire.session.Session;
+import org.jivesoftware.openfire.spi.ConnectionConfiguration;
+import org.jivesoftware.openfire.spi.ConnectionManagerImpl;
+import org.jivesoftware.openfire.spi.ConnectionType;
+import org.jivesoftware.util.JiveGlobals;
+import org.jivesoftware.util.LocaleUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmpp.packet.Packet;
+import org.xmpp.packet.StreamError;
+
+import javax.annotation.Nullable;
+import javax.net.ssl.SSLPeerUnverifiedException;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -32,26 +51,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-
-import javax.annotation.Nullable;
-import javax.net.ssl.SSLPeerUnverifiedException;
-
-import org.jivesoftware.openfire.*;
-import org.jivesoftware.openfire.auth.UnauthorizedException;
-import org.jivesoftware.openfire.session.IncomingServerSession;
-import org.jivesoftware.openfire.session.LocalSession;
-import org.jivesoftware.openfire.session.Session;
-import org.jivesoftware.openfire.spi.ConnectionConfiguration;
-import org.jivesoftware.openfire.spi.ConnectionManagerImpl;
-import org.jivesoftware.openfire.spi.ConnectionType;
-import org.jivesoftware.util.JiveGlobals;
-import org.jivesoftware.util.LocaleUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.xmpp.packet.Packet;
-
-import com.jcraft.jzlib.JZlib;
-import com.jcraft.jzlib.ZOutputStream;
 
 /**
  * An object to track the state of a XMPP client-server session.
@@ -248,8 +247,8 @@ public class SocketConnection implements Connection {
             writer.flush();
         }
         catch (Exception e) {
-            Log.warn("Closing no longer valid connection" + "\n" + this.toString(), e);
-            close();
+            Log.warn("Closing no longer valid connection" + "\n" + this, e);
+            close(new StreamError(StreamError.Condition.internal_server_error, "An error occurred while trying to validate this connection."));
         }
         finally {
             // Register that we finished sending data on the connection
@@ -469,7 +468,7 @@ public class SocketConnection implements Connection {
      * Closes the connection without sending any data (not even a stream end-tag).
      */
     public void forceClose() {
-        close( true );
+        close(true, null);
     }
 
     /**
@@ -477,7 +476,17 @@ public class SocketConnection implements Connection {
      */
     @Override
     public void close() {
-        close( false );
+        close(false, null);
+    }
+
+    /**
+     * Closes the connection after trying to send an optional error and stream end tag.
+     *
+     * @param error If non-null, the end-stream tag will be preceded with this error.
+     */
+    @Override
+    public void close(@Nullable final StreamError error) {
+        close(false, error);
     }
 
     /**
@@ -486,7 +495,7 @@ public class SocketConnection implements Connection {
      * when sending data over the socket has taken a long time and we need to close the socket, discard
      * the connection and its session.
      */
-    private void close(boolean force) {
+    private void close(boolean force, @Nullable final StreamError error) {
         if (state.compareAndSet(State.OPEN, State.CLOSED)) {
             
             if (session != null) {
@@ -500,7 +509,14 @@ public class SocketConnection implements Connection {
                     allowedToWrite = true;
                     // Register that we started sending data on the connection
                     writeStarted();
-                    writer.write("</stream:stream>");
+
+                    String rawEndStream = "";
+                    if (error != null) {
+                        rawEndStream = error.toXML();
+                    }
+                    rawEndStream += "</stream:stream>";
+
+                    writer.write(rawEndStream);
                     writer.flush();
                 }
                 catch (Exception e) {
@@ -522,9 +538,7 @@ public class SocketConnection implements Connection {
 
     @Override
     public void systemShutdown() {
-        deliverRawText("<stream:error><system-shutdown " +
-                "xmlns='urn:ietf:params:xml:ns:xmpp-streams'/></stream:error>");
-        close();
+        close(new StreamError(StreamError.Condition.system_shutdown));
     }
 
     void writeStarted() {
@@ -541,7 +555,7 @@ public class SocketConnection implements Connection {
      * not finished in a long time or when the client has not sent a heartbeat for a long time.
      * In any of both cases the socket will be closed.
      *
-     * @return true if the socket was closed due to a bad health.s
+     * @return true if the socket was closed due to a bad health.
      */
     boolean checkHealth() {
         // Check that the sending operation is still active

xmppserver/src/main/java/org/jivesoftware/openfire/net/SocketReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,6 @@
  */
 
 package org.jivesoftware.openfire.net;
-
-import java.io.IOException;
-import java.net.Socket;
-import java.util.List;
 
 import org.dom4j.Element;
 import org.dom4j.io.XMPPPacketReader;
@@ -37,13 +33,11 @@ import org.slf4j.LoggerFactory;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
-import org.xmpp.packet.IQ;
-import org.xmpp.packet.JID;
-import org.xmpp.packet.Message;
-import org.xmpp.packet.PacketError;
-import org.xmpp.packet.Presence;
-import org.xmpp.packet.Roster;
-import org.xmpp.packet.StreamError;
+import org.xmpp.packet.*;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
 
 /**
  * A SocketReader creates the appropriate {@link Session} based on the defined namespace in the
@@ -353,15 +347,10 @@ public abstract class SocketReader implements Runnable {
      * closing the connection a stream error will be sent to the entity.
      */
     void closeNeverSecuredConnection() {
-        // Set the not_authorized error
-        StreamError error = new StreamError(StreamError.Condition.not_authorized);
-        // Deliver stanza
-        connection.deliverRawText(error.toXML());
-        // Close the underlying connection
-        connection.close();
+        // Send a stream error and close the underlying connection.
+        connection.close(new StreamError(StreamError.Condition.not_authorized, "TLS is mandatory, but was not established."));
         // Log a warning so that admins can track this case from the server side
-        Log.warn("TLS was required by the server and connection was never secured. " +
-                "Closing connection : " + connection);
+        Log.warn("TLS was required by the server and connection was never secured. Closing connection: {}", connection);
     }
 
     private IQ getIQ(Element doc) {
@@ -432,16 +421,10 @@ public abstract class SocketReader implements Runnable {
             sb.append("xmlns=\"").append(xpp.getNamespace(null)).append("\" ");
             sb.append("xmlns:stream=\"").append(xpp.getNamespace("stream")).append("\" ");
             sb.append("version=\"1.0\">");
-            // Set the host_unknown error
-            StreamError error = new StreamError(StreamError.Condition.host_unknown);
-            sb.append(error.toXML());
-            // Deliver stanza
-            connection.deliverRawText(sb.toString());
-            // Close the underlying connection
-            connection.close();
+            // Send the host_unknown error and close the underlying connection
+            connection.close(new StreamError(StreamError.Condition.host_unknown, "The 'to' attribute does not specify an XMPP domain entity served by this service."));
             // Log a warning so that admins can track this cases from the server side
-            Log.warn("Closing session due to incorrect hostname in stream header. Host: " + host +
-                    ". Connection: " + connection);
+            Log.warn("Closing session due to incorrect hostname in stream header. Host: {}. Connection: {}", host, connection);
         }
 
         // Create the correct session based on the sent namespace. At this point the server
@@ -461,15 +444,10 @@ public abstract class SocketReader implements Runnable {
             sb.append("xmlns=\"").append(xpp.getNamespace(null)).append("\" ");
             sb.append("xmlns:stream=\"").append(xpp.getNamespace("stream")).append("\" ");
             sb.append("version=\"1.0\">");
-            // Include the bad-namespace-prefix in the response
-            StreamError error = new StreamError(StreamError.Condition.bad_namespace_prefix);
-            sb.append(error.toXML());
-            connection.deliverRawText(sb.toString());
-            // Close the underlying connection
-            connection.close();
+            // Include the bad-namespace-prefix in the response and close the underlying connection.
+            connection.close(new StreamError(StreamError.Condition.bad_namespace_prefix, "The namespace used in the request does not identify functionality that can be provided by this endpoint."));
             // Log a warning so that admins can track this cases from the server side
-            Log.warn("Closing session due to bad_namespace_prefix in stream header. Prefix: " +
-                    xpp.getNamespace(null) + ". Connection: " + connection);
+            Log.warn("Closing session due to bad_namespace_prefix in stream header. Prefix: {}. Connection: {}", xpp.getNamespace(null), connection);
         }
     }
 

xmppserver/src/main/java/org/jivesoftware/openfire/net/SocketReadingMode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,6 @@
 
 package org.jivesoftware.openfire.net;
 
-import java.io.IOException;
-import java.net.Socket;
-
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.jivesoftware.openfire.Connection;
@@ -29,6 +26,8 @@ import org.xmlpull.v1.XmlPullParserException;
 import org.xmpp.packet.StreamError;
 
 import javax.net.ssl.SSLHandshakeException;
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Abstract class for {@link BlockingReadingMode}.
@@ -66,15 +65,10 @@ abstract class SocketReadingMode {
      */
     protected boolean negotiateTLS() {
         if (socketReader.connection.getTlsPolicy() == Connection.TLSPolicy.disabled) {
-            // Set the not_authorized error
-            StreamError error = new StreamError(StreamError.Condition.not_authorized);
-            // Deliver stanza
-            socketReader.connection.deliverRawText(error.toXML());
-            // Close the underlying connection
-            socketReader.connection.close();
+            // Send a not_authorized error and close the underlying connection
+            socketReader.connection.close(new StreamError(StreamError.Condition.not_authorized, "A request to negotiate TLS is denied, as TLS has been disabled by configuration."));
             // Log a warning so that admins can track this case from the server side
-            Log.warn("TLS requested by initiator when TLS was never offered by server. " +
-                    "Closing connection : " + socketReader.connection);
+            Log.warn("TLS requested by initiator when TLS was never offered by server. Closing connection: {}", socketReader.connection);
             return false;
         }
         // Client requested to secure the connection using TLS. Negotiate TLS.

commit 5bab9980e6e578e7589f134b8b13de77f618e17e

OF-2413: Ensure that connections are closed, when no error occurs.
The fix (which as of yet is unreleased) for OF-2413 introduces a bug, where a call to close() doesn't do anything anymore.

This bug is introduced in commit 257806e4ce061462de782d69fe5afeabdcdc9264, which was merged in the main branch of the Openfire repository with pull request https://github.com/igniterealtime/Openfire/pull/2026. No releases have been made from that branch, since this issue was introduced. By merging this change prior to the next release of the main branch (expected to be v4.8.0), the bug can be fixed before it was ever released.

xmppserver/src/main/java/org/jivesoftware/openfire/net/VirtualConnection.java
@@ -182,6 +182,7 @@ public abstract class VirtualConnection implements Connection {
      */
     @Override
     public void close() {
+        close(null);
     }
 
     /**
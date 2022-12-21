f7e9f97f3cd83f640cc0865821fd1ca10eed13d5
Guus der Kinderen <guus.der.kinderen@gmail.com>
2022-12-21 16:17:33 +0100

    OF-2561: Fix fallback for ConnectionListener 'verify'
    This fixes a bug that occurs when the verifyCertificateValidity call of a ConnectionListener is delegated to a fall-back ConnectionListener.

--- xmppserver/src/main/java/org/jivesoftware/openfire/spi/ConnectionListener.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/spi/ConnectionListener.java
@@ -775,7 +775,7 @@ public class ConnectionListener
         }
         else
         {
-            return JiveGlobals.getBooleanProperty( propertyName, getConnectionListener( type.getFallback() ).acceptSelfSignedCertificates() );
+            return JiveGlobals.getBooleanProperty( propertyName, getConnectionListener( type.getFallback() ).verifyCertificateValidity() );
         }
     }
 

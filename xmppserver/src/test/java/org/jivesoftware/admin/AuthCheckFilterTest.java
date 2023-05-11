c8f646b505fd3c317a188007cc8a63db667dafb1
Brent Slater <brent.slater@surevine.com>
2023-05-11 14:40:45 +0100

    OF-2597 simplifying

--- xmppserver/src/test/java/org/jivesoftware/admin/AuthCheckFilterTest.java
+++ xmppserver/src/test/java/org/jivesoftware/admin/AuthCheckFilterTest.java
@@ -70,7 +70,7 @@ public class AuthCheckFilterTest {
         assertFalse(AuthCheckFilter.testURLPassesExclude("login.jsp?logout=false&another=true", "login.jsp?logout=false"));
 
         assertFalse(AuthCheckFilter.testURLPassesExclude("setup/setup-/../../log.jsp?log=info&mode=asc&lines=All","setup/setup-*"));
-        assertFalse(AuthCheckFilter.testURLPassesExclude("setup/setup-/%2E/%2E/log.jsp?log=info&mode=asc&lines=All","setup/setup-*"));
+        assertFalse(AuthCheckFilter.testURLPassesExclude("setup/setup-/%2E%2E/%2E%2E/log.jsp?log=info&mode=asc&lines=All","setup/setup-*"));
         assertFalse(AuthCheckFilter.testURLPassesExclude("setup/setup-s/%u002e%u002e/%u002e%u002e/log.jsp?log=info&mode=asc&lines=Al", "setup/setup-*"));
        
         assertTrue(AuthCheckFilter.testURLPassesExclude("setup/setup-new.jsp","setup/setup-*"));

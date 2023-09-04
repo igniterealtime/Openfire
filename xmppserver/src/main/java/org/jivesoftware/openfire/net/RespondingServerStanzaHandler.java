01111d8f2387bf82f9529c8151e25af8cf89d59f
Guus der Kinderen <guus.der.kinderen@gmail.com>
2023-09-04 19:01:52 +0200

    OF-2655: Refactor previous fix
    This refactors the previous fix to an extend that it simply re-uses pre-existing API.

--- xmppserver/src/main/java/org/jivesoftware/openfire/net/RespondingServerStanzaHandler.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/net/RespondingServerStanzaHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software, 2023 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2023 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ public class RespondingServerStanzaHandler extends StanzaHandler {
 
         // Clear routing table cache of previous sessions for domain pair and re-add it.
         RoutingTable routingTable = XMPPServer.getInstance().getRoutingTable();
-        routingTable.replaceServerRoute(domainPair, (LocalOutgoingServerSession) session);
+        routingTable.addServerRoute(domainPair, (LocalOutgoingServerSession) session); // This _replaces_ the pre-existing session in the routing table.
         SessionManager sessionManager = SessionManager.getInstance();
         sessionManager.outgoingServerSessionCreated((LocalOutgoingServerSession) session);
 

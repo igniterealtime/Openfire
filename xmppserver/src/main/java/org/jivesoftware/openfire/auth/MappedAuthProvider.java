529ef56c24ad454db76650edf9919d471f37f791
Dan Caseley <dan@caseley.me.uk>
2023-12-14 23:08:08 +0000

    OF-2716: Fix consistency and fix some missing copyright references
    Via script, amend any items missing the (C) after Copyright, amend any items missing 'All rights reserved' at the end of the copyright line, then run other fixes now that they're consistent.

--- xmppserver/src/main/java/org/jivesoftware/openfire/auth/MappedAuthProvider.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/auth/MappedAuthProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 IgniteRealtime.org
+ * Copyright (C) 2016 IgniteRealtime.org, 2016-2019 Ignite Realtime Foundation. All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- xmppserver/src/main/java/org/jivesoftware/openfire/sasl/ScramSha1SaslServer.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/sasl/ScramSha1SaslServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Surevine Ltd
+ * Copyright (C) 2015 Surevine Ltd, 2016-2020 Ignite Realtime Foundation. All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

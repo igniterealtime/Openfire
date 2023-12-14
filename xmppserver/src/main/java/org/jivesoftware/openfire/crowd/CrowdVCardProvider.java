a4734a54a128e82d50d388b3d6b1bc3a3de61879
Dan Caseley <dan@caseley.me.uk>
2023-12-14 21:58:57 +0000

    OF-2716: Fix years for Ignite Realtime references in copyright headers
    Via script, use git history to amend years of edits of the file

--- xmppserver/src/main/java/org/jivesoftware/openfire/crowd/CrowdVCardProvider.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/crowd/CrowdVCardProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Issa Gorissen <issa-gorissen@usa.net>, 2022 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2012 Issa Gorissen <issa-gorissen@usa.net>, 2017-2022 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpBindContentSecurityPolicyFilter.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpBindContentSecurityPolicyFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2017-2023 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

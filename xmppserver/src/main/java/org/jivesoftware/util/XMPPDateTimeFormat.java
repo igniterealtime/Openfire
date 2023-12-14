3345d1b51a7b5771c9d2a2c8a22a6945743d321d
Dan Caseley <dan@caseley.me.uk>
2023-12-14 23:08:08 +0000

    OF-2716: Fix consistency and fix some missing copyright references
    Via script, amend any items missing the (C) after Copyright, amend any items missing 'All rights reserved' at the end of the copyright line, then run other fixes now that they're consistent.

--- xmppserver/src/main/java/org/jivesoftware/util/XMPPDateTimeFormat.java
+++ xmppserver/src/main/java/org/jivesoftware/util/XMPPDateTimeFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Florian Schmaus
+ * Copyright (C) 2013 Florian Schmaus, 2017-2019 Ignite Realtime Foundation. All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

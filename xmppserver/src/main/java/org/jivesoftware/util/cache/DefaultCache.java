d77190863b221fffdbbac9cb1cc54a5288bf114a
Guus der Kinderen <guus.der.kinderen@gmail.com>
2024-02-08 14:14:27 +0100

    OF-2792: Admin Console Cache Summary for Clustering
    When clustering is used, many caches are not byte-size based, but entity-count based. The admin console page should properly show this.
    
    This commit modifies the Cache interface to provide additional data used to display this page. Specifically:
    - Caches now report their capacity unit (byte or entity)
    - Caches can report additional remarks (eg: "this is a per clusternode count")
    
    The Cache Overview admin console page has been modified to make use of these new attributes. When combined with an update of the Hazelcast plugin (for it to report the correct values), this improves the correctness of the displayed data remarkably.
    
    The changes in this commit are intended to be backwards compatible with older versions of the Hazelcast plugin. The data reported will still be wrong, but functinality should not break.

--- xmppserver/src/main/java/org/jivesoftware/util/cache/DefaultCache.java
+++ xmppserver/src/main/java/org/jivesoftware/util/cache/DefaultCache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2008 Jive Software, 2016-2022 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2004-2008 Jive Software, 2016-2024 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -336,6 +336,17 @@ public class DefaultCache<K extends Serializable, V extends Serializable> implem
         synchronized (this) {
             return new HashSet<>(map.keySet());
         }
+    }
+
+    /**
+     * Defines the unit used to calculate the capacity of the cache, which for all instances of this class is byte-size
+     * based.
+     *
+     * @return the unit to be used to calculate the capacity of this cache, which will be {@link CapacityUnit#BYTES}.
+     */
+    @Override
+    public CapacityUnit getCapacityUnit() {
+        return CapacityUnit.BYTES;
     }
 
     /**

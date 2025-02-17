1e954ea42d9101434be193316b61a8e815afe43d
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-02-17 17:00:09 +0100

    OF-2967: Remove newlins in toString implementations
    Where a stanza is used in a toString(), use to `toXML` rather than `toString` of the stanza, as the latter uses 'pretty print' (which introduces newlines).

--- xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpSession.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/http/HttpSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2008 Jive Software, 2017-2023 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2005-2008 Jive Software, 2017-2025 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1471,7 +1471,7 @@ public class HttpSession extends LocalClientSession {
             ", isInitialized=" + isInitialized() +
             ", hasAuthToken=" + (getAuthToken() != null) +
             ", peer address='" + peerAddress +'\'' +
-            ", presence='" + getPresence().toString() + '\'' +
+            ", presence='" + getPresence().toXML() + '\'' +
             ", hold='" + getHold() + '\'' +
             ", wait='" + getWait() + '\'' +
             ", maxRequests='" + getMaxRequests() + '\'' +

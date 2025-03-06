b5440647a65c3fe416e5bcca2ebd33fe8ec4e7bc
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-03-06 12:41:25 +0100

    OF-2760: Fix MSSQL date/time column
    The Transact-SQL timestamp data type is different from the timestamp data type defined in the ISO standard. In MSSQL, `timestamp` is a synonym for the `rowversion` data type.

--- distribution/src/database/upgrade/36/openfire_sqlserver.sql
+++ distribution/src/database/upgrade/36/openfire_sqlserver.sql
@@ -5,7 +5,7 @@ CREATE TABLE ofMucRoomRetiree (
   name                NVARCHAR(50)  NOT NULL,
   alternateJID        NVARCHAR(2000),
   reason              NVARCHAR(1024),
-  retiredAt           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  retiredAt           DATETIME DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT ofMucRoomRetiree_pk PRIMARY KEY (serviceID, name)
 );
 

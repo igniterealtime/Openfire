1e697e9b261e80f77167537408b59567a8c97e38
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-03-13 17:02:42 +0100

    CI: Database-check: create dummy OPENFIRE_HOME
    The original code expects files to exist in a certain relative position (which isn't the case on GitHub). Instead, the required files are now created.

--- build/ci/updater/src/main/java/com/igniterealtime/openfire/updaterunner/Main.java
+++ build/ci/updater/src/main/java/com/igniterealtime/openfire/updaterunner/Main.java
@@ -18,6 +18,8 @@ package com.igniterealtime.openfire.updaterunner;
 import org.jivesoftware.database.*;
 import org.jivesoftware.util.JiveGlobals;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -49,8 +51,13 @@ public class Main {
             Path parent = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
             PropertiesReader reader = new PropertiesReader(parent.resolve("maven-archiver/pom.properties")); // TODO determine why we read properties but not use them. Is this an existence check only?
 
-            Path distributionDir = parent.resolve("../../../../distribution/target/distribution-base");
-            JiveGlobals.setHomePath(distributionDir);
+            // Create a dummy OPENFIRE_HOME, in which the config files are created where XML properties will be stored.
+            final Path confDir = Files.createDirectory(parent.resolve("conf"));
+            final Path propertyFile = Files.createFile(confDir.resolve("openfire.xml"));
+            Files.writeString(propertyFile, "<jive/>");
+            final Path securityFile = Files.createFile(confDir.resolve("security.xml"));
+            Files.writeString(securityFile, "<security/>");
+            JiveGlobals.setHomePath(parent);
 
             JiveGlobals.setXMLProperty("connectionProvider.className", "org.jivesoftware.database.DefaultConnectionProvider");
             JiveGlobals.setXMLProperty("database.defaultProvider.driver", connectionDriver);

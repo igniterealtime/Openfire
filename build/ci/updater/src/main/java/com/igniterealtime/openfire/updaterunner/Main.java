edc52d6bb7e1c568a14471477294e5905cea7a71
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-03-20 23:11:16 +0100

    CI: database tests require OPENFIRE_HOME

--- build/ci/updater/src/main/java/com/igniterealtime/openfire/updaterunner/Main.java
+++ build/ci/updater/src/main/java/com/igniterealtime/openfire/updaterunner/Main.java
@@ -15,17 +15,19 @@
  */
 package com.igniterealtime.openfire.updaterunner;
 
+import org.apache.commons.io.FileUtils;
 import org.jivesoftware.database.*;
 import org.jivesoftware.util.JiveGlobals;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 
 public class Main {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws Exception
+    {
         if (args.length == 0) {
             throw new IllegalArgumentException("Invoke with one argument: the path to the compiled distribution (eg: 'distribution/target/distribution-base/'");
         }
@@ -55,7 +57,27 @@ public class Main {
         if (!distributionDir.toFile().exists()) {
             throw new IllegalArgumentException("Distribution directory does not exist: " + distributionDir);
         }
-        JiveGlobals.setHomePath(distributionDir);
+
+        // The Home path (OPENFIRE_HOME) is used for two things:
+        // - this application needs openfire.xml and security.xml in OPENFIRE_HOME/conf
+        // - Openfire will attempt to load the database scripts from OPENFIRE_HOME/resources/database
+        // Note that when CI is executing this application, the workspace is _uncompiled_. This application attempts to
+        // create a mock OPENFIRE_HOME that holds just enough information for the application to be able to execute.
+        final Path openfireHome = Files.createDirectory(distributionDir.resolve("mockOpenfireHome"));
+
+        // Create the 'conf' directory.
+        final Path confDir = Files.createDirectory(openfireHome.resolve("conf"));
+        final Path propertyFile = Files.createFile(confDir.resolve("openfire.xml"));
+        Files.writeString(propertyFile, "<jive/>");
+        final Path securityFile = Files.createFile(confDir.resolve("security.xml"));
+        Files.writeString(securityFile, "<security/>");
+
+        // Create the 'resources/database' directory (which is a direct copy from source)
+        final Path sourceDir = distributionDir.resolve("distribution/src/database");
+        final Path destinationDir = Files.createDirectories(openfireHome.resolve("resources/database"));
+        FileUtils.copyDirectory(sourceDir.toFile(), destinationDir.toFile());
+
+        JiveGlobals.setHomePath(openfireHome);
 
         JiveGlobals.setXMLProperty("connectionProvider.className", "org.jivesoftware.database.DefaultConnectionProvider");
         JiveGlobals.setXMLProperty("database.defaultProvider.driver", connectionDriver);
@@ -75,7 +97,7 @@ public class Main {
             //Try updating explicitly. It'll already have happened once as a byproduct of the above, but we need to trap the result
             SchemaManager sm = new SchemaManager();
             boolean fixed = sm.checkOpenfireSchema(conn);
-            if(!fixed){
+            if (!fixed) {
                 throw new Exception("Failed to install or upgrade the database! It is likely that the installation or upgrade SQL scripts contain a bug.");
             }
             System.out.println("Database schema check complete.");

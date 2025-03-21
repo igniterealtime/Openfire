9cc3bbe530145ea0835e1d47900a14dfb5ea33ca
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-03-21 15:06:07 +0100

    Spelling: search/replaced 'attibutes' -> 'attributes'

--- xmppserver/src/main/java/org/jivesoftware/openfire/crowd/CrowdGroupProvider.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/crowd/CrowdGroupProvider.java
@@ -262,7 +262,7 @@ public class CrowdGroupProvider extends AbstractGroupProvider {
     // TODO search on group attributes in Crowd
     @Override
     public Collection<String> search(String key, String value) {
-        LOG.info("Search groups on attibutes not implemented yet");
+        LOG.info("Search groups on attributes not implemented yet");
         return Collections.emptyList();
     }
 

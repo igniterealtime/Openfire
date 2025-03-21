10e73d5de8244eb49ed7f754ed9608f370394fa0
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-03-21 15:15:00 +0100

    Spelling: search/replaced 'intitiator' -> 'initiator'

--- xmppserver/src/main/java/org/jivesoftware/openfire/filetransfer/FileTransferRejectedException.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/filetransfer/FileTransferRejectedException.java
@@ -78,7 +78,7 @@ public class FileTransferRejectedException extends Exception {
     }
 
     /**
-     * Returns the text to include in a message that will be sent to the intitiator and target
+     * Returns the text to include in a message that will be sent to the initiator and target
      * of the file transfer that got rejected or {@code null} if none was defined. If no text was
      * specified then no message will be sent to the parties of the rejected file transfer.
      *

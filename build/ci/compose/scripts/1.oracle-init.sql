6784a36f8b410ca37f717f78a3552dd7e6088eb3
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-03-22 20:57:00 +0100

    CI: database install tests should start with an empty database
    Some databases require an explicit CREATE DATABASE statement, while other Docker images already provision an empty database.

--- build/ci/compose/scripts/1.oracle-init.sql
+++ build/ci/compose/scripts/1.oracle-init.sql
@@ -3,3 +3,4 @@ create user openfire identified by SecurePa55w0rd;
 grant connect, resource to openfire;
 grant unlimited tablespace to openfire;
 alter session set current_schema = openfire;
+CREATE DATABASE openfire;

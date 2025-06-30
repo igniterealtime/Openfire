70f67a1037f3a405426410c2c719bf38801b270b
akrherz <akrherz@iastate.edu>
2025-06-30 13:11:05 -0500

    March version toward 5.0.2-SNAPSHOT

--- xmppserver/src/main/java/org/jivesoftware/openfire/spi/XMPPServerInfoImpl.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/spi/XMPPServerInfoImpl.java
@@ -39,7 +39,7 @@ public class XMPPServerInfoImpl implements XMPPServerInfo {
 
     private final Date startDate;
 
-    public static final Version VERSION = new Version(5, 0, 1, Version.ReleaseStatus.Release, -1 );
+    public static final Version VERSION = new Version(5, 0, 2, Version.ReleaseStatus.Alpha, -1 );
 
     /**
      * Simple constructor

b510881fc9b69494063428d5e5aa7c9be4a91764
Guus der Kinderen <guus.der.kinderen@gmail.com>
2026-01-19 19:06:38 +0100

    OF-3169: Fix infinite recursion in CachingPubsubPersistenceProvider flush
    Removed a recursive call to flushPendingChanges(boolean) that could cause unbounded recursion, stack overflows, and excessive cluster flush traffic under concurrent load.

--- xmppserver/src/main/java/org/jivesoftware/openfire/pubsub/CachingPubsubPersistenceProvider.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/pubsub/CachingPubsubPersistenceProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2025 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2020-2026 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -638,7 +638,6 @@ public class CachingPubsubPersistenceProvider implements PubSubPersistenceProvid
         }
 
         delegate.bulkPublishedItems( addList, delList );
-        flushPendingChanges( sendToCluster );
     }
 
     @Override

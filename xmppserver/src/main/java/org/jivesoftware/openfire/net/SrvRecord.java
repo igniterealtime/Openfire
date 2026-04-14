3b51f22b3adf998c6927c007648f65828ea3f4b5
Guus der Kinderen <guus.der.kinderen@gmail.com>
2026-04-14 15:40:17 +0200

    OF-3250: (code review) update copyright year

--- xmppserver/src/main/java/org/jivesoftware/openfire/net/SrvRecord.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/net/SrvRecord.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Ignite Realtime Foundation. All rights reserved.
+ * Copyright (C) 2024-2026 Ignite Realtime Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

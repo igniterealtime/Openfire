d7e92d5a4b0ab130310cf7a9eb50b7be139e7414
Guus der Kinderen <guus.der.kinderen@gmail.com>
2026-04-24 14:24:43 +0200

    OF-3257/OF-3258: Improve one-time initialization of SERVER_SECRET_NONEXISTENT_USERS
    The previous method for one-time initialization of the SERVER_SECRET_NONEXISTENT_USERS property (which depended on a static initializer block) proved to be fragile.
    
    In this commit, initialization happens in a getter that should be used instead of directly accessing the property.

--- xmppserver/src/main/java/org/jivesoftware/openfire/sasl/ScramSha1SaslServer.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/sasl/ScramSha1SaslServer.java
@@ -555,7 +555,7 @@ public class ScramSha1SaslServer implements SaslServer {
     {
         try {
             return ScramUtils.computeHmac(
-                SERVER_SECRET_NONEXISTENT_USERS.getValue().getBytes(StandardCharsets.UTF_8),
+                getServerSecretForNonExistentUsers().getBytes(StandardCharsets.UTF_8),
                 input
             );
         } catch (SaslException e) {

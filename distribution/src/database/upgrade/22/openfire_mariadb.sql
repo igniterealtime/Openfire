019be128efa74d056c68fb3280b888d71127565f
Guus der Kinderen <guus.der.kinderen@gmail.com>
2026-06-02 12:28:42 +0200

    OF-3239 (code review): Fix invalid  comment in v22 upgrade scripts

--- distribution/src/database/upgrade/22/openfire_mariadb.sql
+++ distribution/src/database/upgrade/22/openfire_mariadb.sql
@@ -1,4 +1,4 @@
-// add columns for SASL SCRAM-SHA-1
+# add columns for SASL SCRAM-SHA-1
 ALTER TABLE ofUser ADD COLUMN storedKey VARCHAR(32);
 ALTER TABLE ofUser ADD COLUMN serverKey VARCHAR(32);
 ALTER TABLE ofUser ADD COLUMN salt VARCHAR(32);

acc6a2e82d61527222cca98bb5b20a48d7411147
Dave Cridland <dave@cridland.net>
2026-07-21 16:13:34 +0100

    FAST - HT-* database upgrade

---
+++ distribution/src/database/upgrade/41/openfire_mariadb.sql
@@ -0,0 +1,11 @@
+# Add ofFastToken table for FAST (XEP-0484) authentication token storage.
+
+CREATE TABLE ofFastToken (
+  username              VARCHAR(64)     NOT NULL,
+  mechanism             VARCHAR(32)     NOT NULL,
+  tokenHash             VARCHAR(64)     NOT NULL,
+  expiry                VARCHAR(35)     NOT NULL,
+  PRIMARY KEY (username, mechanism)
+);
+
+UPDATE ofVersion SET version = 41 WHERE name = 'openfire';
---
+++ distribution/src/database/upgrade/41/openfire_oracle.sql
@@ -0,0 +1,13 @@
+-- Add ofFastToken table for FAST (XEP-0484) authentication token storage.
+
+CREATE TABLE ofFastToken (
+  username              VARCHAR2(64)    NOT NULL,
+  mechanism             VARCHAR2(32)    NOT NULL,
+  tokenHash             VARCHAR2(64)    NOT NULL,
+  expiry                VARCHAR2(35)    NOT NULL,
+  CONSTRAINT ofFastToken_pk PRIMARY KEY (username, mechanism)
+);
+
+UPDATE ofVersion SET version = 41 WHERE name = 'openfire';
+
+commit;
